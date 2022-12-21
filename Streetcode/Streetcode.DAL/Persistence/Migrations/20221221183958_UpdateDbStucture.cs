918ea0cb6cc0bf1f16010ab5d1b7eaa7438b9238
dlend <dlend@softserveinc.com>
2022-12-21 20:40:50 +0200

    runned miigration after pull

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20221221183958_UpdateDbStucture.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    public partial class UpdateDbStucture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 21, 20, 39, 57, 929, DateTimeKind.Local).AddTicks(7374));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 21, 20, 39, 57, 929, DateTimeKind.Local).AddTicks(7314));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 21, 20, 39, 57, 929, DateTimeKind.Local).AddTicks(7350));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 21, 20, 39, 57, 929, DateTimeKind.Local).AddTicks(7355));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 13, 18, 4, 50, 189, DateTimeKind.Local).AddTicks(3678));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 13, 18, 4, 50, 189, DateTimeKind.Local).AddTicks(3616));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 13, 18, 4, 50, 189, DateTimeKind.Local).AddTicks(3655));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 13, 18, 4, 50, 189, DateTimeKind.Local).AddTicks(3660));
+        }
+    }
+}

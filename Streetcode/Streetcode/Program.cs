70cc65032b5da065913e391e9ec22a20f9a69a79
grygorenkod <hryhorenkodm@gmail.com>
2022-12-21 18:13:42 +0200

    Add appsettings & environment variables

--- Streetcode/Streetcode/Program.cs
+++ Streetcode/Streetcode/Program.cs
@@ -1,14 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Services.Interfaces;
 using Services.Services;
+using System;
+
+// for Nuke
+//environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+//if (environment != "Local")
+//{
+//    Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Local", EnvironmentVariableTarget.Machine);
+//}
 
 var builder = WebApplication.CreateBuilder(args);
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.SetBasePath(Directory.GetCurrentDirectory());
+    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    config.AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true);
+    config.AddEnvironmentVariables("STREETCODE_");
+});
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAppServices();
@@ -19,16 +35,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    builder.Configuration.AddUserSecrets<string>();
     app.UseSwagger();
     app.UseSwaggerUI();
+    
+}
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
 
-
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();

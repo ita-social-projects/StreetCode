e1f76bb924ff8211523e2e65fe549f4242c21f64
dimasster <dmytrobuchkovsky@gmail.com>
2022-12-23 17:48:44 +0200

    improve clean target

--- build/Targets/BaseBuild.cs
+++ build/Targets/BaseBuild.cs
@@ -4,6 +4,7 @@ using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
+using System.Linq;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 using static Nuke.Common.Tools.Npm.NpmTasks;
@@ -12,16 +13,42 @@ namespace Targets;
 
 partial class Build
 {
+    Target CleanTests => _ => _
+    .Executes(() =>
+    {
+        UnitTestsDirectory
+            .GlobDirectories("**/bin", "**/obj")
+            .ForEach(DeleteDirectory);
+        IntegrationTestsDirectory
+            .GlobDirectories("**/bin", "**/obj")
+            .ForEach(DeleteDirectory);
+    });
+
+    Target CleanNukeTemp => _ => _
+    .Executes(() =>
+    {
+        NukeDirectory
+            .GlobFiles("**/execution-plan.html")
+            .ForEach(DeleteFile);
+    });
+
+    Target CleanSubmodule => _ => _
+    .Executes(() =>
+    {
+        ClientDirectory
+            .GlobDirectories("**/node-modules")
+            .ForEach(DeleteDirectory);
+    });
+
     Target Clean => _ => _
-        .DependsOn(SetupNuke)
+        .DependsOn(/*SetupNuke,*/ CleanTests, CleanNukeTemp, CleanSubmodule)
         .Executes(() =>
         {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            SourceDirectory
+                .GlobDirectories("**/bin", "**/obj")
+                .ForEach(DeleteDirectory);
+
             EnsureCleanDirectory(OutputDirectory);
-            
-            //TODO clean node_modules
-            
-            
         });
 
     Target Restore => _ => _

580066c50161add090ad07f42aab64d11c44ab08
dimasster <dmytrobuchkovsky@gmail.com>
2022-12-23 17:59:23 +0200

    make changes to the project

--- build/Build.cs
+++ build/Build.cs
@@ -6,7 +6,7 @@ namespace Targets;
 [ShutdownDotNetAfterServerBuild]
 partial class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Setup);
+    public static int Main() => Execute<Build>(x => x.SetupNuke);
 
     Target Run => _ => _
         .DependsOn(CompileAPI, CompileCli);

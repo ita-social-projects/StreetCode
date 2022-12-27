0807545640d95a1a1715691864621928b2fb829f
EyR1oN <91558615+EyR1oN@users.noreply.github.com>
2022-12-28 00:01:22 +0200

    resolve conflicts

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Fact/Queries/GetAllFactsQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using FluentResults;
-using MediatR;
-using Streetcode.BLL.DTO.Streetcode.TextContent;
-
-namespace Streetcode.BLL.MediatR.Streetcode.Fact.Queries
-{
-    public record GetAllFactsQuery : IRequest<Result<IEnumerable<FactDTO>>>;
-}

2e1e631f9f1d7664f8d434126a9848f6df2ae133
dimasster <dmytrobuchkovsky@gmail.com>
2022-12-27 20:13:01 +0200

    add ukr collation to db

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20221227180254_AddCyrillicCollation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    public partial class AddCyrillicCollation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                collation: "SQL_Ukrainian_CP1251_CI_AS");
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 27, 20, 2, 52, 474, DateTimeKind.Local).AddTicks(2373));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 27, 20, 2, 52, 474, DateTimeKind.Local).AddTicks(2255));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 27, 20, 2, 52, 474, DateTimeKind.Local).AddTicks(2334));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 27, 20, 2, 52, 474, DateTimeKind.Local).AddTicks(2342));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase(
+                oldCollation: "SQL_Ukrainian_CP1251_CI_AS");
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 23, 22, 21, 11, 540, DateTimeKind.Local).AddTicks(939));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 23, 22, 21, 11, 540, DateTimeKind.Local).AddTicks(843));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 23, 22, 21, 11, 540, DateTimeKind.Local).AddTicks(886));
+
+            migrationBuilder.UpdateData(
+                schema: "streetcode",
+                table: "streetcodes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2022, 12, 23, 22, 21, 11, 540, DateTimeKind.Local).AddTicks(890));
+        }
+    }
+}

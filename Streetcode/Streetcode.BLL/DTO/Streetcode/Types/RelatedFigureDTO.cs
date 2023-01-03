b65126c5bd0334b5c5539f95c0e96d622dbc6fbc
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-04 00:33:55 +0200

    add related figure DTO and mediatr

--- Streetcode/Streetcode.BLL/DTO/Streetcode/Types/RelatedFigureDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Streetcode.BLL.DTO.Streetcode;
-
-public class RelatedFigureDTO
-{
-    public int ObserverId { get; set; }
-    public int TargetId { get; set; }
-    public StreetcodeDTO Observer { get; set; }
-    public StreetcodeDTO Target { get; set; }
-}

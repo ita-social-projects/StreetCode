d2a75ffeb2788b267ec7f0c0ae20c093a57438aa
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-04 12:41:03 +0200

    Revert "Merge pull request #55 from ita-social-projects/enh/related-figure"
    This reverts commit 0cbefc5621447e7a122c9668ea3eeebf84c93450, reversing
    changes made to 6ae93bd1f03d6ec19c53150571d65e65db592c11.

--- Streetcode/Streetcode.DAL/Repositories/Interfaces/Streetcode/IRelatedFigureRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Streetcode.DAL.Entities.Streetcode;
-using Streetcode.DAL.Repositories.Interfaces.Base;
-
-namespace Streetcode.DAL.Repositories.Interfaces.Streetcode;
-
-public interface IRelatedFigureRepository : IRepositoryBase<RelatedFigure>
-{
-}

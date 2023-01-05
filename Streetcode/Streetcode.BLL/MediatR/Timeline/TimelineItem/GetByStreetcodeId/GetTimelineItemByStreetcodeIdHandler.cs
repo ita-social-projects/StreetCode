df543abdb5240ee49e819f0ead51be87796e5fc0
EyR1oN <91558615+EyR1oN@users.noreply.github.com>
2023-01-05 21:26:33 +0200

    fixed bugs

--- Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetByStreetcodeId/GetTimelineItemByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetByStreetcodeId/GetTimelineItemByStreetcodeIdHandler.cs
@@ -6,7 +6,7 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.GetByStreetcodeId;
 
-public class GetTimelineItemByStreetcodeIdHandler : IRequestHandler<GetTimelineItemByStreetcodeIdQuery, Result<TimelineItemDTO>>
+public class GetTimelineItemByStreetcodeIdHandler : IRequestHandler<GetTimelineItemByStreetcodeIdQuery, Result<IEnumerable<TimelineItemDTO>>>
 {
     private readonly IMapper _mapper;
     private readonly IRepositoryWrapper _repositoryWrapper;
@@ -17,17 +17,17 @@ public class GetTimelineItemByStreetcodeIdHandler : IRequestHandler<GetTimelineI
         _mapper = mapper;
     }
 
-    public async Task<Result<TimelineItemDTO>> Handle(GetTimelineItemByStreetcodeIdQuery request, CancellationToken cancellationToken)
+    public async Task<Result<IEnumerable<TimelineItemDTO>>> Handle(GetTimelineItemByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
         var timelineItem = await _repositoryWrapper.TimelineRepository
-            .GetSingleOrDefaultAsync(f => f.Streetcodes.Any(s => s.Id == request.StreetcodeId));
+            .GetAllAsync(f => f.Streetcodes.Any(s => s.Id == request.StreetcodeId));
 
         if (timelineItem is null)
         {
             return Result.Fail(new Error($"Cannot find a timelineItem by a streetcode Id: {request.StreetcodeId}"));
         }
 
-        var timelineItemDto = _mapper.Map<TimelineItemDTO>(timelineItem);
+        var timelineItemDto = _mapper.Map<IEnumerable<TimelineItemDTO>>(timelineItem);
         return Result.Ok(timelineItemDto);
     }
 }
--- Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetByStreetcodeId/GetTimelineItemByStreetcodeIdQuery.cs
+++ Streetcode/Streetcode.BLL/MediatR/Timeline/TimelineItem/GetByStreetcodeId/GetTimelineItemByStreetcodeIdQuery.cs
@@ -4,4 +4,4 @@ using Streetcode.BLL.DTO.Timeline;
 
 namespace Streetcode.BLL.MediatR.Timeline.TimelineItem.GetByStreetcodeId;
 
-public record GetTimelineItemByStreetcodeIdQuery(int StreetcodeId) : IRequest<Result<TimelineItemDTO>>;
+public record GetTimelineItemByStreetcodeIdQuery(int StreetcodeId) : IRequest<Result<IEnumerable<TimelineItemDTO>>>;

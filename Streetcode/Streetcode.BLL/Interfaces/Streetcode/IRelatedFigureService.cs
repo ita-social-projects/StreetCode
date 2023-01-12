fd8a3ccb923019fd69273844b02b26305917c392
dimasster <dmytrobuchkovsky@gmail.com>
2023-01-12 23:52:58 +0200

    add related figure service structure

---
+++ Streetcode/Streetcode.BLL/Interfaces/Streetcode/IRelatedFigureService.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using Streetcode.BLL.DTO.Streetcode;
+
+namespace Streetcode.BLL.Interfaces.Streetcode;
+
+public interface IRelatedFigureService
+{
+    public Task<IEnumerable<RelatedFigureDTO>> GetRelatedFiguresByStreetcodeId(int streetcodeId);
+}

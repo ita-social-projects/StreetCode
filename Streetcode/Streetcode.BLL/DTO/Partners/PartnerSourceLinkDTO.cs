c043951e7150b12495f82a5b000f66c8ae855bc9
dimasster <dmytrobuchkovsky@gmail.com>
2023-02-01 16:41:23 +0200

    convert title and utlhref to url prop

--- Streetcode/Streetcode.BLL/DTO/Partners/PartnerSourceLinkDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Partners/PartnerSourceLinkDTO.cs
@@ -5,7 +5,6 @@ namespace Streetcode.BLL.DTO.Partners;
 public class PartnerSourceLinkDTO
 {
     public int Id { get; set; }
-    public string Title { get; set; }
     public LogoTypeDTO LogoTpe { get; set; }
-    public string TargetUrl { get; set; }
+    public UrlDTO TargetUrl { get; set; }
 }

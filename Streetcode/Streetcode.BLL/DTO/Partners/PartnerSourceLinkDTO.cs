a163de7c17f708579d35438101d5c58b54817c3d
dimasster <dmytrobuchkovsky@gmail.com>
2023-02-01 22:23:06 +0200

    fix prop name

--- Streetcode/Streetcode.BLL/DTO/Partners/PartnerSourceLinkDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Partners/PartnerSourceLinkDTO.cs
@@ -5,6 +5,6 @@ namespace Streetcode.BLL.DTO.Partners;
 public class PartnerSourceLinkDTO
 {
     public int Id { get; set; }
-    public LogoTypeDTO LogoTpe { get; set; }
+    public LogoTypeDTO LogoType { get; set; }
     public UrlDTO TargetUrl { get; set; }
 }

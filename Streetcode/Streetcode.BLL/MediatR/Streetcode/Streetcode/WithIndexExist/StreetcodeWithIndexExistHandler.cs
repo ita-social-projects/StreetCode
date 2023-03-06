6f3b7a4c6febc84fa0fff43671a972ebfc36ea60
unknown <nadia.kischyk2003@gmail.com>
2023-03-06 22:41:12 +0200

    add new endpoint for checking if streetcode with index exist

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/WithIndexExist/StreetcodeWithIndexExistHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.WithIndexExist
+{
+    public class StreetcodeWithIndexExistHandler : IRequestHandler<StreetcodeWithIndexExistQuery, Result<bool>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepositoryWrapper _repositoryWrapper;
+
+        public StreetcodeWithIndexExistHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<bool>> Handle(StreetcodeWithIndexExistQuery request, CancellationToken cancellationToken)
+        {
+            var streetcode = await _repositoryWrapper.StreetcodeRepository.GetFirstOrDefaultAsync(s => s.Index == request.index);
+            if (streetcode == null)
+            {
+                return Result.Ok(false);
+            }
+
+            return Result.Ok(true);
+        }
+    }
+}

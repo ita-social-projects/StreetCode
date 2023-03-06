5d72c7bcc038f7217433100cd1ed76b37593577a
Slava <kasterov.v@gmail.com>
2023-03-06 10:18:51 +0200

    SoftDeletingUtils added

---
+++ Streetcode/Streetcode.WebApi/Utils/SoftDeletingUtils.cs
@@ -0,0 +1,43 @@
+﻿using Streetcode.DAL.Enums;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Microsoft.EntityFrameworkCore;
+namespace Streetcode.WebApi.Utils;
+
+public class SoftDeletingUtils
+{
+    private readonly IRepositoryWrapper _repositoryWrapper;
+
+    public SoftDeletingUtils(IRepositoryWrapper repository)
+    {
+        _repositoryWrapper = repository;
+    }
+
+    public async Task DeleteStreetcodeWithStageDeleted(int daysLife)
+    {
+        var streetcodes = await _repositoryWrapper.StreetcodeRepository
+            .GetAllAsync(
+            predicate: s => s.Status == StreetcodeStatus.Deleted && s.UpdatedAt.AddDays(daysLife) <= DateTime.Now,
+            include: s => s.Include(x => x.Observers)
+                           .Include(x => x.Targets));
+
+        if (streetcodes is null || streetcodes.Count() == 0)
+        {
+            Console.WriteLine("SoftDeletingUtils triggered: Nothin to delete!");
+            return;
+        }
+
+        foreach (var streetcode in streetcodes)
+        {
+            _repositoryWrapper.StreetcodeRepository.Delete(streetcode);
+
+            var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
+
+            if (!resultIsSuccess)
+            {
+                throw new Exception("Failed to delete a streetcode");
+            }
+
+            Console.WriteLine($"SoftDeletingUtils triggered: {streetcodes.Count()} streetcodes deleted!");
+        }
+    }
+}

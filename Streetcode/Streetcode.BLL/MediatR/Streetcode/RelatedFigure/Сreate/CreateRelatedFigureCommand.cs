21f9119ba34443bf78fb67c0177a23545d4bc6b2
Katherine Derxen <92515141+Katerix@users.noreply.github.com>
2023-03-07 19:56:45 +0200

    Update CreateRelatedFigureCommand.cs

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedFigure/Сreate/CreateRelatedFigureCommand.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedFigure/Сreate/CreateRelatedFigureCommand.cs
@@ -5,4 +5,4 @@ using Streetcode.BLL.DTO.Streetcode.TextContent;
 
 namespace Streetcode.BLL.MediatR.Streetcode.RelatedFigure.Create;
 
-public record CreateRelatedFigureCommand(int Id1, int Id2) : IRequest<Result<Unit>>;
+public record CreateRelatedFigureCommand(int ObserverId, int TargetId) : IRequest<Result<Unit>>;

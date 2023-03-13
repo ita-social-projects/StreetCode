a45d2f05b29fbcb0f84c8c80c262ea4e00078992
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-03-13 22:32:52 +0200

    Related Terms DAL Layer
    Completed.

--- Streetcode/Streetcode.DAL/Entities/Streetcode/TextContent/Term.cs
+++ Streetcode/Streetcode.DAL/Entities/Streetcode/TextContent/Term.cs
@@ -7,6 +7,7 @@ namespace Streetcode.DAL.Entities.Streetcode.TextContent;
 public class Term
 {
     [Key]
+
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
@@ -16,4 +17,6 @@ public class Term
 
     [Required]
     public string Description { get; set; }
+
+    public List<RelatedTerm> RelatedTerms { get; set; } = new();
 }

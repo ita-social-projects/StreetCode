66080ddfdedc624f93ae23d974c232beadc856c5
Valerii Dekhtiuk <dekhtiuk.v.s@gmail.com>
2023-03-29 00:44:32 +0300

    start

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedTerm/Create/CreateRelatedTermHandlerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
+{
+    internal class CreateRelatedTermHandlerTests
+    {
+    }
+}

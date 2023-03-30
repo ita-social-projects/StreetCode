bdb379e17c51de4e53bd6ad5de47cc3ce51b36fb
unknown <nadia.kischyk2003@gmail.com>
2023-03-30 08:25:07 +0300

    add new user classes added dto, user entity, controller

---
+++ Streetcode/Streetcode.DAL/Repositories/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.DAL.Repositories.Interfaces.Users
+{
+    internal class IUserRepositories
+    {
+    }
+}
---
+++ Streetcode/Streetcode.WebApi/Controllers/Users/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Streetcode.WebApi.Controllers.Users
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

56917c4a171a585b21276525cabe06bfdcb0b8b9
Lolimkeri <demobor96@gmail.com>
2023-04-06 20:35:17 +0300

    Add tests for Partners

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Partners/GetParnerByStreetcodeIdTest.cs
@@ -0,0 +1,183 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.Partners;
+using Streetcode.BLL.MediatR.Partners.GetByStreetcodeId;
+using Streetcode.DAL.Entities.Partners;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Partners;
+
+public class GetParnerByStreetcodeIdTest
+{
+    private Mock<IRepositoryWrapper> _mockRepository;
+    private Mock<IMapper> _mockMapper;
+
+    public GetParnerByStreetcodeIdTest()
+    {
+        _mockRepository = new Mock<IRepositoryWrapper>();
+        _mockMapper = new Mock<IMapper>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_ExistingId()
+    {
+        //Arrange
+        var testStreetcodeContent = GetStreetcodeList().First();
+
+        _mockRepository.Setup(x => x.StreetcodeRepository
+            .GetSingleOrDefaultAsync(
+                It.IsAny<Expression<Func<StreetcodeContent, bool>>>(),
+                null))
+            .ReturnsAsync(testStreetcodeContent);
+
+        _mockRepository.Setup(x => x.PartnersRepository
+              .GetAllAsync(
+                  It.IsAny<Expression<Func<Partner, bool>>>(),
+                    It.IsAny<Func<IQueryable<Partner>,
+              IIncludableQueryable<Partner, object>>>()))
+              .ReturnsAsync(GetPartnerList());
+
+        _mockMapper
+            .Setup(x => x
+            .Map<IEnumerable<PartnerDTO>>(It.IsAny<IEnumerable<Partner>>()))
+            .Returns(GetPartnerDTOList());
+
+        var handler = new GetPartnersByStreetcodeIdHandler(_mockMapper.Object, _mockRepository.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnersByStreetcodeIdQuery(testStreetcodeContent.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.True(result.IsSuccess),
+            () => Assert.NotEmpty(result.Value)
+        );
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_CorrectType()
+    {
+        //Arrange
+        var testStreetcodeContent = GetStreetcodeList().First();
+
+        _mockRepository.Setup(x => x.StreetcodeRepository
+            .GetSingleOrDefaultAsync(
+                It.IsAny<Expression<Func<StreetcodeContent, bool>>>(),
+                null))
+            .ReturnsAsync(testStreetcodeContent);
+
+        _mockRepository.Setup(x => x.PartnersRepository
+              .GetAllAsync(
+                  It.IsAny<Expression<Func<Partner, bool>>>(),
+                    It.IsAny<Func<IQueryable<Partner>,
+              IIncludableQueryable<Partner, object>>>()))
+              .ReturnsAsync(GetPartnerList());
+
+        _mockMapper
+            .Setup(x => x
+            .Map<IEnumerable<PartnerDTO>>(It.IsAny<IEnumerable<Partner>>()))
+            .Returns(GetPartnerDTOList());
+
+        var handler = new GetPartnersByStreetcodeIdHandler(_mockMapper.Object, _mockRepository.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnersByStreetcodeIdQuery(testStreetcodeContent.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.True(result.IsSuccess),
+            () => Assert.IsType<List<PartnerDTO>>(result.ValueOrDefault)
+        );
+    }
+
+    [Fact]
+    public async Task ShouldThrowError_IdNotExist()
+    {
+        //Arrange
+        var testStreetcodeContent = GetStreetcodeList().First();
+        var expectedError = $"Cannot find a coordinates by a streetcode id: {testStreetcodeContent.Id}";
+
+        _mockRepository.Setup(x => x.StreetcodeRepository
+            .GetSingleOrDefaultAsync(
+                It.IsAny<Expression<Func<StreetcodeContent, bool>>>(),
+                null))
+            .ReturnsAsync(testStreetcodeContent);
+
+        _mockRepository.Setup(x => x.PartnersRepository
+              .GetAllAsync(
+                  It.IsAny<Expression<Func<Partner, bool>>>(),
+                    It.IsAny<Func<IQueryable<Partner>,
+              IIncludableQueryable<Partner, object>>>()))
+              .ReturnsAsync(GetPartnerListWithNotExistingStreetcodeId());
+
+        _mockMapper
+            .Setup(x => x
+            .Map<IEnumerable<PartnerDTO>>(It.IsAny<IEnumerable<Partner>>()))
+            .Returns(GetPartnerDTOListWithNotExistingId());
+
+        var handler = new GetPartnersByStreetcodeIdHandler(_mockMapper.Object, _mockRepository.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnersByStreetcodeIdQuery(testStreetcodeContent.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.True(result.IsFailed),
+            () => Assert.Equal(expectedError, result.Errors.First().Message)
+        );
+    }
+
+    private static IEnumerable<Partner> GetPartnerList()
+    {
+        var partners = new List<Partner>
+        {
+            new Partner
+            {
+                Id = 1,
+                Streetcodes = GetStreetcodeList()
+            },
+        };
+
+        return partners;
+    }
+
+    private static List<StreetcodeContent> GetStreetcodeList()
+    {
+        var streetCodes = new List<StreetcodeContent>
+        {
+            new StreetcodeContent
+            {
+                Id = 1
+            },
+        };
+
+        return streetCodes;
+    }
+    private static List<Partner>? GetPartnerListWithNotExistingStreetcodeId()
+    {
+        return null;
+    }
+    private static List<PartnerDTO>? GetPartnerDTOListWithNotExistingId()
+    {
+        return null;
+    }
+    private static List<PartnerDTO> GetPartnerDTOList()
+    {
+        var partners = new List<PartnerDTO>
+        {
+            new PartnerDTO
+            {
+                Id = 1
+            },
+        };
+
+        return partners;
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Partners/GetPartnerByIdTest.cs
@@ -0,0 +1,141 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.Partners;
+using Streetcode.BLL.MediatR.Partners.GetById;
+using Streetcode.DAL.Entities.Partners;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Partners;
+
+public class GetPartnerByIdTest
+{
+    private Mock<IRepositoryWrapper> _mockRepository;
+    private Mock<IMapper> _mockMapper;
+
+    public GetPartnerByIdTest() {
+        _mockMapper = new Mock<IMapper>();
+        _mockRepository = new Mock<IRepositoryWrapper>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_ExistingId()
+    {
+        //Arrange
+        var testPartner = GetPartner();
+
+        _mockRepository.Setup(x => x.PartnersRepository
+            .GetSingleOrDefaultAsync(
+               It.IsAny<Expression<Func<Partner, bool>>>(),
+                It.IsAny<Func<IQueryable<Partner>,
+                IIncludableQueryable<Partner, object>>>()))
+            .ReturnsAsync(testPartner);
+
+        _mockMapper
+            .Setup(x => x
+            .Map<PartnerDTO>(It.IsAny<Partner>()))
+            .Returns(GetPartnerDTO());
+
+        var handler = new GetPartnerByIdHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnerByIdQuery(testPartner.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.True(result.IsSuccess),
+            () => Assert.Equal(result.Value.Id, testPartner.Id)
+        );
+    }
+
+    [Fact]
+    public async Task ShouldReturnErrorResponse_NotExistingId()
+    {
+        //Arrange
+        var testPartner = GetPartner();
+        var expectedError = $"Cannot find any partner with corresponding id: {testPartner.Id}";
+
+        _mockRepository.Setup(x => x.PartnersRepository
+            .GetSingleOrDefaultAsync(
+               It.IsAny<Expression<Func<Partner, bool>>>(),
+                It.IsAny<Func<IQueryable<Partner>,
+                IIncludableQueryable<Partner, object>>>()))
+            .ReturnsAsync(GetPartnerWithNotExistingId());
+
+        _mockMapper
+            .Setup(x => x
+            .Map<PartnerDTO>(It.IsAny<Partner>()))
+            .Returns(GetPartnerDTOWithNotExistingId());
+
+        var handler = new GetPartnerByIdHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnerByIdQuery(testPartner.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.True(result.IsFailed),
+            () => Assert.Equal(expectedError, result.Errors.First().Message)
+        );
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_CorrectType()
+    {
+        //Arrange
+        var testPartner = GetPartner();
+
+        _mockRepository.Setup(x => x.PartnersRepository
+            .GetSingleOrDefaultAsync(
+               It.IsAny<Expression<Func<Partner, bool>>>(),
+                It.IsAny<Func<IQueryable<Partner>,
+                IIncludableQueryable<Partner, object>>>()))
+            .ReturnsAsync(testPartner);
+
+        _mockMapper
+            .Setup(x => x
+            .Map<PartnerDTO>(It.IsAny<Partner>()))
+            .Returns(GetPartnerDTO());
+
+        var handler = new GetPartnerByIdHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetPartnerByIdQuery(testPartner.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result.ValueOrDefault),
+            () => Assert.IsType<PartnerDTO>(result.ValueOrDefault)
+        );
+    }
+
+    private static Partner GetPartner()
+    {
+        return new Partner
+        {
+            Id = 1
+        };
+    }
+    private static Partner? GetPartnerWithNotExistingId()
+    {
+        return null;
+    }
+
+    private static PartnerDTO GetPartnerDTO()
+    {
+        return new PartnerDTO
+        {
+            Id = 1
+        };
+    }
+
+    private static PartnerDTO? GetPartnerDTOWithNotExistingId()
+    {
+        return null;
+    }
+}

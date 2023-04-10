650bf5446862c9ab1e285b17f4c30545af7e0925
Slava <kasterov.v@gmail.com>
2023-04-10 13:04:31 +0300

    Second part
    Now we have separate console app in order to launch sql scripts

---
+++ Streetcode/DbSeeding/Program.cs
@@ -0,0 +1,41 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    static int Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        var upgrader =
+            DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsFromFileSystem(@"..\..\..\..\Streetcode.DAL\Persistence\ScriptsSeeding\")
+                .LogToConsole()
+                .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return -1;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Success!");
+        Console.ResetColor();
+        return 0;
+    }
+}

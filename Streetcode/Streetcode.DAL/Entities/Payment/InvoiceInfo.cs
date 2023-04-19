ef09189aa1224889c428fb71659797ad907135eb
Kateryna Derxen <92515141+Katerix@users.noreply.github.com>
2023-04-19 15:02:39 +0300

    Update InvoiceInfo.cs

--- Streetcode/Streetcode.DAL/Entities/Payment/InvoiceInfo.cs
+++ Streetcode/Streetcode.DAL/Entities/Payment/InvoiceInfo.cs
@@ -1,14 +1,9 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Streetcode.DAL.Entities.Payment
 {
     public class InvoiceInfo
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InvoiceInfo"/> class.
-        /// </summary>
-        /// <param name="invoiceId">The unique identifier of the invoice.</param>
-        /// <param name="pageUrl">The page URL to pay the invoice.</param>
         [JsonConstructor]
         public InvoiceInfo(string invoiceId, string pageUrl)
         {
@@ -16,15 +11,9 @@ namespace Streetcode.DAL.Entities.Payment
             PageUrl = pageUrl;
         }
 
-        /// <summary>
-        /// Gets the unique identifier of the invoice.
-        /// </summary>
         [JsonProperty("invoiceId")]
         public string InvoiceId { get; }
 
-        /// <summary>
-        /// Gets the page URL to pay the invoice.
-        /// </summary>
         [JsonProperty("pageUrl")]
         public string PageUrl { get; }
     }

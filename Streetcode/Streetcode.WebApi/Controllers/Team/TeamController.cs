9aaa286fe09dccfe467c496659e9fa71ea2181a9
ormykhalyshyn <or.mykhalyshyn@gmail.com>
2023-05-09 00:41:47 +0300

    commit

---
+++ Streetcode/Streetcode.WebApi/Controllers/Team/TeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Streetcode.WebApi.Controllers.Team
+{
+    public class TeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

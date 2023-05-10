2e908f8e3a0d40ea566c06985ae886221d5fea95
ormykhalyshyn <or.mykhalyshyn@gmail.com>
2023-05-10 23:07:09 +0300

    commit

--- Streetcode/Streetcode.BLL/DTO/Team/TeamMemberLinkDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Team/TeamMemberLinkDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Streetcode.BLL.DTO.Partners;
 
 namespace Streetcode.BLL.DTO.Team
 {

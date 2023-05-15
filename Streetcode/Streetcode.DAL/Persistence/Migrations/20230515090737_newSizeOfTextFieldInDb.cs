61c94046def9e3ca3bdb08de596d2b50a6c52692
ValDekh <smithdekh@gmail.com>
2023-05-15 12:09:01 +0300

    LastChangesWithTextFieldsSize

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20230515090737_newSizeOfTextFieldInDb.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    public partial class newSizeOfTextFieldInDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TextContent",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldMaxLength: 5000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalText",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TextContent",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(max)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalText",
+                schema: "streetcode",
+                table: "texts",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}

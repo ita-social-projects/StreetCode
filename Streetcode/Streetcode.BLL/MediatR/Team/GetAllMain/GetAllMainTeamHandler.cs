3846149cc0591bea9149dd5402043c8ca9233d7a
ormykhalyshyn <or.mykhalyshyn@gmail.com>
2023-05-17 12:08:58 +0300

    added new end-point for team

--- Streetcode/Streetcode.BLL/MediatR/Team/GetAllMain/GetAllMainTeamHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Team/GetAllMain/GetAllMainTeamHandler.cs
@@ -7,22 +7,22 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Team.GetAll
 {
-    public class GetAllTeamHandler : IRequestHandler<GetAllTeamQuery, Result<IEnumerable<TeamMemberDTO>>>
+    public class GetAllMainTeamHandler : IRequestHandler<GetAllMainTeamQuery, Result<IEnumerable<TeamMemberDTO>>>
     {
         private readonly IMapper _mapper;
         private readonly IRepositoryWrapper _repositoryWrapper;
 
-        public GetAllTeamHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper)
+        public GetAllMainTeamHandler(IRepositoryWrapper repositoryWrapper, IMapper mapper)
         {
             _repositoryWrapper = repositoryWrapper;
             _mapper = mapper;
         }
 
-        public async Task<Result<IEnumerable<TeamMemberDTO>>> Handle(GetAllTeamQuery request, CancellationToken cancellationToken)
+        public async Task<Result<IEnumerable<TeamMemberDTO>>> Handle(GetAllMainTeamQuery request, CancellationToken cancellationToken)
         {
             var team = await _repositoryWrapper
                 .TeamRepository
-                .GetAllAsync(include: x => x.Include(x => x.Positions).Include(x => x.TeamMemberLinks));
+                .GetAllAsync(include: x => x.Where(x => x.IsMain).Include(x => x.Positions).Include(x => x.TeamMemberLinks));
 
             if (team is null)
             {

dd4e6a1bc2aef60e25c7b28cbf489f89482a2e3a
Danyil Terentiev <96494594+DanyilTerentiev@users.noreply.github.com>
2023-05-18 23:53:28 +0300

    Update NewsProfile.cs

--- Streetcode/Streetcode.BLL/Mapping/Newss/NewsProfile.cs
+++ Streetcode/Streetcode.BLL/Mapping/Newss/NewsProfile.cs
@@ -8,15 +8,6 @@ namespace Streetcode.BLL.Mapping.Newss
     {
         public NewsProfile()
         {
-            CreateMap<NewsDTO, News>()
-                .ForMember(dest => dest.Id, opt => opt.Ignore())
-                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Image))
-                .ForMember(dest => dest.ImageId, opt => opt.MapFrom(src => src.ImageId))
-                .ForMember(dest => dest.URL, opt => opt.MapFrom(src => src.URL))
-                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Text))
-                .ForMember(dest => dest.CreationDate, opt => opt.Ignore());
-
             CreateMap<News, NewsDTO>().ReverseMap();
         }
     }

54d2c4c467dfc404dea861515ecc26d0583251f4
unknown <nadia.kischyk2003@gmail.com>
2023-05-21 18:23:06 +0300

    addNullResultToTextQuery

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/GetByStreetcodeId/GetTextByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/GetByStreetcodeId/GetTextByStreetcodeIdHandler.cs
@@ -2,11 +2,13 @@
 using FluentResults;
 using MediatR;
 using Streetcode.BLL.DTO.Streetcode.TextContent;
+using Streetcode.BLL.DTO.Transactions;
+using Streetcode.BLL.MediatR.ResultVariations;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Text.GetByStreetcodeId;
 
-public class GetTextByStreetcodeIdHandler : IRequestHandler<GetTextByStreetcodeIdQuery, Result<TextDTO>>
+public class GetTextByStreetcodeIdHandler : IRequestHandler<GetTextByStreetcodeIdQuery, Result<TextDTO?>>
 {
     private readonly IMapper _mapper;
     private readonly IRepositoryWrapper _repositoryWrapper;
@@ -17,16 +19,23 @@ public class GetTextByStreetcodeIdHandler : IRequestHandler<GetTextByStreetcodeI
         _mapper = mapper;
     }
 
-    public async Task<Result<TextDTO>> Handle(GetTextByStreetcodeIdQuery request, CancellationToken cancellationToken)
+    public async Task<Result<TextDTO?>> Handle(GetTextByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
-        var text = await _repositoryWrapper.TextRepository.GetFirstOrDefaultAsync(text => text.StreetcodeId == request.StreetcodeId);
+        var text = await _repositoryWrapper.TextRepository
+            .GetFirstOrDefaultAsync(text => text.StreetcodeId == request.StreetcodeId);
 
         if (text is null)
         {
-            return Result.Fail(new Error($"Cannot find any text by the streetcode id: {request.StreetcodeId}"));
+            if (await _repositoryWrapper.StreetcodeRepository
+                 .GetFirstOrDefaultAsync(s => s.Id == request.StreetcodeId) == null)
+            {
+                return Result.Fail(new Error($"Cannot find a transaction link by a streetcode id: {request.StreetcodeId}, because such streetcode doesn`t exist"));
+            }
         }
 
-        var textDto = _mapper.Map<TextDTO>(text);
-        return Result.Ok(textDto);
+        var textDto = _mapper.Map<TextDTO?>(text);
+        NullResult<TextDTO?> result = new NullResult<TextDTO?>();
+        result.WithValue(_mapper.Map<TextDTO?>(text));
+        return result;
     }
 }

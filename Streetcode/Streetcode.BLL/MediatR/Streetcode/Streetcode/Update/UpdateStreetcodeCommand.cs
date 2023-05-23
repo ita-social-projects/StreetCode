e0786a7455fb8f774e46c9649e5e53baba26d9ef
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-05-23 14:56:06 +0300

    beginning of development

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/Update/UpdateStreetcodeCommand.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.Update;
+using Streetcode.DAL.Entities.Streetcode;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.Update
+{
+	public record class UpdateStreetcodeCommand(StreetcodeUpdateDTO Streetcode) : IRequest<Result<StreetcodeUpdateDTO>>;
+}
---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/Update/UpdateStreetcodeHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.Update;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.Create;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.Update
+{
+	internal class UpdateStreetcodeHandler : IRequestHandler<UpdateStreetcodeCommand, Result<StreetcodeUpdateDTO>>
+	{
+		private readonly IMapper _mapper;
+		private readonly IRepositoryWrapper _repositoryWrapper;
+
+		public UpdateStreetcodeHandler(IMapper mapper, IRepositoryWrapper repositoryWrapper)
+		{
+			_mapper = mapper;
+			_repositoryWrapper = repositoryWrapper;
+		}
+
+		public Task<Result<StreetcodeUpdateDTO>> Handle(UpdateStreetcodeCommand request, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

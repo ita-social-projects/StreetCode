e38879c95f152ba24a73d44350da969452c98e89
Kateryna <92515141+Katerix@users.noreply.github.com>
2023-05-26 00:06:51 +0300

    some fixes

--- Streetcode/Streetcode.BLL/MediatR/Instagram/GetAll/GetAllPostsHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Instagram/GetAll/GetAllPostsHandler.cs
@@ -1,11 +1,11 @@
 ﻿using FluentResults;
+using MediatR;
 using Streetcode.BLL.Interfaces.Instagram;
-using Streetcode.BLL.MediatR.Payment;
-using Streetcode.DAL.Entities.Payment;
+using Streetcode.DAL.Entities.Instagram;
 
 namespace Streetcode.BLL.MediatR.Instagram.GetAll
 {
-    public class GetAllPostsHandler
+    public class GetAllPostsHandler : IRequestHandler<GetAllPostsQuery, Result<IEnumerable<InstagramPost>>>
     {
         private const int _limit = 10;
         private readonly IInstagramService _instagramService;
@@ -14,7 +14,7 @@ namespace Streetcode.BLL.MediatR.Instagram.GetAll
             _instagramService = instagramService;
         }
 
-        public async Task<Result<InvoiceInfo>> Handle(GetAllPostsHandler request, CancellationToken cancellationToken)
+        public async Task<Result<IEnumerable<InstagramPost>>> Handle(GetAllPostsQuery request, CancellationToken cancellationToken)
         {
             var result = await _instagramService.GetPostsAsync();
 
@@ -22,24 +22,3 @@ namespace Streetcode.BLL.MediatR.Instagram.GetAll
         }
     }
 }
-
-/*public class CreateInvoiceHandler : IRequestHandler<CreateInvoiceCommand, Result<InvoiceInfo>>
-    {
-        private const int _hryvnyaCurrencyCode = 980;
-        private const int _currencyMultiplier = 100;
-        private readonly IPaymentService _paymentService;
-
-        public CreateInvoiceHandler(IPaymentService paymentService)
-        {
-            _paymentService = paymentService;
-        }
-
-        public async Task<Result<InvoiceInfo>> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
-        {
-            var invoice = new Invoice(request.Payment.Amount * _currencyMultiplier, _hryvnyaCurrencyCode, new MerchantPaymentInfo { Destination = "Добровільний внесок на статутну діяльність ГО «Історична Платформа»" }, request.Payment.RedirectUrl);
-
-            var result = await _paymentService.CreateInvoiceAsync(invoice);
-
-            return Result.Ok(result);
-        }
-    }*/
--- Streetcode/Streetcode.WebApi/Controllers/Instagram/InstagramController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Instagram/InstagramController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.MediatR.Instagram.GetAll;
+using Streetcode.BLL.MediatR.Payment;
+using Streetcode.BLL.MediatR.Team.Position.GetAll;
+using Streetcode.DAL.Entities.Instagram;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,13 +11,13 @@ namespace Streetcode.WebApi.Controllers.Instagram
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class InstagramController : ControllerBase
+    public class InstagramController : BaseApiController
     {
-        // GET: api/<InstagramController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("/GetPosts")]
+        public async Task<IActionResult> GetAllAsync()
         {
-            return new string[] { "value1", "value2" };
+            return HandleResult(await Mediator.Send(new GetAllPostsQuery()));
         }
     }
 }

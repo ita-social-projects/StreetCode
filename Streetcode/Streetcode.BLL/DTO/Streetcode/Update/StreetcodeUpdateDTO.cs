a5802ee91e603f69be12e502836699366c36708e
Kuzmych Roman <67451349+Dobriyr@users.noreply.github.com>
2023-06-01 11:58:40 +0300

    remove comment

--- Streetcode/Streetcode.BLL/DTO/Streetcode/Update/StreetcodeUpdateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/Update/StreetcodeUpdateDTO.cs
@@ -25,14 +25,11 @@ namespace Streetcode.BLL.DTO.Streetcode.Update
 		public DateTime? EventEndOrPersonDeathDate { get; set; }
 		public TextUpdateDTO Text { get; set; }
 		public IEnumerable<StreetcodeToponymUpdateDTO>? StreetcodeToponym { get; set; }
-
-		// public
-
-		// public List<SubtitleUpdateDTO> Subtitles { get; set; }
-		// public IEnumerable<FactUpdateDTO> Facts { get; set; }
-		// public IEnumerable<VideoUpdateDTO> Videos { get; set; }
-		// public IEnumerable<RelatedFigureUpdateDTO> RelatedFigures { get; set; }
-		// public IEnumerable<PartnersUpdateDTO> Partners { get; set; }
-		// public IEnumerable<TimelineItemUpdateDTO> TimelineItems { get; set; }
+		public List<SubtitleUpdateDTO> Subtitles { get; set; }
+		public IEnumerable<FactUpdateDTO> Facts { get; set; }
+		public IEnumerable<VideoUpdateDTO> Videos { get; set; }
+		public IEnumerable<RelatedFigureUpdateDTO> RelatedFigures { get; set; }
+		public IEnumerable<PartnersUpdateDTO> Partners { get; set; }
+		public IEnumerable<TimelineItemUpdateDTO> TimelineItems { get; set; }
 	}
 }

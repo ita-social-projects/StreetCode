0fe863fe616129e2e6592e3b183dadfececc3154
Dmytro Hranovskyi <wehfis@gmail.com>
2023-06-13 22:16:37 +0300

    Added "DeleteTeam" "GetAllTeams" "GetByIdTeam" Tests, with appropriate logic, all tests ran successfully

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Teams/DeleteTeamTest.cs
@@ -0,0 +1,113 @@
+﻿using AutoMapper;
+using Moq;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.MediatR.Team.Delete;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Teams;
+
+public class DeleteTeamTest
+{
+    private Mock<IMapper> _mockMapper;
+    private Mock<IRepositoryWrapper> _mockRepository;
+
+    public DeleteTeamTest()
+    {
+        _mockRepository = new Mock<IRepositoryWrapper>();
+        _mockMapper = new Mock<IMapper>();
+    }
+
+    [Fact]
+    public async Task ShouldDeleteSuccessfully()
+    {
+        //Arrange
+        var testTeam = GetTeam();
+
+        _mockMapper.Setup(x => x.Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
+            .Returns(GetTeamDTO());
+
+        _mockRepository.Setup(x => x.TeamRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<TeamMember, bool>>>(), null))
+            .ReturnsAsync(testTeam);
+
+        var handler = new DeleteTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new DeleteTeamQuery(testTeam.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.True(result.IsSuccess)
+        );
+
+        _mockRepository.Verify(x => x.TeamRepository.Delete(It.Is<TeamMember>(x => x.Id == testTeam.Id)), Times.Once);
+        _mockRepository.Verify(x => x.SaveChanges(), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldThrowExeption_IdNotExisting()
+    {
+        //Arrange
+        var testTeam = GetTeam();
+        var expectedError = "No team with such id";
+
+        _mockRepository.Setup(x => x.TeamRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<TeamMember, bool>>>(), null))
+            .ReturnsAsync(GetTeamWithNotExistingId());
+
+        //Act
+        var handler = new DeleteTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        var result = await handler.Handle(new DeleteTeamQuery(testTeam.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Equal(expectedError, result.Errors.First().Message);
+
+        _mockRepository.Verify(x => x.TeamRepository.Delete(It.IsAny<TeamMember>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task ShouldThrowExeption_SaveChangesAsyncIsNotSuccessful()
+    {
+        //Arrange
+        var testTeam = GetTeam();
+        var expectedError = "The team wasn`t added";
+
+        _mockMapper.Setup(x => x.Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
+            .Returns(GetTeamDTO());
+
+        _mockRepository.Setup(x => x.TeamRepository.GetFirstOrDefaultAsync(It.IsAny<Expression<Func<TeamMember, bool>>>(), null))
+            .ReturnsAsync(testTeam);
+        _mockRepository.Setup(x => x.SaveChanges())
+            .Throws(new Exception(expectedError));
+
+        //Act
+        var handler = new DeleteTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        var result = await handler.Handle(new DeleteTeamQuery(testTeam.Id), CancellationToken.None);
+
+        //Assert
+        Assert.Equal(expectedError, result.Errors.First().Message);
+    }
+
+    private static TeamMember GetTeam()
+    {
+        return new TeamMember
+        {
+            Id = 1
+        };
+    }
+
+    private static TeamMemberDTO GetTeamDTO()
+    {
+        return new TeamMemberDTO();
+    }
+
+    private static TeamMember? GetTeamWithNotExistingId()
+    {
+        return null;
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Teams/GetAllTeamsTest.cs
@@ -0,0 +1,137 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.MediatR.Team.GetAll;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.DAL.Entities.Streetcode.TextContent;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Teams;
+
+public class GetAllTeamTest
+{
+    private Mock<IRepositoryWrapper> _mockRepository;
+    private Mock<IMapper> _mockMapper;
+
+    public GetAllTeamTest()
+    {
+        _mockRepository = new Mock<IRepositoryWrapper>();
+        _mockMapper = new Mock<IMapper>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_CorrectType()
+    {
+        //Arrange
+        _mockRepository.Setup(x => x.TeamRepository.GetAllAsync(
+            null,
+            It.IsAny<Func<IQueryable<TeamMember>, IIncludableQueryable<TeamMember, object>>>()))
+            .ReturnsAsync(GetTeamList());
+
+        _mockMapper
+            .Setup(x => x.Map<IEnumerable<TeamMemberDTO>>(It.IsAny<IEnumerable<TeamMember>>()))
+            .Returns(GetListTeamDTO());
+
+        var handler = new GetAllTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetAllTeamQuery(), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.IsType<List<TeamMemberDTO>>(result.ValueOrDefault)
+        );
+    }
+
+    [Fact]
+    public async Task ShouldReturnSuccessfully_CountMatch()
+    {
+        //Arrange
+        _mockRepository.Setup(x => x.TeamRepository.GetAllAsync(
+            null,
+            It.IsAny<Func<IQueryable<TeamMember>, IIncludableQueryable<TeamMember, object>>>()))
+            .ReturnsAsync(GetTeamList());
+
+        _mockMapper
+            .Setup(x => x
+            .Map<IEnumerable<TeamMemberDTO>>(It.IsAny<IEnumerable<TeamMember>>()))
+            .Returns(GetListTeamDTO());
+
+        var handler = new GetAllTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetAllTeamQuery(), CancellationToken.None);
+
+        //Assert
+        Assert.Multiple(
+            () => Assert.NotNull(result),
+            () => Assert.Equal(GetTeamList().Count(), result.Value.Count())
+        );
+    }
+
+    [Fact]
+    public async Task ShouldThrowExeption_IdNotExist()
+    {
+        //Arrange
+        var expectedError = "Cannot find any team";
+
+        _mockRepository.Setup(x => x.TeamRepository.GetAllAsync(
+            null,
+            It.IsAny<Func<IQueryable<TeamMember>, IIncludableQueryable<TeamMember, object>>>()))
+            .ReturnsAsync(GetTeamListWithNotExistingId());
+
+        var handler = new GetAllTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+        //Act
+        var result = await handler.Handle(new GetAllTeamQuery(), CancellationToken.None);
+
+        //Assert
+        Assert.Equal(expectedError, result.Errors.First().Message);
+
+        _mockMapper.Verify(x => x.Map<IEnumerable<TeamMemberDTO>>(It.IsAny<IEnumerable<TeamMember>>()), Times.Never);
+    }
+
+    private static IEnumerable<TeamMember> GetTeamList()
+    {
+        var team_members = new List<TeamMember>
+        {
+            new TeamMember
+            {
+                Id = 1
+            },
+
+            new TeamMember
+            {
+                Id = 2
+            }
+        };
+
+        return team_members;
+    }
+
+    private static List<TeamMember>? GetTeamListWithNotExistingId()
+    {
+        return null;
+    }
+
+    private static List<TeamMemberDTO> GetListTeamDTO()
+    {
+        var team_membersDTO = new List<TeamMemberDTO>
+        {
+            new TeamMemberDTO
+            {
+                Id = 1
+            },
+
+            new TeamMemberDTO
+            {
+                Id = 2,
+            }
+        };
+
+        return team_membersDTO;
+    }
+}

e56b9e8a9332dd754aa32e85720378e28b6a2115
Andrii Savka <andrii2353@gmail.com>
2023-06-13 21:58:06 +0300

    Create unit tests for create positions endpoint. Fix to get correct message from mediator

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Team/Position/CreatePositionTests.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using Moq;
+using Streetcode.BLL.DTO.Partners;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.MediatR.Partners.Create;
+using Streetcode.BLL.MediatR.Team.Create;
+using Streetcode.DAL.Entities.Partners;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Streetcode.XUnitTest.MediatRTests.Team.Position
+{
+    public class CreatePositionTests
+    {
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IRepositoryWrapper> _mockRepository;
+
+        public CreatePositionTests()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockRepository = new Mock<IRepositoryWrapper>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessfully_TypeIsCorrect()
+        {
+            //Arrange
+            var testPositions = GetPositions();
+
+            _mockMapper.Setup(x => x.Map<Positions>(It.IsAny<PositionDTO>()))
+                .Returns(testPositions);
+            _mockMapper.Setup(x => x.Map<PositionDTO>(It.IsAny<Partner>()))
+                .Returns(GetPositionsDTO());
+
+            _mockRepository.Setup(x => x.PositionRepository.CreateAsync(It.Is<Positions>(y => y.Id == testPositions.Id)))
+                .ReturnsAsync(testPositions);
+            _mockRepository.Setup(x => x.StreetcodeRepository.GetAllAsync(It.IsAny<Expression<Func<StreetcodeContent, bool>>>(), null))
+                .ReturnsAsync(new List<StreetcodeContent>());
+            _mockRepository.Setup(x => x.SaveChanges())
+                .Returns(1);
+
+            var handler = new CreatePositionHandler(_mockMapper.Object, _mockRepository.Object);
+
+            //Act
+            var result = await handler.Handle(new CreatePositionQuery(GetPositionsDTO()), CancellationToken.None);
+
+            //Assert
+            Assert.IsType<PositionDTO>(result.Value);
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessfully_WhenPartnerAdded()
+        {
+            //Arrange
+            var testPositions = GetPositions();
+
+            _mockMapper.Setup(x => x.Map<Positions>(It.IsAny<PositionDTO>()))
+                .Returns(testPositions);
+            _mockMapper.Setup(x => x.Map<PositionDTO>(It.IsAny<Positions>()))
+                .Returns(GetPositionsDTO());
+
+            _mockRepository.Setup(x => x.PositionRepository.CreateAsync(It.Is<Positions>(y => y.Id == testPositions.Id)))
+                .ReturnsAsync(testPositions);
+            _mockRepository.Setup(x => x.StreetcodeRepository.GetAllAsync(It.IsAny<Expression<Func<StreetcodeContent, bool>>>(), null))
+                .ReturnsAsync(new List<StreetcodeContent>());
+            _mockRepository.Setup(x => x.SaveChanges())
+                .Returns(1);
+
+            var handler = new CreatePositionHandler(_mockMapper.Object, _mockRepository.Object);
+
+            //Act
+            var result = await handler.Handle(new CreatePositionQuery(GetPositionsDTO()), CancellationToken.None);
+
+            //Assert
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task ShouldThrowExeption_SaveChangesIsNotSuccessful()
+        {
+            //Arrange
+            var testPositions = GetPositions();
+            var expectedError = "Failed to create a Position";
+
+            _mockMapper.Setup(x => x.Map<Positions>(It.IsAny<PositionDTO>()))
+                .Returns(testPositions);
+            _mockMapper.Setup(x => x.Map<PositionDTO>(It.IsAny<Positions>()))
+                .Returns(GetPositionsDTO());
+
+            _mockRepository.Setup(x => x.PositionRepository.CreateAsync(It.Is<Positions>(y => y.Id == testPositions.Id)))
+                .ReturnsAsync(testPositions);
+
+            _mockRepository.Setup(x => x.SaveChanges())
+                .Throws(new Exception(expectedError));
+
+            var handler = new CreatePositionHandler(_mockMapper.Object, _mockRepository.Object);
+
+            //Act
+            var result = await handler.Handle(new CreatePositionQuery(GetPositionsDTO()), CancellationToken.None);
+
+            //Assert
+            Assert.Equal(expectedError, result.Errors.First().Message);
+
+            _mockRepository.Verify(x => x.StreetcodeRepository.GetAllAsync(It.IsAny<Expression<Func<StreetcodeContent, bool>>>(), null), Times.Never);
+        }
+
+        private static Positions GetPositions()
+        {
+            return new Positions()
+            {
+                Id = 1
+            };
+        }
+
+        private static PositionDTO GetPositionsDTO()
+        {
+            return new PositionDTO();
+        }
+    }
+}

008e96ef36c5c456501d6c9114943e2c7e948232
Dmytro Hranovskyi <wehfis@gmail.com>
2023-06-16 12:01:06 +0300

    deleted spare spaces, added setup methods.

--- Streetcode/Streetcode.XUnitTest/MediatRTests/Teams/GetByIdTeamTest.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Teams/GetByIdTeamTest.cs
@@ -9,133 +9,127 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 using System.Linq.Expressions;
 using Xunit;
 
-namespace Streetcode.XUnitTest.MediatRTests.Teams;
-
-public class GetTeamByIdTest
+namespace Streetcode.XUnitTest.MediatRTests.Teams
 {
-    private Mock<IRepositoryWrapper> _mockRepository;
-    private Mock<IMapper> _mockMapper;
-
-    public GetTeamByIdTest()
+    public class GetTeamByIdTest
     {
-        _mockMapper = new Mock<IMapper>();
-        _mockRepository = new Mock<IRepositoryWrapper>();
-    }
+        private Mock<IRepositoryWrapper> _mockRepository;
+        private Mock<IMapper> _mockMapper;
 
-    [Fact]
-    public async Task ShouldReturnSuccessfully_ExistingId()
-    {
-        //Arrange
-        var testTeam = GetTeam();
-
-        _mockRepository.Setup(x => x.TeamRepository
-            .GetSingleOrDefaultAsync(
-               It.IsAny<Expression<Func<TeamMember, bool>>>(),
-                It.IsAny<Func<IQueryable<TeamMember>,
-                IIncludableQueryable<TeamMember, object>>>()))
-            .ReturnsAsync(testTeam);
-
-        _mockMapper
-            .Setup(x => x
-            .Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
-            .Returns(GetTeamDTO());
-
-        var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
-
-        //Act
-        var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
-
-        //Assert
-        Assert.Multiple(
-            () => Assert.NotNull(result),
-            () => Assert.True(result.IsSuccess),
-            () => Assert.Equal(result.Value.Id, testTeam.Id)
-        );
-    }
-
-    [Fact]
-    public async Task ShouldReturnErrorResponse_NotExistingId()
-    {
-        //Arrange
-        var testTeam = GetTeam();
-        var expectedError = $"Cannot find any team with corresponding id: {testTeam.Id}";
-
-        _mockRepository.Setup(x => x.TeamRepository
-            .GetSingleOrDefaultAsync(
-               It.IsAny<Expression<Func<TeamMember, bool>>>(),
-                It.IsAny<Func<IQueryable<TeamMember>,
-                IIncludableQueryable<TeamMember, object>>>()))
-            .ReturnsAsync(GetTeamWithNotExistingId());
-
-        _mockMapper
-            .Setup(x => x
-            .Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
-            .Returns(GetTeamDTOWithNotExistingId());
-
-        var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
-
-        //Act
-        var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
-
-        //Assert
-        Assert.Multiple(
-            () => Assert.True(result.IsFailed),
-            () => Assert.Equal(expectedError, result.Errors.First().Message)
-        );
-    }
-
-    [Fact]
-    public async Task ShouldReturnSuccessfully_CorrectType()
-    {
-        //Arrange
-        var testTeam = GetTeam();
-
-        _mockRepository.Setup(x => x.TeamRepository
-            .GetSingleOrDefaultAsync(
-               It.IsAny<Expression<Func<TeamMember, bool>>>(),
-                It.IsAny<Func<IQueryable<TeamMember>,
-                IIncludableQueryable<TeamMember, object>>>()))
-            .ReturnsAsync(testTeam);
-
-        _mockMapper
-            .Setup(x => x
-            .Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
-            .Returns(GetTeamDTO());
-
-        var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
-
-        //Act
-        var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
-
-        //Assert
-        Assert.Multiple(
-            () => Assert.NotNull(result.ValueOrDefault),
-            () => Assert.IsType<TeamMemberDTO>(result.ValueOrDefault)
-        );
-    }
-
-    private static TeamMember GetTeam()
-    {
-        return new TeamMember
+        public GetTeamByIdTest()
         {
-            Id = 1
-        };
-    }
-    private static TeamMember? GetTeamWithNotExistingId()
-    {
-        return null;
-    }
+            _mockMapper = new Mock<IMapper>();
+            _mockRepository = new Mock<IRepositoryWrapper>();
+        }
 
-    private static TeamMemberDTO GetTeamDTO()
-    {
-        return new TeamMemberDTO
+        private void SetupGetTeamById(TeamMember testTeam)
         {
-            Id = 1
-        };
-    }
+            _mockRepository.Setup(x => x.TeamRepository
+                .GetSingleOrDefaultAsync(
+                    It.IsAny<Expression<Func<TeamMember, bool>>>(),
+                    It.IsAny<Func<IQueryable<TeamMember>, IIncludableQueryable<TeamMember, object>>>()))
+                .ReturnsAsync(testTeam);
+        }
 
-    private static TeamMemberDTO? GetTeamDTOWithNotExistingId()
-    {
-        return null;
+        private void SetupMapTeamMember(TeamMemberDTO teamMemberDTO)
+        {
+            _mockMapper.Setup(x => x
+                .Map<TeamMemberDTO>(It.IsAny<TeamMember>()))
+                .Returns(teamMemberDTO);
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessfully_ExistingId()
+        {
+            // Arrange
+            var testTeam = GetTeam();
+            var teamDTO = GetTeamDTO();
+
+            SetupGetTeamById(testTeam);
+            SetupMapTeamMember(teamDTO);
+
+            var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(
+                () => Assert.NotNull(result),
+                () => Assert.True(result.IsSuccess),
+                () => Assert.Equal(result.Value.Id, testTeam.Id)
+            );
+        }
+
+        [Fact]
+        public async Task ShouldReturnErrorResponse_NotExistingId()
+        {
+            // Arrange
+            var testTeam = GetTeam();
+            var expectedError = $"Cannot find any team with corresponding id: {testTeam.Id}";
+            var teamDTOWithNotExistingId = GetTeamDTOWithNotExistingId();
+
+            SetupGetTeamById(GetTeamWithNotExistingId());
+            SetupMapTeamMember(teamDTOWithNotExistingId);
+
+            var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(
+                () => Assert.True(result.IsFailed),
+                () => Assert.Equal(expectedError, result.Errors.First().Message)
+            );
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessfully_CorrectType()
+        {
+            // Arrange
+            var testTeam = GetTeam();
+            var teamDTO = GetTeamDTO();
+
+            SetupGetTeamById(testTeam);
+            SetupMapTeamMember(teamDTO);
+
+            var handler = new GetByIdTeamHandler(_mockRepository.Object, _mockMapper.Object);
+
+            // Act
+            var result = await handler.Handle(new GetByIdTeamQuery(testTeam.Id), CancellationToken.None);
+
+            // Assert
+            Assert.Multiple(
+                () => Assert.NotNull(result.ValueOrDefault),
+                () => Assert.IsType<TeamMemberDTO>(result.ValueOrDefault)
+            );
+        }
+
+        private static TeamMember GetTeam()
+        {
+            return new TeamMember
+            {
+                Id = 1
+            };
+        }
+
+        private static TeamMember GetTeamWithNotExistingId()
+        {
+            return null;
+        }
+
+        private static TeamMemberDTO GetTeamDTO()
+        {
+            return new TeamMemberDTO
+            {
+                Id = 1
+            };
+        }
+
+        private static TeamMemberDTO GetTeamDTOWithNotExistingId()
+        {
+            return null;
+        }
     }
 }

782be3060b13fa08412cb4ffd3e3288f5cec75b9
KateYatsiuk <katerynayatsiuk@gmail.com>
2023-06-19 11:55:18 +0300

    Added seeding method

---
+++ Streetcode/Streetcode.XIntegrationTest/BlobServiceTests/SeedingTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using Streetcode.XIntegrationTest.BlobServiceTests.Utils;
+using Xunit;
+using System.IO;
+
+namespace Streetcode.XIntegrationTest.Tests
+{
+    public class BlobStorageFixtureTests
+    {
+        [Fact]
+        public void Seed_ShouldCreateTestFilesInBlobStorage()
+        {
+            // Arrange
+            var blobPath = "../../../BlobServiceTests/Utils/BlobStorageTest/";
+            var fixture = new BlobStorageFixture(blobPath);
+
+            // Act
+            fixture.Seed();
+            
+            // Assert
+            Assert.True(Directory.EnumerateFiles(blobPath).Any());
+        }
+    }
+}
--- Streetcode/Streetcode.XIntegrationTest/BlobServiceTests/Utils/BlobStorageFixture.cs
+++ Streetcode/Streetcode.XIntegrationTest/BlobServiceTests/Utils/BlobStorageFixture.cs
@@ -1,30 +1,60 @@
 ﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using Streetcode.BLL.Interfaces.BlobStorage;
 using Streetcode.BLL.Services.BlobStorageService;
+using Streetcode.DAL.Entities.Media;
+using Streetcode.DAL.Entities.Media.Images;
+using System.Text;
 
 namespace Streetcode.XIntegrationTest.BlobServiceTests.Utils
 {
     public class BlobStorageFixture
     {
-        private readonly IBlobService blobService;
+        private readonly BlobService blobService;
         private readonly IOptions<BlobEnvironmentVariables> environmentVariables;
-        private bool isInitialized;
+        // private bool isInitialized;
 
         public BlobStorageFixture(string blobPath = "../../BlobStorageTest/", string blobKey = "somethingForTest")
         {
             environmentVariables = Options.Create(new BlobEnvironmentVariables());
             environmentVariables.Value.BlobStorePath = blobPath;
             environmentVariables.Value.BlobStoreKey = blobKey;
-            isInitialized = false;
+            //isInitialized = false;
 
             blobService = new BlobService(environmentVariables);
         }
 
         public void Seed()
         {
-            if (!isInitialized)
-            { 
-                
+            //if (!isInitialized)
+            string blobPath = environmentVariables.Value.BlobStorePath;
+            if(!Directory.EnumerateFiles(blobPath).Any())
+            {
+                string initialDataImagePath = "../../../../Streetcode.DAL/InitialData/images.json";
+                string initialDataAudioPath = "../../../../Streetcode.DAL/InitialData/audios.json";
+                string imageJson = File.ReadAllText(initialDataImagePath, Encoding.UTF8);
+                string audiosJson = File.ReadAllText(initialDataAudioPath, Encoding.UTF8);
+                var imgfromJson = JsonConvert.DeserializeObject<List<Image>>(imageJson);
+                var audiosfromJson = JsonConvert.DeserializeObject<List<Audio>>(audiosJson);
+
+                foreach (var img in imgfromJson)
+                {
+                    string filePath = Path.Combine(blobPath, img.BlobName);
+                    if (!File.Exists(filePath))
+                    {
+                        blobService.SaveFileInStorageBase64(img.Base64, img.BlobName.Split('.')[0], img.BlobName.Split('.')[1]);
+                    }
+                }
+
+                foreach (var audio in audiosfromJson)
+                {
+                    string filePath = Path.Combine(blobPath, audio.BlobName);
+                    if (!File.Exists(filePath))
+                    {
+                        blobService.SaveFileInStorageBase64(audio.Base64, audio.BlobName.Split('.')[0], audio.BlobName.Split('.')[1]);
+                    }
+                }
+                //isInitialized = true;
             }
         }
     }

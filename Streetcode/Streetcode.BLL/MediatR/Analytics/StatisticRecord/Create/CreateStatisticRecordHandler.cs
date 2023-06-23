84d8a432e99ab49a530c55548aa2e9c59a18f046
ValDekh <smithdekh@gmail.com>
2023-06-23 17:09:53 +0300

    Few chages

--- Streetcode/Streetcode.BLL/MediatR/Analytics/StatisticRecord/Create/CreateStatisticRecordHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Analytics/StatisticRecord/Create/CreateStatisticRecordHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FluentResults;
 using MediatR;
+using Microsoft.Extensions.Localization;
 using Streetcode.BLL.DTO.Analytics;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
@@ -12,11 +13,13 @@ namespace Streetcode.BLL.MediatR.Analytics.StatisticRecord.Create
     {
         private readonly IMapper _mapper;
         private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly IStringLocalizer<CreateStatisticRecordHandler> _stringLocalizer;
 
-        public CreateStatisticRecordHandler(IMapper mapper, IRepositoryWrapper repositoryWrapper)
+        public CreateStatisticRecordHandler(IMapper mapper, IRepositoryWrapper repositoryWrapper, IStringLocalizer<CreateStatisticRecordHandler> stringLocalizer)
         {
             _mapper = mapper;
             _repositoryWrapper = repositoryWrapper;
+            _stringLocalizer = stringLocalizer;
         }
 
         public async Task<Result<StatisticRecordDTO>> Handle(CreateStatisticRecordCommand request, CancellationToken cancellationToken)
@@ -25,28 +28,28 @@ namespace Streetcode.BLL.MediatR.Analytics.StatisticRecord.Create
 
             if (statRecord == null)
             {
-                return Result.Fail(new Error("Mapped record is null"));
+                return Result.Fail(new Error(_stringLocalizer?["MappedRecordIsNull"].Value));
             }
 
             var createdRecord = _repositoryWrapper.StatisticRecordRepository.Create(statRecord);
 
             if (createdRecord == null)
             {
-                return Result.Fail(new Error("Created record is null"));
+                return Result.Fail(new Error(_stringLocalizer?["CreatedRecordIsNull"].Value));
             }
 
             var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
 
             if (!resultIsSuccess)
             {
-                return Result.Fail(new Error("Cannot save created record"));
+                return Result.Fail(new Error(_stringLocalizer?["CannotSaveCreatedRecord"].Value));
             }
 
             var mappedCreatedRecord = _mapper.Map<StatisticRecordDTO>(createdRecord);
 
             if (mappedCreatedRecord == null)
             {
-                return Result.Fail(new Error("Mapped created record is null"));
+                return Result.Fail(new Error(_stringLocalizer?["MappedCreatedRecordIsNull"].Value));
             }
 
             return Result.Ok(mappedCreatedRecord);

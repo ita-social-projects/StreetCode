89b4a354e9f681fbfeda1ff25c612b1ca42032cb
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-06-27 16:08:36 +0300

    Added separate method for setup and removed magic strings

--- Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedTerm/Create/CreateRelatedTermHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedTerm/Create/CreateRelatedTermHandlerTests.cs
@@ -23,18 +23,50 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
             _mapperMock = new Mock<IMapper>();
         }
 
+        private (RelatedTermDTO relatedTermDTO, Entity entity) CreateRelatedTermObjects(int termId, string word)
+        {
+            var relatedTermDTO = new RelatedTermDTO { TermId = termId, Word = word };
+            var entity = new Entity { TermId = relatedTermDTO.TermId, Word = relatedTermDTO.Word };
+
+            return (relatedTermDTO, entity);
+        }
+
+        private void SetupMapperMockToMapEntity(RelatedTermDTO relatedTermDTO, Entity entity)
+        {
+            _mapperMock.Setup(m => m.Map<Entity>(It.IsAny<RelatedTermDTO>())).Returns(entity);
+        }
+
+        private void SetupGetAllAsyncWithExistingTerms(List<Entity> existingTerms)
+        {
+            _repositoryWrapperMock.Setup(r => r.RelatedTermRepository
+                .GetAllAsync(It.IsAny<Expression<Func<Entity, bool>>>(),
+                    It.IsAny<Func<IQueryable<Entity>, IIncludableQueryable<Entity, object>>>()))
+                .ReturnsAsync(existingTerms);
+        }
+
+        private void VerifyCreateAndSaveChangesNever()
+        {
+            _repositoryWrapperMock.Verify(r => r.RelatedTermRepository.Create(It.IsAny<Entity>()), Times.Never);
+            _repositoryWrapperMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+        }
+
+        private void VerifyCreateAndSaveChangesOnce(Entity entity)
+        {
+            _repositoryWrapperMock.Verify(r => r.RelatedTermRepository.Create(entity), Times.Once);
+            _repositoryWrapperMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+        }
+
         [Theory]
         [InlineData(2, "example")]
         public async Task ShouldReturnSuccessfully_WhenRelatedTermAdded(int termId, string word)
         {
             // Arrange
-            var relatedTermDTO = new RelatedTermDTO { TermId = termId, Word = word };
-            var entity = new Entity { TermId = termId, Word = word };
+            var (relatedTermDTO, entity) = CreateRelatedTermObjects(termId, word);
             var createRelatedTermCommand = new CreateRelatedTermCommand(relatedTermDTO);
 
             _repositoryWrapperMock.Setup(r => r.RelatedTermRepository.Create(entity));
             _repositoryWrapperMock.Setup(r => r.SaveChangesAsync()).ReturnsAsync(1);
-            _mapperMock.Setup(m => m.Map<Entity>(relatedTermDTO)).Returns(entity);
+            SetupMapperMockToMapEntity(relatedTermDTO, entity);
 
             var handler = new CreateRelatedTermHandler(_repositoryWrapperMock.Object, _mapperMock.Object);
 
@@ -51,7 +83,7 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
         public async Task Handle_Should_Return_Error_When_Related_Term_Is_Null()
         {
             // Arrange
-            _mapperMock.Setup(m => m.Map<Entity>(It.IsAny<RelatedTermDTO>())).Returns<Entity>(null);
+            SetupMapperMockToMapEntity(It.IsAny<RelatedTermDTO>(), null);
             var handler = new CreateRelatedTermHandler(_repositoryWrapperMock.Object, _mapperMock.Object);
             var command = new CreateRelatedTermCommand(new RelatedTermDTO());
 
@@ -60,7 +92,8 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
 
             // Assert
             Assert.True(result.IsFailed);
-            Assert.Equal("Cannot create new related word for a term!", result.Errors.First().Message);
+            Assert.False(result.IsSuccess);
+            Assert.Empty(result.Errors);
         }
 
         [Theory]
@@ -68,15 +101,11 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
         public async Task Handle_Should_Return_Error_When_Related_Term_Already_Exists(int termId, string word)
         {
             // Arrange
-            var relatedTermDTO = new RelatedTermDTO { TermId = termId, Word = word };
-            var entity = new Entity { TermId = relatedTermDTO.TermId, Word = relatedTermDTO.Word };
+            var (relatedTermDTO, entity) = CreateRelatedTermObjects(termId, word);
             var existingTerms = new List<Entity> { entity };
 
-            _mapperMock.Setup(m => m.Map<Entity>(It.IsAny<RelatedTermDTO>())).Returns(entity);
-            _repositoryWrapperMock.Setup(r => r.RelatedTermRepository
-                .GetAllAsync(It.IsAny<Expression<Func<Entity, bool>>>(),
-                    It.IsAny<Func<IQueryable<Entity>, IIncludableQueryable<Entity, object>>>()))
-                .ReturnsAsync(existingTerms);
+            SetupMapperMockToMapEntity(relatedTermDTO, entity);
+            SetupGetAllAsyncWithExistingTerms(existingTerms);
 
             var handler = new CreateRelatedTermHandler(_repositoryWrapperMock.Object, _mapperMock.Object);
             var command = new CreateRelatedTermCommand(relatedTermDTO);
@@ -86,9 +115,9 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
 
             // Assert
             Assert.True(result.IsFailed);
-            Assert.Equal("Слово з цим визначенням уже існує", result.Errors.First().Message);
-            _repositoryWrapperMock.Verify(r => r.RelatedTermRepository.Create(entity), Times.Never);
-            _repositoryWrapperMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+            Assert.False(result.IsSuccess);
+            Assert.Empty(result.Errors);
+            VerifyCreateAndSaveChangesNever();
         }
 
         [Theory]
@@ -96,17 +125,13 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
         public async Task Handle_Should_Return_Error_When_SaveChangesAsync_Fails(int termId, string word)
         {
             // Arrange
-            var relatedTermDTO = new RelatedTermDTO { TermId = termId, Word = word };
-            var entity = new Entity { TermId = relatedTermDTO.TermId, Word = relatedTermDTO.Word };
+            var (relatedTermDTO, entity) = CreateRelatedTermObjects(termId, word);
             var existingTerms = new List<Entity>();
             var repositoryMock = new Mock<IRepositoryWrapper>();
             var mapperMock = new Mock<IMapper>();
 
             mapperMock.Setup(m => m.Map<Entity>(It.IsAny<RelatedTermDTO>())).Returns(entity);
-            _repositoryWrapperMock.Setup(r => r.RelatedTermRepository
-                .GetAllAsync(It.IsAny<Expression<Func<Entity, bool>>>(),
-                    It.IsAny<Func<IQueryable<Entity>, IIncludableQueryable<Entity, object>>>()))
-                .ReturnsAsync(existingTerms);
+            SetupGetAllAsyncWithExistingTerms(existingTerms);
 
             repositoryMock.Setup(r => r.RelatedTermRepository.Create(It.IsAny<Entity>()));
             repositoryMock.Setup(r => r.SaveChangesAsync()).ReturnsAsync(0);
@@ -119,9 +144,9 @@ namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerm.Create
 
             // Assert
             Assert.True(result.IsFailed);
-            Assert.Equal("Cannot save changes in the database after related word creation!", result.Errors.First().Message);
-            repositoryMock.Verify(r => r.RelatedTermRepository.Create(entity), Times.Once);
-            repositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+            Assert.False(result.IsSuccess);
+            Assert.Empty(result.Errors);
+            VerifyCreateAndSaveChangesOnce(entity);
         }
     }
 }

8fd3acca367831f5a0629ae6c11665a25aeb9fbf
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-06-29 12:58:35 +0300

    new handler

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Term/GetAllWithRelated/GetAllTermsWithRelatedQuery.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode.TextContent;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Term.GetAllWithRelated
+{
+    public record GetAllTermsWithRelatedQuery : IRequest<Result<IEnumerable<TermDTO>>>;
+}

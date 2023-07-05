21b7014dbf950112550e392e06fbc01b5ae7ef00
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-07-05 14:40:15 +0300

    merge

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -21,8 +21,7 @@ if (app.Environment.EnvironmentName == "Local")
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-    await app.ApplyMigrations();
-   /* await app.SeedDataAsync();*/
+    // await app.SeedDataAsync();
 }
 else
 {

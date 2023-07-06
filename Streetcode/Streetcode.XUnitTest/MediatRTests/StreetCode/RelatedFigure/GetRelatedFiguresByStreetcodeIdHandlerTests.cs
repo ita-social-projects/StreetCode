0d4eb586f371748f1272f7a870426e532fdebb23
ormykhalyshyn <or.mykhalyshyn@gmail.com>
2023-07-06 17:55:29 +0300

    added test coverage for new code(only Related...)

--- Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedFigure/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedFigure/GetRelatedFiguresByStreetcodeIdHandlerTests.cs
@@ -9,10 +9,11 @@ using Entities = Streetcode.DAL.Entities.Streetcode;
 using Streetcode.DAL.Entities.Streetcode.Types;
 using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.BLL.DTO.Streetcode.RelatedFigure;
+using Streetcode.DAL.Entities.Media.Images;
 
 namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
 {
-  public class GetRelatedFiguresByStreetcodeIdHandlerTests
+    public class GetRelatedFiguresByStreetcodeIdHandlerTests
     {
         private readonly Mock<IRepositoryWrapper> _repository;
         private readonly Mock<IMapper> _mapper;
@@ -40,7 +41,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
             {
                 new RelatedFigureDTO()
             };
-            
+
             RepositorySetup(testRelatedList.AsQueryable(), null);
 
             _repository.Setup(x => x.StreetcodeRepository.GetAllAsync(
@@ -56,23 +57,23 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
             var result = await handler.Handle(new GetRelatedFigureByStreetcodeIdQuery(id), CancellationToken.None);
             // assert
             Assert.Multiple(
-                () => Assert.NotNull(result), 
-                () => Assert.NotEmpty(result.Value));         
+                () => Assert.NotNull(result),
+                () => Assert.NotEmpty(result.Value));
         }
 
         [Theory]
         [InlineData(1)]
         public async Task Handle_ReturnsCorrectType(int id)
-        {   
+        {
             // arrange
             var testStreetcodeContentList = new List<StreetcodeContent>()
             {
                 new StreetcodeContent()
             };
 
-            var testRelatedFigureList = new List<Entities.RelatedFigure>() 
-            { 
-                new Entities.RelatedFigure() 
+            var testRelatedFigureList = new List<Entities.RelatedFigure>()
+            {
+                new Entities.RelatedFigure()
             };
 
             var testRelatedDTO = new RelatedFigureDTO() { Id = id };
@@ -94,7 +95,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
         [Theory]
         [InlineData(2)]
         public async Task Handle_NonExisting_ReturnsGetAllNull(int id)
-        {   
+        {
             // arrange
             var testRelatedFigureEmptyList = new List<Entities.RelatedFigure>();
             var testRelatedDTO = new RelatedFigureDTO() { Id = id };
@@ -113,7 +114,7 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
         [Theory]
         [InlineData(2)]
         public async Task Handle_NonExisting_ReturnsFindAllNull(int id)
-        {   
+        {
             // arrange
             string expectedErrorMessage = $"Cannot find any related figures by a streetcode id: {id}";
             var testRelatedDTO = new RelatedFigureDTO() { Id = id };
@@ -143,6 +144,39 @@ namespace Streetcode.XUnitTest.MediatRTests.Streetcode.RelatedFigure
                 It.IsAny<Func<IQueryable<StreetcodeContent>, IIncludableQueryable<StreetcodeContent, object>>?>()))
                 .ReturnsAsync(streetcodeContents);
         }
+
+        // ... Existing code in GetRelatedFiguresByStreetcodeIdHandlerTests ...
+
+        [Fact]
+        public void Handle_OrdersImagesByAlt()
+        {
+            // Arrange
+            var relatedFigures = new List<StreetcodeContent>
+            {
+                new StreetcodeContent
+                {
+                    Images = new List<Image>
+                    {
+                        new Image { ImageDetails = new ImageDetails { Alt = "B" } },
+                        new Image { ImageDetails = new ImageDetails { Alt = "A" } }
+                    }
+                }
+            };
+
+            // Act
+            foreach (StreetcodeContent streetcode in relatedFigures)
+            {
+                if (streetcode.Images != null)
+                {
+                    streetcode.Images = streetcode.Images.OrderBy(img => img.ImageDetails?.Alt).ToList();
+                }
+            }
+
+            // Assert
+            Assert.Equal("A", relatedFigures[0].Images[0].ImageDetails.Alt);
+            Assert.Equal("B", relatedFigures[0].Images[1].ImageDetails.Alt);
+        }
+
 
         private void MapperSetup(RelatedFigureDTO relatedFigureDTO)
         {

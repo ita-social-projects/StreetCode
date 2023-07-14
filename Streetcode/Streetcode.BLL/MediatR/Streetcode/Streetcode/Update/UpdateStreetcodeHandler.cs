d1c2ef457a86250a20d06b297493ca9e845f5b1d
unknown <nadia.kischyk2003@gmail.com>
2023-07-14 12:40:39 +0300

    fix updating streetcode type

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/Update/UpdateStreetcodeHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/Update/UpdateStreetcodeHandler.cs
@@ -16,6 +16,7 @@ using Streetcode.DAL.Entities.Media.Images;
 using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.DAL.Entities.Timeline;
 using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.DAL.Enums;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.Update
 {
@@ -57,9 +58,14 @@ namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.Update
                         await UpdateEntitiesAsync(new List<TextUpdateDTO> { request.Streetcode.Text }, _repositoryWrapper.TextRepository);
                     }
 
-                    _repositoryWrapper.StreetcodeRepository.Entry(streetcodeToUpdate).Property(x => x.CreatedAt).IsModified = false;
-                    streetcodeToUpdate.UpdatedAt = DateTime.UtcNow;
                     _repositoryWrapper.StreetcodeRepository.Update(streetcodeToUpdate);
+
+                    _repositoryWrapper.StreetcodeRepository.Entry(streetcodeToUpdate).Property(x => x.CreatedAt).IsModified = false;
+                    var discriminatorProperty = _repositoryWrapper.StreetcodeRepository.Entry(streetcodeToUpdate).Property<string>(StreetcodeTypeDiscriminators.DiscriminatorName );
+                    discriminatorProperty.CurrentValue = StreetcodeTypeDiscriminators.GetStreetcodeType(request.Streetcode.StreetcodeType);
+                    discriminatorProperty.IsModified = true;
+
+                    streetcodeToUpdate.UpdatedAt = DateTime.UtcNow;
                     var isResultSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
 
                     if (isResultSuccess)

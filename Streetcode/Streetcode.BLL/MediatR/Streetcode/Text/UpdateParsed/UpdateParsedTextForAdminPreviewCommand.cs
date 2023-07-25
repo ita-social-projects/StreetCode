986b122cf5f73369879b52780be58fa8e7592e30
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-07-25 12:25:13 +0300

    update rename

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Text/UpdateParsed/UpdateParsedTextForAdminPreviewCommand.cs
@@ -0,0 +1,9 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Text.GetParsed
+{
+    public record UpdateParsedTextForAdminPreviewCommand(string TextToParse) : IRequest<Result<string>>
+    {
+    }
+}

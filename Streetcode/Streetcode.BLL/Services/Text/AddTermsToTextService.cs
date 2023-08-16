7e1e88308be0cc0b614dddd79cd33cff681345f0
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-08-16 20:14:20 +0300

    fix related term

--- Streetcode/Streetcode.BLL/Services/Text/AddTermsToTextService.cs
+++ Streetcode/Streetcode.BLL/Services/Text/AddTermsToTextService.cs
@@ -35,7 +35,7 @@ namespace Streetcode.BLL.Services.Text
                 var manipulationWord = word;
                 if (word.Contains("<p>"))
                 {
-                    var split = word.Replace("<p>", "<span><br>");
+                    var split = word.Replace("<p>", "<span>");
                     manipulationWord = split;
                 }
 

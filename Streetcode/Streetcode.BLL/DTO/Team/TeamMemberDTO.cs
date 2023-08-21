f0f585a47fefc3f25648d4fa8bd831813adea3c0
maximu211 <63352037+maximu211@users.noreply.github.com>
2023-08-21 11:08:27 +0300

    Revert "Revert "team fix""

--- Streetcode/Streetcode.BLL/DTO/Team/TeamMemberDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Team/TeamMemberDTO.cs
@@ -8,8 +8,7 @@ namespace Streetcode.BLL.DTO.Team
     public class TeamMemberDTO
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? Name { get; set; }
         public string Description { get; set; }
         public bool IsMain { get; set; }
         public int ImageId { get; set; }

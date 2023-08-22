1c17ca4470aa8c60de54109cef52f715c78fc4ad
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2023-08-22 15:37:31 +0300

    black and white fix

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetAllMainPage/GetAllStreetcodesMainPageHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetAllMainPage/GetAllStreetcodesMainPageHandler.cs
@@ -7,6 +7,7 @@ using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllStreetcodesMainPage;
 using Streetcode.BLL.SharedResource;
+using Streetcode.DAL.Enums;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllMainPage
@@ -31,10 +32,16 @@ namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.GetAllMainPage
         {
             var streetcodes = await _repositoryWrapper.StreetcodeRepository.GetAllAsync(
                 predicate: sc => sc.Status == DAL.Enums.StreetcodeStatus.Published,
-                include: src => src.Include(item => item.Text).Include(item => item.Images));
+                include: src => src.Include(item => item.Text).Include(item => item.Images).ThenInclude(x => x.ImageDetails));
 
             if (streetcodes != null)
             {
+                const int keyNumOfImageToDisplay = (int)ImageAssigment.Blackandwhite;
+                foreach (var streetcode in streetcodes)
+                {
+                    streetcode.Images = streetcode.Images.Where(x => x.ImageDetails.Alt.Equals(keyNumOfImageToDisplay.ToString())).ToList();
+                }
+
                 var random = new Random();
                 var shuffledStreetcodes = streetcodes.OrderBy(sc => random.Next());
 

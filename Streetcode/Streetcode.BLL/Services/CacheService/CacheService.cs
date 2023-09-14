840fcece2fff76a12fb92d51cf03a6f82e05dfb2
Vitalii <98163405+MementoMorj@users.noreply.github.com>
2023-09-14 22:49:47 +0300

    logger

--- Streetcode/Streetcode.BLL/Services/CacheService/CacheService.cs
+++ Streetcode/Streetcode.BLL/Services/CacheService/CacheService.cs
@@ -7,26 +7,32 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Streetcode.BLL.Interfaces.Cache;
+using Streetcode.BLL.Interfaces.Logging;
 
 namespace Streetcode.BLL.Services.CacheService
 {
     public class CacheService : ICacheService
     {
         private readonly IMemoryCache _cache;
+        private readonly ILoggerService _logger;
         private readonly ConcurrentDictionary<object, SemaphoreSlim> _locks = new ConcurrentDictionary<object, SemaphoreSlim>();
 
-        public CacheService(IMemoryCache cache)
+        public CacheService(IMemoryCache cache, ILoggerService logger)
         {
             _cache = cache;
+            _logger = logger;
         }
 
         public async Task<T> GetOrSetAsync<T>(string key, Func<Task<T>> getItemCallback, TimeSpan cacheDuration)
         {
+            _logger.LogInformation(key + "GetOrSetAsync function start");
             if (_cache.TryGetValue(key, out T cachedItem))
             {
+                _logger.LogInformation(key + "TryGetValue function true");
                 return cachedItem;
             }
 
+            _logger.LogInformation(key + "TryGetValue function false");
             SemaphoreSlim mylock = _locks.GetOrAdd(key, k => new SemaphoreSlim(1, 1));
 
             await mylock.WaitAsync();
@@ -35,20 +41,22 @@ namespace Streetcode.BLL.Services.CacheService
             {
                 if (_cache.TryGetValue(key, out cachedItem))
                 {
+                    _logger.LogInformation(key + "TryGetValue function true");
                     return cachedItem;
                 }
 
+                _logger.LogInformation(key + "TryGetValue function false");
                 T item = await getItemCallback();
 
                 var cacheEntryOptions = new MemoryCacheEntryOptions
                 {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10),
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30),
                     SlidingExpiration = cacheDuration,
                     Priority = CacheItemPriority.Normal,
                 };
 
                 _cache.Set(key, item, cacheEntryOptions);
-
+                _logger.LogInformation(key + "Set function true");
                 return item;
             }
             finally

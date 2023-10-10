a6f5d5f922dc24190b664afe40c398f6c1bf2381
malyna2 <malynovskyiandriy35@gmail.com>
2023-10-10 18:37:40 +0300

    getting streetcodes on main page with pagination/back

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Streetcode/GetPageMainPage/GetPageOfStreetcodesMainPageQuery.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Streetcode;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Streetcode.GetPageMainPage
+{
+    public record GetPageOfStreetcodesMainPageQuery(ushort page, ushort pageSize, int shuffleSeed) : IRequest<Result<IEnumerable<StreetcodeMainPageDTO>>>;
+}

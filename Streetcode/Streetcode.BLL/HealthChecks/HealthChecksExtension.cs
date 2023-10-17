b32f877c20b1d99f304d967394f52354f80d0932
Oleh-Kril <oleh.kril.t@gmail.com>
2023-10-17 16:32:15 +0300

    Revert "fix typo"
    This reverts commit e03d57b1a4be6378cb278349b007fb31e1324d7a.

--- Streetcode/Streetcode.BLL/HealthChecks/HealthChecksExtension.cs
+++ Streetcode/Streetcode.BLL/HealthChecks/HealthChecksExtension.cs
@@ -14,7 +14,7 @@ namespace Streetcode.BLL.HealthChecks
     {
         public static IEndpointRouteBuilder UseHealthChecks(this IEndpointRouteBuilder endpoints)
         {
-            endpoints.MapHealthChecks("/health", new HealthCheckOptions
+            endpoints.MapHealthChecks("/healthz", new HealthCheckOptions
             {
                 Predicate = check => check.Name == "StartupProbe",
                 ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse

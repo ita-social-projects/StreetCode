c073a7f03376bc5fbc97bc4afe908c7b6ed8fe1d
Oleh-Kril <oleh.kril.t@gmail.com>
2023-10-24 20:55:56 +0300

    add migrations needed for streetcode creation

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20231024173910_AddIdToStreetcodeArt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    public partial class AddIdToStreetcodeArt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "streetcode",
+                table: "streetcode_art",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "streetcode",
+                table: "streetcode_art");
+        }
+    }
+}

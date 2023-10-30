626cd4caa07dce9070685f2b114532b8d379335e
Oleh-Kril <oleh.kril.t@gmail.com>
2023-10-30 09:01:46 +0200

    fix https and remove app.ApplyMigrations

--- Streetcode/Streetcode.WebApi/Program.cs
+++ Streetcode/Streetcode.WebApi/Program.cs
@@ -43,10 +43,6 @@ else
     app.UseHsts();
 }
 
-await app.ApplyMigrations();
-
-var appUrl = builder.Configuration.GetValue<string>("HealthCheckUrl");
-app.Urls.Add(appUrl);
 app.MapHealthChecksUI();
 app.UseHealthChecks();
 app.AddCleanAudiosJob();

54e1650eb42176139863d3c9158ede3d2d47fd72
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-04 21:29:15 +0200

    Change RoleConfiguration to RoleAndUserConfiguration

---
+++ Streetcode/Streetcode.WebApi/Configuration/RoleAndUserConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Streetcode.DAL.Persistence;
+using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.WebApi.Configuration
+{
+    public class RoleAndUserConfiguration
+    {
+        public static async Task AddUsersAndRoles(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<StreetcodeDbContext>() !;
+
+            // Create roles in database.
+            await context.Roles.AddRangeAsync(
+                new IdentityRole("admin"),
+                new IdentityRole("user"));
+
+            await context.SaveChangesAsync();
+
+            // Populate initial admin with information.
+            var initialAdmin = new User
+            {
+                Name = "admin",
+                Surname = "admin",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                UserName = "admin",
+                NormalizedUserName = "ADMIN",
+                PhoneNumber = "777-777-77-77",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+            // Add initial admin.
+            var password = new PasswordHasher<User>();
+            var hashed = password.HashPassword(initialAdmin, "SuperAdmin#007");
+            initialAdmin.PasswordHash = hashed;
+
+            await context.Users.AddAsync(initialAdmin);
+            await context.SaveChangesAsync();
+
+            // Assign role 'admin' to initialAdmin.
+            await AssignRole(serviceProvider, initialAdmin.Email, "admin");
+            await context.SaveChangesAsync();
+        }
+
+        public static async Task AssignRole(IServiceProvider services, string email, string role)
+        {
+            UserManager<User> userManager = services.GetService<UserManager<User>>() !;
+            User user = await userManager!.FindByEmailAsync(email);
+            var result = await userManager.AddToRoleAsync(user, role);
+        }
+    }
+}

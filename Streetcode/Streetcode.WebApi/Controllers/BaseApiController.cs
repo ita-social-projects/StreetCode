109090184ac689f97c475a4a80bb10d1238de16f
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-05 00:02:20 +0200

    Add UserManager and RoleManager injection in UserController; add POST method for registration

--- Streetcode/Streetcode.WebApi/Controllers/BaseApiController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/BaseApiController.cs
@@ -16,7 +16,7 @@ public class BaseApiController : ControllerBase
     }
 
     protected IMediator Mediator => _mediator ??=
-        HttpContext.RequestServices.GetService<IMediator>()!;
+        HttpContext.RequestServices.GetService<IMediator>() !;
     protected ActionResult HandleResult<T>(Result<T> result)
     {
         if (result.IsSuccess)

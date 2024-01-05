41ca37d0048e3cc195c969a8a5ac9c0ae394b120
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-05 09:53:27 +0200

    Add PhoneNumber to UserRegisterDTO

--- Streetcode/Streetcode.BLL/DTO/Users/UserRegisterDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Users/UserRegisterDTO.cs
@@ -12,19 +12,29 @@ namespace Streetcode.BLL.DTO.Users
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Surname { get; set; }
+
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
         [Required]
         [MaxLength(20, ErrorMessage = "UserName maximum length is 20")]
         public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
         [Required]
         [MaxLength(20, ErrorMessage = "Password maximum length is 20")]
         [MinLength(14, ErrorMessage = "Password minimum length is 14")]
         public string Password { get; set; }
+
         [Required]
         [Display(Name = "Confirm password")]
         [MaxLength(20, ErrorMessage = "Password maximum length is 20")]

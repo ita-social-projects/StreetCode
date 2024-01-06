e2ee3fb49bc04e73de32ef572cc6cffdb6483468
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-06 11:38:02 +0200

    Delete AuthRepository; change order of fileds in jwt token

--- Streetcode/Streetcode.DAL/Repositories/Interfaces/Authorization/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Streetcode.DAL.Entities.Users;
-
-namespace Streetcode.DAL.Repositories.Interfaces.Authorization
-{
-    public interface IAuthRepository
-    {
-        Task RegisterAsync(User User, string password);
-    }
-}

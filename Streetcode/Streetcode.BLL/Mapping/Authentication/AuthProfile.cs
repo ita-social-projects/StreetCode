3461380ec22b180a1097d8440eb9212c3778e8ce
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-09 19:19:36 +0200

    Create RegisterResponseDTO

---
+++ Streetcode/Streetcode.BLL/Mapping/Authentication/AuthProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Streetcode.BLL.DTO.Authentication;
+using Streetcode.BLL.DTO.Users;
+using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.BLL.Mapping.Authentication
+{
+    public class AuthProfile : Profile
+    {
+        public AuthProfile()
+        {
+            CreateMap<User, RegisterResponseDTO>().ReverseMap();
+        }
+    }
+}
--- Streetcode/Streetcode.BLL/MediatR/Authentication/Register/RegisterQuery.cs
+++ Streetcode/Streetcode.BLL/MediatR/Authentication/Register/RegisterQuery.cs
@@ -5,5 +5,5 @@ using Streetcode.BLL.DTO.Users;
 
 namespace Streetcode.BLL.MediatR.Users.SignUp
 {
-    public record RegisterQuery(RegisterRequestDTO registerRequestDTO) : IRequest<Result<UserDTO>>;
+    public record RegisterQuery(RegisterRequestDTO registerRequestDTO) : IRequest<Result<RegisterResponseDTO>>;
 }

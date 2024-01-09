31cb93951612986ef6fd799c002dfc1b631cffba
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-09 17:32:59 +0200

    Create new folder 'Validation' in '.BLL/Services'

--- Streetcode/Streetcode.BLL/Services/Validation/ValidEmailAttribute.cs
+++ Streetcode/Streetcode.BLL/Services/Validation/ValidEmailAttribute.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Streetcode.BLL.Services.Authentication
+namespace Streetcode.BLL.Services.Validation
 {
     public class ValidEmailAttribute : ValidationAttribute
     {

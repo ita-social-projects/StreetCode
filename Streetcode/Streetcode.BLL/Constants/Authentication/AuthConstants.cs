7d7791a1cef6b370b7ab5bc244e2758ae2d7d067
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-09 19:04:32 +0200

    Create file with constants for Authentication

---
+++ Streetcode/Streetcode.BLL/Constants/Authentication/AuthConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.Constants.Authentication
+{
+    public static class AuthConstants
+    {
+        public const string NAME = "John";
+        public const string SURNAME = "Doe";
+        public const string EMAIL = "example@gmail.com";
+        public const string USERNAME = "John_Doe";
+        public const string PHONENUMBER = "+111-111-11-11";
+        public const string PASSWORD = "";
+    }
+}
--- Streetcode/Streetcode.BLL/DTO/Authentication/RegisterRequestDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Authentication/RegisterRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using Streetcode.BLL.Constants.Authentication;
 using Streetcode.BLL.Services.Authentication;
 
 namespace Streetcode.BLL.DTO.Authentication;
@@ -8,36 +9,36 @@ public class RegisterRequestDTO
 {
     [Required]
     [MaxLength(50)]
-    [DefaultValue("John")]
+    [DefaultValue(AuthConstants.NAME)]
     public string Name { get; set; }
 
     [Required]
     [MaxLength(50)]
-    [DefaultValue("Doe")]
+    [DefaultValue(AuthConstants.SURNAME)]
     public string Surname { get; set; }
 
     [Required]
     [EmailAddress]
     [ValidEmail]
-    [DefaultValue("example@gmail.com")]
+    [DefaultValue(AuthConstants.EMAIL)]
     public string Email { get; set; }
 
     [Required]
     [MaxLength(50, ErrorMessage = "UserName maximum length is 50")]
-    [DefaultValue("John_Doe")]
+    [DefaultValue(AuthConstants.USERNAME)]
     public string UserName { get; set; }
 
     [Required]
     [Display(Name = "Phone Number")]
     [Phone]
-    [DefaultValue("+111-111-11-11")]
+    [DefaultValue(AuthConstants.PHONENUMBER)]
     public string PhoneNumber { get; set; }
 
     [Required]
     [MaxLength(30, ErrorMessage = "Password maximum length is 30")]
     [MinLength(14, ErrorMessage = "Password minimum length is 14")]
     [StrongPassword]
-    [DefaultValue("")]
+    [DefaultValue(AuthConstants.PASSWORD)]
     public string Password { get; set; }
 
     [Required]

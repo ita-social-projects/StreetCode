826b241a65fadcfce7468ce8dc7d7d06723c7314
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-09 17:25:23 +0200

    Create RegisterRequestDTO; make RegisterHandler and AuthController consume RegisterRequestDTO

---
+++ Streetcode/Streetcode.BLL/DTO/Authentication/RegisterRequestDTO.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Streetcode.BLL.Services.Authentication;
+
+namespace Streetcode.BLL.DTO.Authentication;
+
+public class RegisterRequestDTO
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Surname { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [ValidEmail]
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "UserName maximum length is 50")]
+    public string UserName { get; set; }
+
+    [Required]
+    [Display(Name = "Phone Number")]
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    [MaxLength(30, ErrorMessage = "Password maximum length is 30")]
+    [MinLength(14, ErrorMessage = "Password minimum length is 14")]
+    [StrongPassword]
+    public string Password { get; set; }
+
+    [Required]
+    [Display(Name = "Confirm password")]
+    [MaxLength(30, ErrorMessage = "Password maximum length is 30")]
+    [MinLength(14, ErrorMessage = "Password minimum length is 14")]
+    [Compare(nameof(Password))]
+    public string PasswordConfirmed { get; set; }
+}

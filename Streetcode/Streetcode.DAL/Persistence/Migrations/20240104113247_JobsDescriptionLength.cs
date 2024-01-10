de04b511489e363dd17e327a04269801ab8675de
Kateryna Yatsiuk <113370393+KateYatsiuk@users.noreply.github.com>
2024-01-10 17:13:52 +0200

    Changed job description length limit (#1006)

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20240104113247_JobsDescriptionLength.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    public partial class JobsDescriptionLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "jobs",
+                table: "job",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "jobs",
+                table: "job",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}

ca64b6c8dae0084dd690031f592872f69b996f97
Valerii Dekhtiyk <61435019+ValDekh@users.noreply.github.com>
2024-01-12 13:57:07 +0200

    Integration test cases Create Streetcode (#975)
    * Add some test cases
    
    * Add intagration tast cases for Streetcode create.
    
    * Add some changes
    
    * Fixed test case with StreetcodeCreateDTO
    
    * Correct appSettings
    
    * Fix All integration tests after merging with develop
    
    * Adjusted remarks

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Media/VideoControllerTests.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Media/VideoControllerTests.cs
@@ -1,10 +1,10 @@
-﻿namespace Streetcode.XIntegrationTest.ControllerTests.Media
+﻿using Streetcode.BLL.DTO.Media;
+using Streetcode.XIntegrationTest.ControllerTests.Utils;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Media.Video;
+using Xunit;
+namespace Streetcode.XIntegrationTest.ControllerTests.Media
 {
-    using Streetcode.BLL.DTO.Media;
-    using Streetcode.BLL.DTO.Media.Audio;
-    using Streetcode.XIntegrationTest.ControllerTests.Utils;
-    using Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Media.Video;
-    using Xunit;
+   
 
     public class VideoControllerTests : BaseControllerTests, IClassFixture<CustomWebApplicationFactory<Program>>
     {

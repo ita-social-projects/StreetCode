e8fab49daff11bc36040cb654ff5c5b1fc083df8
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-17 09:24:56 +0200

    Add ExtractRegisterRequest attribute; add AuthControllerTests class for integration testing

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Authentication/AuthControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Streetcode;
+using Streetcode.XIntegrationTest.ControllerTests.Utils;
+using System.Net;
+using Xunit;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Authentication;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Authentication;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Authentication
+{
+    public class AuthControllerTests : BaseControllerTests<AuthenticationClient>, IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        public AuthControllerTests(CustomWebApplicationFactory<Program> factory)
+           : base(factory, "/api/Auth")
+        {
+        }
+
+        [Fact]
+        [ExtractRegisterRequest]
+        public async Task Register_ReturnsSuccessStatusCode()
+        {
+            // Arrange.
+            var registerRequest = ExtractRegisterRequest.RegisterRequest;
+
+            // Act.
+            var response = await this.client.Register(registerRequest);
+
+            // Assert.
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        [ExtractRegisterRequest]
+        public async Task Register_InvalidInputData_Returns404BadRequest()
+        {
+            // Arrange.
+            var registerRequest = ExtractRegisterRequest.RegisterRequest;
+            registerRequest.Email = string.Empty;
+
+            // Act.
+            var response = await this.client.Register(registerRequest);
+
+            // Assert.
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}

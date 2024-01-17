77b45ec55cdfc0db0400b71cd1b2f60131dea2d7
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-17 14:03:26 +0200

    Implement suggested changes

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Streetcode/Update/StreetcodeUpdateControllerTests.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Streetcode/Update/StreetcodeUpdateControllerTests.cs
@@ -1,17 +1,5 @@
-﻿using Streetcode.BLL.DTO.AdditionalContent.Subtitles;
-using Streetcode.BLL.DTO.AdditionalContent.Tag;
-using Streetcode.BLL.DTO.Analytics.Update;
-using Streetcode.BLL.DTO.Media.Art;
-using Streetcode.BLL.DTO.Media.Audio;
-using Streetcode.BLL.DTO.Media.Images;
-using Streetcode.BLL.DTO.Media.Video;
-using Streetcode.BLL.DTO.Partners.Update;
-using Streetcode.BLL.DTO.Sources.Update;
-using Streetcode.BLL.DTO.Streetcode.RelatedFigure;
-using Streetcode.BLL.DTO.Streetcode.TextContent.Fact;
+﻿using Streetcode.BLL.DTO.AdditionalContent.Tag;
 using Streetcode.BLL.DTO.Streetcode.Update;
-using Streetcode.BLL.DTO.Timeline.Update;
-using Streetcode.BLL.DTO.Toponyms;
 using Streetcode.BLL.Enums;
 using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.XIntegrationTest.ControllerTests.Utils;
@@ -34,12 +22,8 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
         [ExtractTestUpdateStreetcode]
         public async Task Update_ReturnSuccessStatusCode()
         {
-            StreetcodeContent expectedStreetcode = ExtractTestUpdateStreetcode.StreetcodeForTest;
+            StreetcodeUpdateDTO updateStreetCodeDTO = ExtractTestUpdateStreetcode.StreetcodeForTest;
 
-            var updateStreetCodeDTO = CreateMoqStreetCodeDTO(
-                expectedStreetcode.Id,
-                expectedStreetcode.Index,
-                expectedStreetcode.TransliterationUrl);
             var response = await client.UpdateAsync(updateStreetCodeDTO);
 
             Assert.True(response.IsSuccessStatusCode);
@@ -49,15 +33,10 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
         [ExtractTestUpdateStreetcode]
         public async Task Update_ChangesTitleAndTransliterationUrl()
         {
-            StreetcodeContent expectedStreetcode = ExtractTestUpdateStreetcode.StreetcodeForTest;
-
-            var updateStreetCodeDTO = CreateMoqStreetCodeDTO(
-                expectedStreetcode.Id,
-                expectedStreetcode.Index,
-                expectedStreetcode.TransliterationUrl);
+            StreetcodeUpdateDTO updateStreetCodeDTO = ExtractTestUpdateStreetcode.StreetcodeForTest;
             await client.UpdateAsync(updateStreetCodeDTO);
 
-            var responseGetByIdUpdated = await client.GetByIdAsync(expectedStreetcode.Id);
+            var responseGetByIdUpdated = await client.GetByIdAsync(updateStreetCodeDTO.Id);
             var streetCodeContent = CaseIsensitiveJsonDeserializer.Deserialize<StreetcodeContent>(responseGetByIdUpdated.Content);
             Assert.Multiple(() =>
             {
@@ -70,12 +49,9 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
         [ExtractTestUpdateStreetcode]
         public async Task Update_WithInvalidId_ReturnsBadRequest()
         {
+            StreetcodeUpdateDTO updateStreetCodeDTO = ExtractTestUpdateStreetcode.StreetcodeForTest;
+            updateStreetCodeDTO.Id++;
 
-            StreetcodeContent expectedStreetcode = ExtractTestUpdateStreetcode.StreetcodeForTest;
-            var updateStreetCodeDTO = CreateMoqStreetCodeDTO(
-                expectedStreetcode.Id + 1,
-                expectedStreetcode.Index,
-                expectedStreetcode.TransliterationUrl);
             var response = await client.UpdateAsync(updateStreetCodeDTO);
 
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
@@ -85,12 +61,9 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
         [ExtractTestUpdateStreetcode]
         public async Task Update_WithInvalidData_ReturnsBadRequest()
         {
-            StreetcodeContent expectedStreetcode = ExtractTestUpdateStreetcode.StreetcodeForTest;
-            var updateStreetCodeDTO = CreateMoqStreetCodeDTO(
-                expectedStreetcode.Id,
-                expectedStreetcode.Index,
-                expectedStreetcode.TransliterationUrl);
+            StreetcodeUpdateDTO updateStreetCodeDTO = ExtractTestUpdateStreetcode.StreetcodeForTest;
             updateStreetCodeDTO.Title = null; // Invalid data
+
             var response = await client.UpdateAsync(updateStreetCodeDTO);
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
@@ -99,11 +72,7 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
         [ExtractTestUpdateStreetcode]
         public async Task Update_WithInvalidTags_ReturnsBadRequest()
         {
-            StreetcodeContent expectedStreetcode = ExtractTestUpdateStreetcode.StreetcodeForTest;
-            var updateStreetCodeDTO = CreateMoqStreetCodeDTO(
-                expectedStreetcode.Id,
-                expectedStreetcode.Index,
-                expectedStreetcode.TransliterationUrl);
+            StreetcodeUpdateDTO updateStreetCodeDTO = ExtractTestUpdateStreetcode.StreetcodeForTest;
 
             // Invalid tag data
             updateStreetCodeDTO.Tags = new List<StreetcodeTagUpdateDTO>
@@ -114,38 +83,13 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Streetcode.Update
                             Title = "Invalid Tag",
                             IsVisible = true,
                             Index = 0,
-                            StreetcodeId = expectedStreetcode.Id,
+                            StreetcodeId = updateStreetCodeDTO.Id,
                             ModelState = ModelState.Updated
                         },
                     };
 
             var response = await client.UpdateAsync(updateStreetCodeDTO);
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-        }
-
-        private StreetcodeUpdateDTO CreateMoqStreetCodeDTO(int id, int index, string transliterationUrl)
-        {
-            return new StreetcodeUpdateDTO
-            {
-                Id = id,
-                Index = index,
-                Title = "New Title",
-                TransliterationUrl = transliterationUrl,
-                Tags = new List<StreetcodeTagUpdateDTO>(),
-                Facts = new List<FactUpdateDto>(),
-                Audios = new List<AudioUpdateDTO>(),
-                Images = new List<ImageUpdateDTO>(),
-                Videos = new List<VideoUpdateDTO>(),
-                Partners = new List<PartnersUpdateDTO>(),
-                Toponyms = new List<StreetcodeToponymUpdateDTO>(),
-                Subtitles = new List<SubtitleUpdateDTO>(),
-                DateString = "22 травня 2023",
-                TimelineItems = new List<TimelineItemCreateUpdateDTO>(),
-                RelatedFigures = new List<RelatedFigureUpdateDTO>(),
-                StreetcodeArts = new List<StreetcodeArtCreateUpdateDTO>(),
-                StatisticRecords = new List<StatisticRecordUpdateDTO>(),
-                StreetcodeCategoryContents = new List<StreetcodeCategoryContentUpdateDTO>(),
-            };
         }
     }
 }

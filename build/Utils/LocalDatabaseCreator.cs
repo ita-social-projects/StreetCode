6923c45d561c0a3563854fe1f7d6b89a0e8820e0
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-25 17:21:09 +0200

    Create LocalDatabaseCreator

---
+++ build/Utils/LocalDatabaseCreator.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using static Nuke.Common.NukeBuild;
+using System;
+using System.Data;
+using System.IO;
+
+namespace Utils
+{
+    public static class LocalDatabaseCreator
+    {
+        static readonly string PATH_TO_APPSETTINGS = Path.Combine(RootDirectory, "Streetcode/Streetcode.WebApi");
+        public static void CreateDatabase()
+        {
+            string connectionString = GetConnectionString();
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            sqlConnectionStringBuilder.InitialCatalog = string.Empty;
+
+            SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            
+
+            string Query = $"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Example') CREATE DATABASE [Example];";
+            SqlCommand Command = new SqlCommand(Query, connection);
+            try
+            {
+                connection.Open();
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            var environment = Environment.GetEnvironmentVariable("STREETCODE_ENVIRONMENT") ?? "IntegrationTests";
+
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
+                .ConfigureCustom(environment);
+
+            var configuration = configBuilder.Build();
+
+            return configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private static IConfigurationBuilder ConfigureCustom(this IConfigurationBuilder builder, string environment)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(Path.Combine(PATH_TO_APPSETTINGS, "appsettings.json"), optional: false, reloadOnChange: true)
+                .AddJsonFile(Path.Combine(PATH_TO_APPSETTINGS, $"appsettings.{environment}.json"), optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("STREETCODE_");
+
+            return builder;
+        }
+    }
+}

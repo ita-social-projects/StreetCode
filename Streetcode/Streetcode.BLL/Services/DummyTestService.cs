cfe29051f429bde4244178752f5caa59516751cf
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-29 13:35:55 +0200

    Add dummy class to check code coverage

---
+++ Streetcode/Streetcode.BLL/Services/DummyTestService.cs
@@ -0,0 +1,19 @@
+﻿namespace Streetcode.BLL.Services
+{
+    public static class DummyTestService
+    {
+        private static int d;
+        static DummyTestService()
+        {
+            d = 10;
+        }
+
+        public static void Output()
+        {
+            for (int i = 0; i < d; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

67b8802ba0fda9b8d1fb61e4031ca2d5b459aacd
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-30 20:56:29 +0200

    Add StreetcodeContentExtracter; add TestDataProvider

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/TestDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter
+{
+    public static class TestDataProvider
+    {
+        public static T GetTestData<T>()
+        {
+            string jsonFilePath = @"../../../testData.json";
+            using StreamReader reader = new (jsonFilePath);
+            string fileJson = reader.ReadToEnd();
+            var patsedFileJson = JObject.Parse(fileJson);
+
+            string entityJson = patsedFileJson[nameof(T)]?.ToString();
+            T entity = JsonConvert.DeserializeObject<T>(entityJson);
+            return entity;
+        }
+    }
+}

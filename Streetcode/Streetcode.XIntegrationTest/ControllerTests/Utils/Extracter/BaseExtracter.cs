55eb257baa91655dd35059417f2b0dddf93715ea
Ivan Yadchyshyn <yad7devadress@gmail.com>
2024-01-31 09:08:09 +0200

    Add StreetcodeContentExtraction

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/BaseExtracter.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/BaseExtracter.cs
@@ -10,24 +10,26 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter
             _dbHelper = BaseControllerTests.GetSqlDbHelper();
         }
 
-        public static T Extract<T>(T entity, Func<T, bool>? searchPredicate = null)
+        public static T Extract<T>(T entity, Func<T, bool> searchPredicate)
             where T : class, new()
         {
 
             if (!_dbHelper.Any<T>(searchPredicate))
             {
                 _dbHelper.AddNewItem<T>(entity);
+                _dbHelper.SaveChanges();
             }
 
             return _dbHelper.GetExistItem<T>(searchPredicate);
         }
 
-        public static void Remove<T>(T entity, Func<T, bool>? searchPredicate = null)
+        public static void Remove<T>(T entity, Func<T, bool> searchPredicate)
             where T : class, new()
         {
             if (_dbHelper.Any<T>(searchPredicate))
             {
                 _dbHelper.DeleteItem<T>(entity);
+                _dbHelper.SaveChanges();
             }
         }
     }

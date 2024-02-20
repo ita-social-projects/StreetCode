e3c9119b7f78b723441c5c8377fb8b55fd7090b2
BohdanBybliv <bohdanbybliv7@gmail.com>
2024-02-20 12:00:20 +0200

    fix sonarquality

--- Streetcode/Streetcode.BLL/DTO/ReCaptchaResponseDTO/ReCaptchaResponseDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/ReCaptchaResponseDTO/ReCaptchaResponseDTO.cs
@@ -3,6 +3,6 @@
     public class ReCaptchaResponseDto
     {
         public bool Success { get; set; }
-        public string[] ErrorCodes { get; set; }
+        public string[] ErrorCodes { get; set; } = null!;
     }
 }

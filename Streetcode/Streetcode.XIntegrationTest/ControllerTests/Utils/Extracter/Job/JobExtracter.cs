c21279d4d4a82fd57523777c74cf13dbfca84398
IvannYad <yad7devadress@gmail.com>
2024-04-01 17:21:17 +0300

    feat: add tests for Create Job

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/Job/JobExtracter.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.Job
+{
+    public class JobExtracter
+    {
+        public static DAL.Entities.Jobs.Job Extract(int jobId)
+        {
+            DAL.Entities.Jobs.Job testJob = TestDataProvider.GetTestData<DAL.Entities.Jobs.Job>();
+
+            testJob.Id = jobId;
+
+            return BaseExtracter.Extract<DAL.Entities.Jobs.Job>(testJob, job => job.Id == jobId);
+        }
+
+        public static void Remove(DAL.Entities.Jobs.Job entity)
+        {
+            BaseExtracter.RemoveByPredicate<DAL.Entities.Jobs.Job>(tag => tag.Id == entity.Id);
+        }
+    }
+}

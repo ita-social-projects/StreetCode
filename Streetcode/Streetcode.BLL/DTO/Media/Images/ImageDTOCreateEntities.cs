5adfcfd99d3919ba27e624c87ed33eaede654a13
Valerii Dekhtiyk <smithdekh@gmail.com>
2024-04-11 13:00:14 +0300

    Change newsDTO

---
+++ Streetcode/Streetcode.BLL/DTO/Media/Images/ImageDTOCreateEntities.cs
@@ -0,0 +1,10 @@
+﻿namespace Streetcode.BLL.DTO.Media.Images
+{
+    public class ImageDTOCreateEntities
+    {
+        public string? BlobName { get; set; }
+        public string? Base64 { get; set; }
+        public string? MimeType { get; set; }
+        public ImageDetailsDto? ImageDetails { get; set; }
+    }
+}
--- Streetcode/Streetcode.BLL/DTO/News/NewsCreateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/News/NewsCreateDTO.cs
@@ -8,7 +8,7 @@ namespace Streetcode.BLL.DTO.News
         public string Text { get; set; }
         public int? ImageId { get; set; }
         public string URL { get; set; }
-        public ImageDTO? Image { get; set; }
+        public ImageDTOCreateEntities? Image { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }

6fe64ecf8a231de0f9b1bfb3a47305eddd2f8e24
VladyslavPavlysko <vladyslav.pavlysko.work@gmail.com>
2024-04-14 18:41:03 +0300

    Update Delete tag

---
+++ Streetcode/Streetcode.BLL/MediatR/AdditionalContent/Tag/Update/UpdateTagHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.AdditionalContent;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using TagEntity = Streetcode.DAL.Entities.AdditionalContent.Tag;
+
+namespace Streetcode.BLL.MediatR.AdditionalContent.Tag.Update
+{
+	public class UpdateTagHandler : IRequestHandler<UpdateTagCommand, Result<TagDTO>>
+	{
+		private readonly IRepositoryWrapper _repositoryWrapper;
+		private readonly IMapper _mapper;
+		private readonly ILoggerService _logger;
+		public UpdateTagHandler(IRepositoryWrapper repository, IMapper mapper, ILoggerService logger)
+		{
+			_repositoryWrapper = repository;
+			_mapper = mapper;
+			_logger = logger;
+		}
+
+		public async Task<Result<TagDTO>> Handle(UpdateTagCommand request, CancellationToken cancellationToken)
+		{
+			var existedTag =
+				await _repositoryWrapper.TagRepository.GetFirstOrDefaultAsync(x => x.Id == request.tag.Id);
+			if (existedTag is null)
+			{
+				string exMessage = $"No tag found by entered Id - {request.tag.Id}";
+				_logger.LogError(request, exMessage);
+				return Result.Fail(exMessage);
+			}
+
+			try
+			{
+				var tagToUpdate = _mapper.Map<TagEntity>(request.tag);
+				_repositoryWrapper.TagRepository.Update(tagToUpdate);
+				await _repositoryWrapper.SaveChangesAsync();
+				return Result.Ok(_mapper.Map<TagDTO>(tagToUpdate));
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError(request, ex.Message);
+				return Result.Fail(ex.Message);
+			}
+		}
+	}
+}
--- Streetcode/Streetcode.WebApi/Controllers/AdditionalContent/TagController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/AdditionalContent/TagController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.AdditionalContent;
 using Streetcode.BLL.DTO.AdditionalContent.Tag;
 using Streetcode.BLL.MediatR.AdditionalContent.Tag.Create;
+using Streetcode.BLL.MediatR.AdditionalContent.Tag.Delete;
 using Streetcode.BLL.MediatR.AdditionalContent.Tag.GetAll;
 using Streetcode.BLL.MediatR.AdditionalContent.Tag.GetById;
 using Streetcode.BLL.MediatR.AdditionalContent.Tag.GetByStreetcodeId;
+using Streetcode.BLL.MediatR.AdditionalContent.Tag.Update;
 using Streetcode.DAL.Enums;
 
 namespace Streetcode.WebApi.Controllers.AdditionalContent;
@@ -43,5 +45,23 @@ public class TagController : BaseApiController
     public async Task<IActionResult> Create([FromBody] CreateTagDTO tagTitle)
     {
         return HandleResult(await Mediator.Send(new CreateTagQuery(tagTitle)));
+    }
+
+    [HttpDelete("{id:int}")]
+    [Authorize(Roles = nameof(UserRole.Admin))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        return HandleResult(await Mediator.Send(new DeleteTagCommand(id)));
+    }
+
+    [HttpPut]
+    [Authorize(Roles = nameof(UserRole.Admin))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> Update([FromBody] UpdateTagDTO tagDto)
+    {
+        return HandleResult(await Mediator.Send(new UpdateTagCommand(tagDto)));
     }
 }

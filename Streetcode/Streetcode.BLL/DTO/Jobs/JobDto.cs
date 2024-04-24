1d7b2fefbf0276ac381fb142319e3dbb26d3dd54
Shossy <pashka.v1331@gmail.com>
2024-04-24 17:12:42 +0300

    added validation for DTOs

--- Streetcode/Streetcode.BLL/DTO/Jobs/JobDto.cs
+++ Streetcode/Streetcode.BLL/DTO/Jobs/JobDto.cs
@@ -7,12 +7,17 @@ using System.Threading.Tasks;
 
 namespace Streetcode.BLL.DTO.Jobs
 {
-	public class JobDto
-	{
-		public int Id { get; set; }
-		public string Title { get; set; }
-		public bool Status { get; set; }
-		public string Description { get; set; }
-		public string Salary { get; set; }
-	}
+    public class JobDto
+    {
+        public int Id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "Max Length is 50")]
+        public string Title { get; set; }
+        public bool Status { get; set; }
+        [StringLength(3000, ErrorMessage = "Max Length is 3000")]
+        public string Description { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(15, ErrorMessage = "Max Length is 15")]
+        public string Salary { get; set; }
+    }
 }

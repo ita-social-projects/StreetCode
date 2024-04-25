db4737db71e53841ebe3d00dc556986ba05d6fc7
Anastasiia <89141775+Spoilsport0@users.noreply.github.com>
2024-04-26 00:17:36 +0300

    Update UpdateNewsHandler.cs

--- Streetcode/Streetcode.BLL/MediatR/Newss/Update/UpdateNewsHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Newss/Update/UpdateNewsHandler.cs
@@ -1,4 +1,4 @@
-﻿using AutoMapper;
+using AutoMapper;
 using FluentResults;
 using MediatR;
 using Microsoft.Extensions.Localization;
@@ -11,7 +11,7 @@ using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Newss.Update
 {
-    public class UpdateNewsHandler : IRequestHandler<UpdateNewsCommand, Result<UpdateNewsDTO>>
+    public class UpdateNewsHandler : IRequestHandler<UpdateNewsCommand, Result<NewsDTO>>
     {
         private readonly IRepositoryWrapper _repositoryWrapper;
         private readonly IMapper _mapper;
@@ -35,7 +35,7 @@ namespace Streetcode.BLL.MediatR.Newss.Update
             _stringLocalizerCannotConvertNull = stringLocalizerCannotConvertNull;
         }
 
-        public async Task<Result<UpdateNewsDTO>> Handle(UpdateNewsCommand request, CancellationToken cancellationToken)
+        public async Task<Result<NewsDTO>> Handle(UpdateNewsCommand request, CancellationToken cancellationToken)
         {
             var news = _mapper.Map<News>(request.news);
             if (news is null)
@@ -82,14 +82,13 @@ namespace Streetcode.BLL.MediatR.Newss.Update
                     _repositoryWrapper.ImageRepository.Delete(img);
                 }
             }
-            
-            _repositoryWrapper.NewsRepository.Update(news);
 
+            _repositoryWrapper.NewsRepository.Update(news);
             var resultIsSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
 
             if(resultIsSuccess)
             {
-                return Result.Ok(_mapper.Map<UpdateNewsDTO>(news));
+                return Result.Ok(response);
             }
             else
             {

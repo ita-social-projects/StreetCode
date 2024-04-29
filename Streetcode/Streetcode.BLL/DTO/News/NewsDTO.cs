4797e75e7152b59a49e03c80a09f4c466d8709f7
Shossy <pashka.v1331@gmail.com>
2024-04-29 19:00:28 +0300

    applied validation to create dto

--- Streetcode/Streetcode.BLL/DTO/News/NewsDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/News/NewsDTO.cs
@@ -6,15 +6,9 @@ namespace Streetcode.BLL.DTO.News
     public class NewsDTO
     {
         public int Id { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(100, ErrorMessage = "Max Length is 100")]
         public string Title { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(15000, ErrorMessage = "Max Length is 15000")]
         public string Text { get; set; }
         public int? ImageId { get; set; }
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(200, ErrorMessage = "Max Length is 200")]
         public string URL { get; set; }
         public ImageDTO? Image { get; set; }
         public DateTime CreationDate { get; set; }

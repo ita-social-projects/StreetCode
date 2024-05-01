468732c5fc8b88a8d30ceede86e33b51cec21ea1
Dmytro <dimakhved@gmail.com>
2024-05-01 19:46:19 +0300

    integrational tests, 2 not working

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Timeline/HistoricalContextClient.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using Streetcode.BLL.DTO.Timeline;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Timeline;
+
+public class HistoricalContextClient : StreetcodeRelatedBaseClient
+{
+    public HistoricalContextClient(HttpClient client, string secondPartUrl = "")
+        : base(client, secondPartUrl)
+    {
+    }
+    public async Task<RestResponse> UpdateAsync(HistoricalContextDTO updateTagDTO, string authToken = "")
+    {
+        return await this.SendCommand("/Update", Method.Put, updateTagDTO, authToken);
+    }
+
+    public async Task<RestResponse> CreateAsync(HistoricalContextDTO createTagDTO, string authToken = "")
+    {
+        return await this.SendCommand("/Create", Method.Post, createTagDTO, authToken);
+    }
+
+    public async Task<RestResponse> Delete(int id, string authToken = "")
+    {
+        return await this.SendCommand($"/Delete/{id}", Method.Delete, new HistoricalContextDTO(), authToken);
+    }
+}
---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/Timeline/HistoricalContextExtracter.cs
@@ -0,0 +1,20 @@
+﻿using Streetcode.DAL.Entities.Timeline;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.Timeline;
+
+public class HistoricalContextExtracter
+{
+    public static HistoricalContext Extract(int contextId)
+    {
+        HistoricalContext testContext = TestDataProvider.GetTestData<HistoricalContext>();
+
+        testContext.Id = contextId;
+
+        return BaseExtracter.Extract<HistoricalContext>(testContext, context => context.Id == contextId);
+    }
+
+    public static void Remove(HistoricalContext entity)
+    {
+        BaseExtracter.RemoveByPredicate<HistoricalContext>(context => context.Id == entity.Id);
+    }
+}

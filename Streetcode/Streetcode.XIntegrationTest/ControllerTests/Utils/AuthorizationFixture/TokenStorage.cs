3339f7b1b9084771dfd46f1d66e0d4b43ec9cc7d
VladyslavPavlysko <vladyslav.pavlysko.work@gmail.com>
2024-05-02 10:43:34 +0300

    flacky test fix (i hope)

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/AuthorizationFixture/TokenStorage.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/AuthorizationFixture/TokenStorage.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Streetcode.BLL.Interfaces.Authentication;
 using Streetcode.BLL.Services.Authentication;
@@ -19,27 +18,30 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Utils
 
         public TokenStorage()
         {
-            this._streetcodeDbContext = this.GetDbContext();
-            this._users = new Dictionary<string, User>();
-            this.SeedDatabaseWithInitialUsers();
+            this._users = new Dictionary<string, User>
+            {
+                ["Admin"] = TEST_USER_ADMIN,
+                ["User"] = TEST_USER_USER,
+            };
 
-            this._configuration = this.GetConfiguration();
+            this._streetcodeDbContext = GetDbContext();
+            this._configuration = GetConfiguration();
+
             this._tokenService = new TokenService(this._configuration, this._streetcodeDbContext);
 
             this.ObtainTokens();
-        }
-
-        public void Dispose()
-        {
-            this.RemoveTestUsers();
-            this._streetcodeDbContext.Dispose();
         }
 
         public string AdminToken { get; private set; }
 
         public string UserToken { get; private set; }
 
-        private StreetcodeDbContext GetDbContext()
+        public void Dispose()
+        {
+            this._streetcodeDbContext.Dispose();
+        }
+
+        private static StreetcodeDbContext GetDbContext()
         {
             var sqlConnectionString = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.IntegrationTests.json")
@@ -50,7 +52,7 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Utils
             return new StreetcodeDbContext(optionBuilder.Options);
         }
 
-        private IConfiguration GetConfiguration()
+        private static IConfiguration GetConfiguration()
         {
             Environment.SetEnvironmentVariable("STREETCODE_ENVIRONMENT", "IntegrationTests");
             var environment = Environment.GetEnvironmentVariable("STREETCODE_ENVIRONMENT") ?? "Local";
@@ -65,73 +67,6 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Utils
         {
             this.AdminToken = this._tokenService.GenerateJWTToken(this._users["Admin"]).RawData;
             this.UserToken = this._tokenService.GenerateJWTToken(this._users["User"]).RawData;
-        }
-
-        private void SeedDatabaseWithInitialUsers()
-        {
-            IdentityRole adminRole = TEST_ROLE_ADMIN;
-            IdentityRole userRole = TEST_ROLE_USER;
-            this.AddRoleToDb(adminRole);
-            this.AddRoleToDb(userRole);
-
-            this._users["Admin"] = TEST_USER_ADMIN;
-            this._users["User"] = TEST_USER_USER;
-            this.AddUserToDb(this._users["Admin"]);
-            this.AddUserToDb(this._users["User"]);
-
-            this.AddUserRole(this._users["Admin"], adminRole);
-            this.AddUserRole(this._users["User"], userRole);
-        }
-
-        private void RemoveTestUsers()
-        {
-            foreach (string key in this._users.Keys)
-            {
-                var userFromDb = this._streetcodeDbContext.Users.FirstOrDefault(user => user.Id == this._users[key].Id);
-                if (userFromDb is not null)
-                {
-                    this._streetcodeDbContext.Remove(userFromDb);
-                    this._streetcodeDbContext.SaveChanges();
-                }
-            }
-        }
-
-        private void AddRoleToDb(IdentityRole role)
-        {
-            bool exists = this._streetcodeDbContext.Roles.Any(roleFromDb => roleFromDb.Id == role.Id);
-            if (!exists)
-            {
-                this._streetcodeDbContext.Roles.Add(role);
-                this._streetcodeDbContext.SaveChanges();
-            }
-        }
-
-        private void AddUserToDb(User user)
-        {
-            bool exists = this._streetcodeDbContext.Users.Any(userFromDb => userFromDb.Id == user.Id);
-            if (!exists)
-            {
-                this._streetcodeDbContext.Users.Add(user);
-                this._streetcodeDbContext.SaveChanges();
-            }
-        }
-
-        private void AddUserRole(User user, IdentityRole role)
-        {
-            bool exists = this._streetcodeDbContext
-                .UserRoles
-                .Any(userRole => userRole.UserId == user.Id && userRole.RoleId == role.Id);
-
-            if (!exists)
-            {
-                var actualUserRole = new IdentityUserRole<string>
-                {
-                    UserId = user.Id,
-                    RoleId = role.Id,
-                };
-                this._streetcodeDbContext.UserRoles.Add(actualUserRole);
-                this._streetcodeDbContext.SaveChanges();
-            }
         }
     }
 }
---
+++ build/Utils/AuthConstants.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Streetcode.DAL.Entities.Users;
+using Streetcode.DAL.Enums;
+
+namespace Utils
+{
+    internal static class AuthConstants
+    {
+        public const string TEST_USER_LOGIN_PASSWORD = "User_Login_Password_Q123#";
+
+        public static readonly IdentityRole TEST_ROLE_ADMIN = new IdentityRole()
+        {
+            Id = "Test_Role_Admin_qwe123456rty#",
+            Name = nameof(UserRole.Admin),
+            NormalizedName = nameof(UserRole.Admin).ToUpper(),
+        };
+
+        public static readonly IdentityRole TEST_ROLE_USER = new IdentityRole()
+        {
+            Id = "Test_Role_User_qwe123456rty#",
+            Name = nameof(UserRole.User),
+            NormalizedName = nameof(UserRole.User).ToUpper(),
+        };
+
+        public static readonly User TEST_USER_ADMIN = new User()
+        {
+            Id = "Test_User_Admin_qwe123456rty#",
+            Name = "User_Admin",
+            Surname = "User_Admin",
+            Email = "user@admin.com",
+            UserName = "User_Admin_T",
+        };
+
+        public static readonly User TEST_USER_USER = new User()
+        {
+            Id = "Test_User_User_qwe123456rty#",
+            Name = "User_User",
+            Surname = "User_User",
+            Email = "user@user.com",
+            UserName = "User_User_T",
+        };
+
+        public static readonly User TEST_USER_LOGIN = new User()
+        {
+            Id = "Test_User_Login_qwe123456rty#",
+            Name = "User_Login",
+            Surname = "User_Login",
+            Email = "user@login.com",
+            UserName = "User_Login_T",
+        };
+    }
+}

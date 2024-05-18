3c30a23d6f331b74aa6e2319eb26e85ce8b95465
Dmytro <dimakhved@gmail.com>
2024-05-18 19:15:22 +0300

    add: getById request

---
+++ Streetcode/Streetcode.BLL/MediatR/Team/Position/GetById/GetByIdTeamPositionHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Team.Position.GetById;
+
+public class GetByIdTeamPositionHandler : IRequestHandler<GetByIdTeamPositionQuery, Result<PositionDTO>>
+{
+    private readonly IMapper _mapper;
+    private readonly IRepositoryWrapper _repository;
+    private readonly ILoggerService _loggerService;
+
+    public GetByIdTeamPositionHandler(IMapper mapper, IRepositoryWrapper repository, ILoggerService loggerService)
+    {
+        _mapper = mapper;
+        _repository = repository;
+        _loggerService = loggerService;
+    }
+
+    public async Task<Result<PositionDTO>> Handle(GetByIdTeamPositionQuery request, CancellationToken cancellationToken)
+    {
+        var position = await _repository.PositionRepository.GetFirstOrDefaultAsync(j => j.Id == request.positionId);
+
+        if (position is null)
+        {
+            string exceptionMessege = $"No position found by entered Id - {request.positionId}";
+            _loggerService.LogError(request, exceptionMessege);
+            return Result.Fail(exceptionMessege);
+        }
+
+        try
+        {
+            var positionDTO = _mapper.Map<PositionDTO>(position);
+            return Result.Ok(positionDTO);
+        }
+        catch (Exception ex)
+        {
+            _loggerService.LogError(request, ex.Message);
+            return Result.Fail(ex.Message);
+        }
+    }
+}

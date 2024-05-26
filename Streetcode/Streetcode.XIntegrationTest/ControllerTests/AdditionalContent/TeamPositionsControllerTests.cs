b6402e48f40b7d6beaf2b654e7debf97c0b2e9ed
Dmytro <dimakhved@gmail.com>
2024-05-26 19:45:57 +0300

    add: setted up integration tests

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/AdditionalContent/TeamPositionsControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using Streetcode.BLL.DTO.Team;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.XIntegrationTest.Base;
+using Streetcode.XIntegrationTest.ControllerTests.BaseController;
+using Streetcode.XIntegrationTest.ControllerTests.Utils;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Additional;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.AdditionalContent;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.StreetcodeExtracter;
+using Xunit;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.AdditionalContent;
+
+[Collection("Authorization")]
+public class TeamPositionsControllerTests : BaseAuthorizationControllerTests<TeamPositionsClient>,
+    IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private Positions _testCreatePosition;
+    private Positions _testUpdatePosition;
+    private StreetcodeContent _testStreetcodeContent;
+
+    public TeamPositionsControllerTests(CustomWebApplicationFactory<Program> factory, TokenStorage tokenStorage)
+        : base(factory, "/api/Position", tokenStorage)
+    {
+        int uniqueId = UniqueNumberGenerator.GenerateInt();
+        this._testCreatePosition = TeamPositionsExtracter.Extract(uniqueId);
+        this._testUpdatePosition = TeamPositionsExtracter.Extract(uniqueId);
+        this._testStreetcodeContent = StreetcodeContentExtracter
+            .Extract(
+                uniqueId,
+                uniqueId,
+                Guid.NewGuid().ToString());
+    }
+
+    public override void Dispose()
+    {
+        StreetcodeContentExtracter.Remove(this._testStreetcodeContent);
+        TeamPositionsExtracter.Remove(this._testCreatePosition);
+    }
+
+    [Fact]
+    public async Task GetAll_ReturnSuccessStatusCode()
+    {
+        var response = await this.client.GetAllAsync();
+        var returnedValue =
+            CaseIsensitiveJsonDeserializer.Deserialize<IEnumerable<PositionDTO>>(response.Content);
+
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.NotNull(returnedValue);
+    }
+}
---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Additional/TeamPositionsClient.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Additional;
+
+public class TeamPositionsClient : StreetcodeRelatedBaseClient
+{
+    public TeamPositionsClient(HttpClient client, string secondPartUrl = "")
+        : base(client, secondPartUrl)
+    {
+    }
+
+    public async Task<RestResponse> GetByTitle(string title, string authToken = "")
+    {
+        return await this.SendQuery($"/GetByTitle/{title}", authToken);
+    }
+
+    public async Task<RestResponse> UpdateAsync(PositionDTO positionDto, string authToken = "")
+    {
+        return await this.SendCommand("/Update", Method.Put, positionDto, authToken);
+    }
+
+    public async Task<RestResponse> CreateAsync(PositionDTO positionDto, string authToken = "")
+    {
+        return await this.SendCommand("/Create", Method.Post, positionDto, authToken);
+    }
+
+    public async Task<RestResponse> Delete(int id, string authToken = "")
+    {
+        return await this.SendCommand($"/Delete/{id}", Method.Delete, new PositionDTO(), authToken);
+    }
+}

b43074fb19b131c95a723f786179d16a28e25050
vasylashka <vasylashka@gmail.com>
2024-06-11 20:34:51 +0300

    Add audio validation

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/Media/Audio/CreateAudioTests.cs
@@ -0,0 +1,187 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Localization;
+using Moq;
+using Streetcode.BLL.DTO.Media.Audio;
+using Streetcode.BLL.Interfaces.BlobStorage;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Media.Audio.Create;
+using Streetcode.BLL.SharedResource;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Xunit;
+
+using AudioEntity = Streetcode.DAL.Entities.Media.Audio;
+
+namespace Streetcode.XUnitTest.MediatRTests.Media.Audio
+{
+    public class CreateAudioTests
+    {
+        private readonly Mock<IRepositoryWrapper> _mockRepository;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<ILoggerService> _mockLogger;
+        private readonly Mock<IBlobService> _mockBlobService;
+        private readonly Mock<IStringLocalizer<FailedToCreateSharedResource>> _mockLocalizerFail;
+
+        public CreateAudioTests()
+        {
+            _mockRepository = new Mock<IRepositoryWrapper>();
+            _mockMapper = new Mock<IMapper>();
+            _mockLogger = new Mock<ILoggerService>();
+            _mockBlobService = new Mock<IBlobService>();
+            _mockLocalizerFail = new Mock<IStringLocalizer<FailedToCreateSharedResource>>();
+        }
+
+        [Fact]
+        public async Task ShouldReturnSuccessfully_WhenCreated()
+        {
+            // Arrange
+            var testCreateAudioDTO = GetCreateAudioDTO();
+            var testAudioDTO = GetAudioDTO();
+            var testAudio = GetAudio();
+
+            SetupCreateRepository(1);
+            SetupBlobService();
+            SetupMapper(testAudio, testAudioDTO);
+
+            var handler = new CreateAudioHandler(_mockBlobService.Object, _mockRepository.Object, _mockLogger.Object, _mockMapper.Object, _mockLocalizerFail.Object);
+
+            // Act
+            var result = await handler.Handle(new CreateAudioCommand(testCreateAudioDTO), CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_ExtensionIsRequired()
+        {
+            // Arrange
+            var expectedError = "Extension is required";
+            _mockLocalizerFail.Setup(x => x["ExtensionIsRequired"])
+            .Returns(new LocalizedString("ExtensionIsRequired", expectedError));
+
+            var testCreateAudioDTO = GetCreateAudioDTO_WithoutExtension();
+            var testAudioDTO = GetAudioDTO();
+            var testAudio = GetAudio();
+
+            SetupCreateRepository(-1);
+            SetupBlobService();
+            SetupMapper(testAudio, testAudioDTO);
+
+            var handler = new CreateAudioHandler(_mockBlobService.Object, _mockRepository.Object, _mockLogger.Object, _mockMapper.Object, _mockLocalizerFail.Object);
+
+            // Act
+            var result = await handler.Handle(new CreateAudioCommand(testCreateAudioDTO), CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsFailed);
+            Assert.Equal(expectedError, result.Errors.First().Message);
+        }
+
+        [Fact]
+        public async Task ShouldThrowException_TitleIsRequired()
+        {
+            // Arrange
+            var expectedError = "Title is required";
+            _mockLocalizerFail.Setup(x => x["TitleIsRequired"])
+            .Returns(new LocalizedString("TitleIsRequired", expectedError));
+
+            var testCreateAudioDTO = GetCreateAudioDTO_WithoutTitle();
+            var testAudioDTO = GetAudioDTO();
+            var testAudio = GetAudio();
+
+            SetupCreateRepository(-1);
+            SetupBlobService();
+            SetupMapper(testAudio, testAudioDTO);
+
+            var handler = new CreateAudioHandler(_mockBlobService.Object, _mockRepository.Object, _mockLogger.Object, _mockMapper.Object, _mockLocalizerFail.Object);
+
+            // Act
+            var result = await handler.Handle(new CreateAudioCommand(testCreateAudioDTO), CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsFailed);
+            Assert.Equal(expectedError, result.Errors.First().Message);
+        }
+
+        private void SetupCreateRepository(int returnNumber)
+        {
+            _mockRepository.Setup(x => x.AudioRepository.Create(It.IsAny<AudioEntity>()));
+            _mockRepository.Setup(x => x.SaveChangesAsync()).ReturnsAsync(returnNumber);
+        }
+
+        private void SetupBlobService()
+        {
+            _mockBlobService.Setup(service => service.SaveFileInStorage(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>()))
+                .Returns("fake_blob_name");
+            _mockBlobService.Setup(service => service.FindFileInStorageAsBase64(
+                It.IsAny<string>()))
+                .Returns("fake_base64_string");
+        }
+
+        private void SetupMapper(AudioEntity testAudio, AudioDTO testAudioDTO)
+        {
+            _mockMapper.Setup(x => x.Map<AudioEntity>(It.IsAny<AudioFileBaseCreateDTO>()))
+                .Returns(testAudio);
+            _mockMapper.Setup(x => x.Map<AudioDTO>(It.IsAny<AudioEntity>()))
+                .Returns(testAudioDTO);
+        }
+
+        private static AudioFileBaseCreateDTO GetCreateAudioDTO()
+        {
+            return new AudioFileBaseCreateDTO()
+            {
+                Title = "Title",
+                MimeType = "string",
+                BaseFormat = "ab34",
+                Extension = "string",
+            };
+        }
+
+        private static AudioFileBaseCreateDTO GetCreateAudioDTO_WithoutExtension()
+        {
+            return new AudioFileBaseCreateDTO()
+            {
+                Title = "Title",
+                MimeType = "string",
+                BaseFormat = "ab34",
+            };
+        }
+
+        private static AudioFileBaseCreateDTO GetCreateAudioDTO_WithoutTitle()
+        {
+            return new AudioFileBaseCreateDTO()
+            {
+                MimeType = "string",
+                BaseFormat = "ab34",
+                Extension = "string",
+            };
+        }
+
+        private static AudioDTO GetAudioDTO()
+        {
+            return new AudioDTO()
+            {
+                Id = 1,
+                BlobName = "fake_blob_name",
+                Base64 = "fake_base64_string",
+                MimeType = "string",
+            };
+        }
+
+        private static DAL.Entities.Media.Audio GetAudio()
+        {
+            return new AudioEntity()
+            {
+                Id = 1,
+                BlobName = "hzbTZ58ebTjpDJDCWosy5F55WRkZU0cl+1Gpo_NWJ+0=.string",
+                Base64 = "ab34",
+                MimeType = "string",
+            };
+        }
+
+
+    }
+}

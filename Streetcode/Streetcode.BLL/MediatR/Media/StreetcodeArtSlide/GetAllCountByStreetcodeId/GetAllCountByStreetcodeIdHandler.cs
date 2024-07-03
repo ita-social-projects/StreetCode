159f5e026a37a1322423e12ec0702ed837ba4461
VladyslavPavlysko <vladyslav.pavlysko.work@gmail.com>
2024-07-03 14:26:10 +0300

    get number of slides

---
+++ Streetcode/Streetcode.BLL/MediatR/Media/StreetcodeArtSlide/GetAllCountByStreetcodeId/GetAllCountByStreetcodeIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Media.Art.StreetcodeArtSlide.GetAllCountByStreetcodeId
+{
+    public class GetAllCountByStreetcodeIdHandler : IRequestHandler<GetAllCountByStreetcodeIdQuerry, Result<int>>
+    {
+        private readonly IRepositoryWrapper _repositoryWrapper;
+
+        public GetAllCountByStreetcodeIdHandler(
+            IRepositoryWrapper repositoryWrapper)
+        {
+            _repositoryWrapper = repositoryWrapper;
+        }
+
+        public async Task<Result<int>> Handle(GetAllCountByStreetcodeIdQuerry request, CancellationToken cancellationToken)
+        {
+            var slidesCount = _repositoryWrapper.StreetcodeArtSlideRepository
+                .FindAll(
+                    predicate: sArtSlide => sArtSlide.StreetcodeId == request.StreetcodeId)
+                .Count();
+
+            return Result.Ok(slidesCount);
+        }
+    }
+}

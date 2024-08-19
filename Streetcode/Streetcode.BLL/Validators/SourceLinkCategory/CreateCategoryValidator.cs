1b3aba6a552f978f9d9dad62b7722766ca0ce043
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-08-19 15:13:21 +0300

    add validators for category

---
+++ Streetcode/Streetcode.BLL/Validators/SourceLinkCategory/CreateCategoryValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Streetcode.BLL.DTO.Sources;
+using Streetcode.BLL.MediatR.Sources.SourceLink.Create;
+
+namespace Streetcode.BLL.Validators.SourceLinkCategory;
+
+public class CreateCategoryValidator : AbstractValidator<SourceLinkCreateUpdateCategoryDTO>, IValidator<CreateCategoryCommand>
+{
+    public CreateCategoryValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title cannot be empty.")
+            .MaximumLength(23).WithMessage("Title cannot be longer than 23 characters.")
+            .Matches(@"\S").WithMessage("Title cannot be whitespace.");
+    }
+
+    public ValidationResult Validate(CreateCategoryCommand instance)
+    {
+        return Validate(instance.Category);
+    }
+
+    public Task<ValidationResult> ValidateAsync(CreateCategoryCommand instance, CancellationToken cancellation = default)
+    {
+        return ValidateAsync(instance.Category, cancellation);
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validators/SourceLinkCategory/UpdateCategoryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Streetcode.BLL.MediatR.Sources.SourceLink.Update;
+
+namespace Streetcode.BLL.Validators.SourceLinkCategory;
+
+public class UpdateCategoryValidator : CreateCategoryValidator, IValidator<UpdateCategoryCommand>
+{
+    public ValidationResult Validate(UpdateCategoryCommand instance)
+    {
+        return Validate(instance.Category);
+    }
+
+    public Task<ValidationResult> ValidateAsync(UpdateCategoryCommand instance, CancellationToken cancellation = new CancellationToken())
+    {
+        return ValidateAsync(instance.Category, cancellation);
+    }
+}

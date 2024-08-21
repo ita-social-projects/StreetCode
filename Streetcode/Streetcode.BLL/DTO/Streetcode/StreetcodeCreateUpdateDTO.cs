7428c9e75375b690d3b4eba5ebba9e9a37b84cb4
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-08-22 00:56:34 +0300

    add abstract dto for streetcode

---
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/StreetcodeCreateUpdateDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Streetcode.DAL.Enums;
+
+namespace Streetcode.BLL.DTO.Streetcode;
+
+public abstract class StreetcodeCreateUpdateDTO
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Index { get; set; }
+    public string? Teaser { get; set; }
+    public string DateString { get; set; }
+    public string? Alias { get; set; }
+    public StreetcodeStatus Status { get; set; }
+    public StreetcodeType StreetcodeType { get; set; }
+    public string Title { get; set; }
+    public string TransliterationUrl { get; set; }
+    public string? EventStartOrPersonBirthDate { get; set; }
+    public string? EventEndOrPersonDeathDate { get; set; }
+}
--- Streetcode/Streetcode.BLL/DTO/Streetcode/Update/StreetcodeUpdateDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Streetcode/Update/StreetcodeUpdateDTO.cs
@@ -18,21 +18,9 @@ using Streetcode.DAL.Enums;
 
 namespace Streetcode.BLL.DTO.Streetcode.Update
 {
-    public class StreetcodeUpdateDTO
+    public class StreetcodeUpdateDTO : StreetcodeCreateUpdateDTO
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public int Index { get; set; }
-        public string? Teaser { get; set; }
-        public string DateString { get; set; }
-        public string? Alias { get; set; }
-        public StreetcodeStatus Status { get; set; }
-        public StreetcodeType StreetcodeType { get; set; }
-        public string Title { get; set; }
-        public string TransliterationUrl { get; set; }
-        public DateTime EventStartOrPersonBirthDate { get; set; }
-        public DateTime? EventEndOrPersonDeathDate { get; set; }
         public TextUpdateDTO? Text { get; set; }
         public TransactionLinkUpdateDTO? TransactionLink { get; set; }
         public IEnumerable<StreetcodeToponymUpdateDTO> Toponyms { get; set; }

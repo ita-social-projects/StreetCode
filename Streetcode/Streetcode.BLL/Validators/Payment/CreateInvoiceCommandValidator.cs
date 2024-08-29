16cb8d4302f5c0b7d34c406d6734c468f8111449
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-08-29 21:11:30 +0300

    implemented a validator for invoices

---
+++ Streetcode/Streetcode.BLL/Validators/Payment/CreateInvoiceCommandValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Streetcode.BLL.MediatR.Payment;
+using Streetcode.BLL.SharedResource;
+using Streetcode.BLL.Validators.Common;
+
+namespace Streetcode.BLL.Validators.Payment;
+
+public class CreateInvoiceCommandValidator : AbstractValidator<CreateInvoiceCommand>
+{
+    private const int AmountGreaterThan = 0;
+
+    public CreateInvoiceCommandValidator(
+        IStringLocalizer<FieldNamesSharedResource> fieldLocalizer,
+        IStringLocalizer<FailedToValidateSharedResource> localizer)
+    {
+        RuleFor(c => c.Payment.RedirectUrl)
+            .NotEmpty().WithMessage(x => localizer["CannotBeEmpty", fieldLocalizer["RedirectUrl"]])
+            .MustBeValidUrl().WithMessage(x => localizer["ValidUrl", fieldLocalizer["RedirectUrl"]]);
+
+        RuleFor(c => c.Payment.Amount)
+            .GreaterThan(AmountGreaterThan).WithMessage(x => localizer["GreaterThan", fieldLocalizer["RedirectUrl"], AmountGreaterThan]);
+    }
+}

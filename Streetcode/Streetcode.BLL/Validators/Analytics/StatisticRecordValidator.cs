baa41753de0eb8d28ef9f63ddfc9f25dc814bd43
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-08-30 12:05:21 +0300

    refined validation logic for statistic records

--- Streetcode/Streetcode.BLL/Validators/Analytics/StatisticRecordValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/Analytics/StatisticRecordValidator.cs
@@ -10,6 +10,15 @@ public class StatisticRecordValidator : AbstractValidator<CreateStatisticRecordC
     public StatisticRecordValidator(IStringLocalizer<CreateStatisticRecordCommand> localizer)
     {
         RuleFor(st => st.StatisticRecordDTO.Address)
-            .NotEmpty().MaximumLength(150).WithMessage(localizer["AddressStatisticRecordMaximumLength"]);
+            .NotEmpty().WithMessage(localizer["AddressRequired"])
+            .MaximumLength(150).WithMessage(localizer["AddressStatisticRecordMaximumLength"]);
+
+        RuleFor(st => st.StatisticRecordDTO.StreetcodeCoordinate.Latitude)
+            .NotNull().WithMessage(localizer["LatitudeRequired"])
+            .PrecisionScale(18, 4, true).WithMessage(localizer["LatitudeInvalidPrecision"]);
+
+        RuleFor(st => st.StatisticRecordDTO.StreetcodeCoordinate.Longtitude)
+            .NotNull().WithMessage(localizer["LongtitudeRequired"])
+            .PrecisionScale(18, 4, true).WithMessage(localizer["LongtitudeInvalidPrecision"]);
     }
 }

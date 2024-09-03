917fcbb22e49c5dc94a8f7cdedb73500e8a05e62
DDeviak <117632082+DDeviak@users.noreply.github.com>
2024-09-03 14:03:06 +0300

    chore: Made ImageId in UpdateNewsDTO required

--- Streetcode/Streetcode.BLL/DTO/News/UpdateNewsDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/News/UpdateNewsDTO.cs
@@ -15,7 +15,7 @@ namespace Streetcode.BLL.DTO.News
         [StringLength(15000, ErrorMessage = "Max Length is 15000")]
         public string Text { get; set; }
 
-        public int? ImageId { get; set; }
+        public int ImageId { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         [StringLength(200, ErrorMessage = "Max Length is 200")]

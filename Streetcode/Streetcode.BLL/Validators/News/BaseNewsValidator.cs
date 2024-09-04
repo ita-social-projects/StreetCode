4e6d7a2ccd46469537b0893d27fff42a6cdcc831
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-09-04 10:53:34 +0300

    Add localized messages for news and statistics validators

--- Streetcode/Streetcode.BLL/Validators/News/BaseNewsValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/News/BaseNewsValidator.cs
@@ -2,37 +2,42 @@ using FluentValidation;
 using Microsoft.Extensions.Localization;
 using Streetcode.BLL.DTO.News;
 using Streetcode.BLL.SharedResource;
+using Streetcode.BLL.Validators.Common;
 using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.Validators.News;
 
 public class BaseNewsValidator : AbstractValidator<CreateUpdateNewsDTO>
 {
+    private const int TitleMaxLength = 100;
+    private const int UrlMaxLength = 200;
+    private const int ImageIdMinValue = 0;
     private readonly IRepositoryWrapper _repositoryWrapper;
-    public BaseNewsValidator(IStringLocalizer<FailedToValidateSharedResource> localizer, IRepositoryWrapper repositoryWrapper)
+    public BaseNewsValidator(IStringLocalizer<FailedToValidateSharedResource> localizer, IStringLocalizer<FieldNamesSharedResource> fieldLocalizer, IRepositoryWrapper repositoryWrapper)
     {
         _repositoryWrapper = repositoryWrapper;
 
         RuleFor(x => x.Title)
-                .NotEmpty().WithMessage(x => localizer["TitleRequired"])
-                .MaximumLength(100).WithMessage(x => localizer["TitleMaxLength"])
-                .MustAsync(BeUniqueTitle).WithMessage(x => localizer["TitleNewsAlreadyExists"]);
+                .NotEmpty().WithMessage(x => localizer["CannotBeEmpty", fieldLocalizer["Title"]])
+                .MaximumLength(TitleMaxLength).WithMessage(x => localizer["MaxLength", fieldLocalizer["Title"], TitleMaxLength])
+                .MustAsync(BeUniqueTitle).WithMessage(x => localizer["MustBeUnique", fieldLocalizer["Title"]]);
 
         RuleFor(x => x.Text)
-                .NotEmpty().WithMessage(x => localizer["TextRequired"])
-                .MustAsync(BeUniqueText).WithMessage(x => localizer["TextNewsAlreadyExists"]);
+                .NotEmpty().WithMessage(localizer["CannotBeEmpty", fieldLocalizer["Text"]])
+                .MustAsync(BeUniqueText).WithMessage(localizer["MustBeUnique", fieldLocalizer["Text"]]);
 
         RuleFor(x => x.ImageId)
-                .GreaterThan(0).WithMessage(x => localizer["InvalidImageIdValue"])
-                .MustAsync(BeExistingImageId).WithMessage(x => localizer["ImageIdNotExists"]);
+                .GreaterThan(ImageIdMinValue).WithMessage(x => localizer["Invalid", fieldLocalizer["ImageId"]])
+                .MustAsync(BeExistingImageId).WithMessage(x => localizer["ImageDoesntExist", x.ImageId]);
 
         RuleFor(x => x.CreationDate)
-                .NotEmpty().WithMessage(x => localizer["CreationDateRequired"]);
+                .NotEmpty().WithMessage(x => localizer["IsRequired", fieldLocalizer["CreationDate"]]);
 
         RuleFor(x => x.URL)
-                .NotEmpty().WithMessage(x => localizer["URLRequired"])
-                .Matches("^[a-zA-Z0-9-]*$").WithMessage(x => localizer["URLInvalid"])
-                .MustAsync(BeUniqueUrl).WithMessage(x => localizer["URLAlreadyExistsNews"]);
+                .NotEmpty().WithMessage(x => localizer["CannotBeEmpty", fieldLocalizer["TargetUrl"]])
+                .MaximumLength(UrlMaxLength).WithMessage(localizer["MaxLength", fieldLocalizer["TargetUrl"], UrlMaxLength])
+                .Matches("^[a-zA-Z0-9-]*$").WithMessage(x => localizer["InvalidNewsUrl"])
+                .MustAsync(BeUniqueUrl!).WithMessage(x => localizer["MustBeUnique", fieldLocalizer["TargetUrl"]]);
     }
 
     private async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)

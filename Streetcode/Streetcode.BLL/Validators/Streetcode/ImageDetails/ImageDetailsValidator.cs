f2c2a81c1b8a32a55f4475eaeb7f4e2508b59c95
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-09-04 10:55:11 +0300

    added a validation check for the imageId uniqueness in the ImageDetails validator

--- Streetcode/Streetcode.BLL/Validators/Streetcode/ImageDetails/ImageDetailsValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/Streetcode/ImageDetails/ImageDetailsValidator.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using Microsoft.Extensions.Localization;
 using Streetcode.BLL.DTO.Media.Images;
 using Streetcode.BLL.SharedResource;
+using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.Validators.Streetcode.ImageDetails;
 
@@ -9,11 +10,28 @@ public class ImageDetailsValidator : AbstractValidator<ImageDetailsDto>
 {
     private const int TitleMaxLength = 100;
     private const int AltMaxLength = 200;
-    public ImageDetailsValidator(IStringLocalizer<FailedToValidateSharedResource> localizer, IStringLocalizer<FieldNamesSharedResource> fieldLocalizer)
+    private IRepositoryWrapper _repositoryWrapper;
+    public ImageDetailsValidator(
+        IStringLocalizer<FailedToValidateSharedResource> localizer,
+        IStringLocalizer<FieldNamesSharedResource> fieldLocalizer,
+        IRepositoryWrapper repositoryWrapper)
     {
+        _repositoryWrapper = repositoryWrapper;
+
         RuleFor(dto => dto.Title)
             .MaximumLength(TitleMaxLength).WithMessage(localizer["MaxLength", fieldLocalizer["ImageTitle"], TitleMaxLength]);
+
         RuleFor(dto => dto.Alt)
             .MaximumLength(AltMaxLength).WithMessage(localizer["MaxLength", fieldLocalizer["Alt"], AltMaxLength]);
+
+        RuleFor(dto => dto.ImageId)
+            .MustAsync(BeUniqueImageIdInImageDetails);
+    }
+
+    private async Task<bool> BeUniqueImageIdInImageDetails(int imadeId, CancellationToken cancellationToken)
+    {
+        var existingStreetcodeByIndex = await _repositoryWrapper.ImageDetailsRepository.GetFirstOrDefaultAsync(n => n.ImageId == imadeId);
+
+        return existingStreetcodeByIndex is null;
     }
 }

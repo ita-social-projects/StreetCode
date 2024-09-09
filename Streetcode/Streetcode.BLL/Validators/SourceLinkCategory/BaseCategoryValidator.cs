8069d2e29143c7ee4fa05981e5ba2bd9fe23f38a
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2024-09-10 00:18:05 +0300

    fix uniqueness check for categories

--- Streetcode/Streetcode.BLL/Validators/SourceLinkCategory/BaseCategoryValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/SourceLinkCategory/BaseCategoryValidator.cs
@@ -15,18 +15,10 @@ public class BaseCategoryValidator : AbstractValidator<SourceLinkCreateUpdateCat
         _repositoryWrapper = repositoryWrapper;
         RuleFor(x => x.Title)
             .NotEmpty().WithMessage(localizer["CannotBeEmpty", fieldLocalizer["Title"]])
-            .MaximumLength(MaxCategoryLength).WithMessage(localizer["MaxLength", fieldLocalizer["Title"], MaxCategoryLength])
-            .MustAsync(BeUniqueAsync).WithMessage(localizer["MustBeUnique", fieldLocalizer["Title"]]);
+            .MaximumLength(MaxCategoryLength).WithMessage(localizer["MaxLength", fieldLocalizer["Title"], MaxCategoryLength]);
 
         RuleFor(x => x.ImageId)
             .MustAsync(HasExistingImage).WithMessage(x => localizer["ImageDoesntExist", x.ImageId]);
-    }
-
-    public async Task<bool> BeUniqueAsync(string categoryTitle, CancellationToken token)
-    {
-        var existingCategory = await _repositoryWrapper.SourceCategoryRepository
-            .GetFirstOrDefaultAsync(a => a.Title == categoryTitle);
-        return existingCategory == null;
     }
 
     public async Task<bool> HasExistingImage(int imageId, CancellationToken token)

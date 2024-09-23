3212a74a2288079c3d9a99358de8b9ad34ed5ad8
ulyasaur <ulyano4ka2003@gmail.com>
2024-09-23 09:47:26 +0300

    add pagination to tags

--- Streetcode/Streetcode.BLL/MediatR/AdditionalContent/Tag/GetAll/GetAllTagsQuery.cs
+++ Streetcode/Streetcode.BLL/MediatR/AdditionalContent/Tag/GetAll/GetAllTagsQuery.cs
@@ -1,7 +1,8 @@
 using FluentResults;
 using MediatR;
 using Streetcode.BLL.DTO.AdditionalContent;
+using Streetcode.BLL.DTO.AdditionalContent.Tag;
 
 namespace Streetcode.BLL.MediatR.AdditionalContent.Tag.GetAll;
 
-public record GetAllTagsQuery : IRequest<Result<IEnumerable<TagDTO>>>;
+public record GetAllTagsQuery(ushort? page = null, ushort? pageSize = null): IRequest<Result<GetAllTagsResponseDTO>>;

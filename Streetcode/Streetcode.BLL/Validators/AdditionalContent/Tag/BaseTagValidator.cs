7f818a0e34b6766f29512812293642aec4a00908
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-09-26 11:30:59 +0300

    added tests for tags validators

--- Streetcode/Streetcode.BLL/Validators/AdditionalContent/Tag/BaseTagValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/AdditionalContent/Tag/BaseTagValidator.cs
@@ -8,24 +8,14 @@ namespace Streetcode.BLL.Validators.AdditionalContent.Tag;
 
 public class BaseTagValidator : AbstractValidator<CreateUpdateTagDTO>
 {
-    private const int TitleMaxLength = 50;
-    private readonly IRepositoryWrapper _repositoryWrapper;
+    public const int TitleMaxLength = 50;
     public BaseTagValidator(
-        IRepositoryWrapper repositoryWrapper,
         IStringLocalizer<FailedToValidateSharedResource> localizer,
         IStringLocalizer<FieldNamesSharedResource> fieldLocalizer)
     {
-        _repositoryWrapper = repositoryWrapper;
         RuleFor(t => t.Title)
             .NotEmpty().WithMessage(x => localizer["CannotBeEmpty", fieldLocalizer["Title"]])
             .MaximumLength(TitleMaxLength)
             .WithMessage(x => localizer["MaxLength", fieldLocalizer["Title"], TitleMaxLength]);
-    }
-
-    private async Task<bool> BeUniqueTitle(string title, CancellationToken cancellationToken)
-    {
-        var existingTagByTitle = await _repositoryWrapper.TagRepository.GetFirstOrDefaultAsync(t => t.Title == title);
-
-        return existingTagByTitle is null;
     }
 }

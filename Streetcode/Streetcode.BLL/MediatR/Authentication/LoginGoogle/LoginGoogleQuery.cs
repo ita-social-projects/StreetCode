5e641e1ce81eb094e9413534e09a2adf5d22c70f
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-12-26 19:13:40 +0200

    integrated Google as a login provider

---
+++ Streetcode/Streetcode.BLL/MediatR/Authentication/LoginGoogle/LoginGoogleQuery.cs
@@ -0,0 +1,8 @@
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Authentication.Login;
+
+namespace Streetcode.BLL.MediatR.Authentication.LoginGoogle;
+
+public record LoginGoogleQuery(string idToken)
+    : IRequest<Result<LoginResponseDTO>>;

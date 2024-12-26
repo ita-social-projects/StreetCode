4399ef77ec53f688483c82597a0c8b8fbb41a44e
Daria Kravchuk <daria1kravchuk@gmail.com>
2024-12-26 19:16:53 +0200

    added endpoints for Google Login and User Logout functionality

--- Streetcode/Streetcode.WebApi/Controllers/Authentication/AuthController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Authentication/AuthController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Authentication.Login;
 using Streetcode.BLL.DTO.Authentication.RefreshToken;
 using Streetcode.BLL.DTO.Authentication.Register;
 using Streetcode.BLL.MediatR.Authentication.Login;
+using Streetcode.BLL.MediatR.Authentication.LoginGoogle;
+using Streetcode.BLL.MediatR.Authentication.Logout;
 using Streetcode.BLL.MediatR.Authentication.RefreshToken;
 using Streetcode.BLL.MediatR.Authentication.Register;
 
@@ -30,6 +34,41 @@ namespace Streetcode.WebApi.Controllers.Authentication
         public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequestDTO token)
         {
             return HandleResult(await Mediator.Send(new RefreshTokenQuery(token)));
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Logout()
+        {
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized("User is not authenticated.");
+            }
+
+            var result = await Mediator.Send(new LogoutCommand(userId));
+
+            if (result.IsFailed)
+            {
+                return BadRequest(result.Errors.First().Message);
+            }
+
+            return Ok("Logout successful. Refresh token invalidated.");
+        }
+
+        [HttpPost("google-login")]
+        public async Task<IActionResult> GoogleLogin([FromBody] string idToken)
+        {
+            var result = await Mediator.Send(new LoginGoogleQuery(idToken));
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Value);
+            }
+
+            return Unauthorized(new { message = result.Errors.FirstOrDefault()?.Message });
         }
     }
 }

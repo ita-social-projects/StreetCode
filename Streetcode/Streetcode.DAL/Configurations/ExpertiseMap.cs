58df4f9cb6ee7b1c060baa934ad866b5fecc9497
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2025-01-06 09:32:38 +0200

    added new fluent api configurations

---
+++ Streetcode/Streetcode.DAL/Configurations/ExpertiseMap.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.Users;
+using Streetcode.DAL.Entities.Users.Expertise;
+
+namespace Streetcode.DAL.Configurations;
+
+public class ExpertiseMap : IEntityTypeConfiguration<Expertise>
+{
+    public void Configure(EntityTypeBuilder<Expertise> builder)
+    {
+        //builder
+        //    .HasMany(x => x.Positions)
+        //    .WithMany(x => x.TeamMembers)
+        //    .UsingEntity<TeamMemberPositions>(
+        //        tp => tp.HasOne(x => x.Positions)
+        //            .WithMany()
+        //            .HasForeignKey(x => x.PositionsId),
+        //        tp => tp.HasOne(x => x.TeamMember)
+        //            .WithMany()
+        //            .HasForeignKey(x => x.TeamMemberId));
+
+
+        builder
+            .HasMany(x => x.Users)
+            .WithMany(x => x.Expertises)
+            .UsingEntity<UserExpertise>(
+            ue => ue
+                            .HasOne(x => x.User)
+                            .WithMany()
+                            .HasForeignKey(x => x.UserId)
+                            .OnDelete(DeleteBehavior.Cascade), 
+                        ue => ue
+                            .HasOne(x => x.Expertise)
+                            .WithMany()
+                            .HasForeignKey(x => x.ExpertiseId)
+                            .OnDelete(DeleteBehavior.Cascade)
+            );
+    }
+}
---
+++ Streetcode/Streetcode.DAL/Configurations/UserMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.Media.Images;
+using Streetcode.DAL.Entities.Users;
+
+namespace Streetcode.DAL.Configurations;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .HasOne(u => u.Avatar)
+            .WithOne(i => i.User)
+            .HasForeignKey<Image>(i => i.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

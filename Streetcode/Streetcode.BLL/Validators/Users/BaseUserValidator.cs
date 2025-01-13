4c2b671a0a13f0e1dc616bd54954caf1bee68a79
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2025-01-13 11:09:20 +0200

    added user validators

---
+++ Streetcode/Streetcode.BLL/Validators/Users/BaseUserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Streetcode.BLL.DTO.Users;
+using Streetcode.BLL.SharedResource;
+
+namespace Streetcode.BLL.Validators.Users;
+
+public class BaseUserValidator : AbstractValidator<UpdateUserDTO>
+{
+    public BaseUserValidator()
+    {
+        RuleFor(dto => dto.Expertises).Must(e => e.Count <= 3).WithMessage("Less than 3");
+
+        RuleFor(dto => dto.AboutYourself).MaximumLength(500);
+
+        RuleFor(dto => dto.Name).MinimumLength(3);
+
+        RuleFor(dto => dto.Surname).MinimumLength(3);
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validators/Users/UpdateUserValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Users.Update;
+
+namespace Streetcode.BLL.Validators.Users;
+
+public class UpdateUserValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserValidator(BaseUserValidator baseUserValidator)
+    {
+        RuleFor(dto => dto.UserDto).SetValidator(baseUserValidator);
+    }
+}

167dad20c0e6851e86746d0c2ea5639d062935c8
lisaaksionova bc2024-6 <lisaaksionova@gmail.com>
2025-01-23 11:26:07 +0200

    added integration tests for TransactLinkController

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Extracter/Transaction/TransactLinkExtracter.cs
@@ -0,0 +1,25 @@
+﻿using Streetcode.DAL.Entities.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.Transaction
+{
+    public static class TransactLinkExtracter
+    {
+        public static TransactionLink Extract(int transactId)
+        {
+            TransactionLink transactionLink = TestDataProvider.GetTestData<TransactionLink>();
+            transactionLink.Id = transactId;
+
+            return BaseExtracter.Extract<TransactionLink>(transactionLink, transact => transact.Id == transactId);
+        }
+
+        public static void Remove(TransactionLink transact)
+        {
+            BaseExtracter.RemoveByPredicate<TransactionLink>(t => t.Id == transact.Id);
+        }
+    }
+}

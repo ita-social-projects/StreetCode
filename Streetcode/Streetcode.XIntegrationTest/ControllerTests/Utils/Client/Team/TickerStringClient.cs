05e8779d016f35b37ca683464bef6d76979b46a8
lisaaksionova bc2024-6 <lisaaksionova@gmail.com>
2025-01-29 12:54:03 +0200

    added tickerstring unit & integration tests

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Team/TickerStringClient.cs
@@ -0,0 +1,14 @@
+﻿using RestSharp;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Team
+{
+    public class TickerStringClient(HttpClient client, string secondPartUrl = "")
+        : BaseClient(client, secondPartUrl)
+    {
+        public async Task<RestResponse> GetNameTickerString(string authToken = "")
+        {
+            return await this.SendQuery("/GetNameTickerString/", authToken);
+        }
+    }
+}

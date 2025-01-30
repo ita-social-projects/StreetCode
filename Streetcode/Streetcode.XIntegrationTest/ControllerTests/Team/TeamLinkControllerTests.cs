4213a40bc04a10754ec45d3bdbcf5a077053ea30
lisaaksionova bc2024-6 <lisaaksionova@gmail.com>
2025-01-31 01:09:51 +0200

    added teamLink unit & integration tests

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Team/TeamLinkControllerTests.cs
@@ -0,0 +1,137 @@
+﻿using System.Net;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.DAL.Entities.Media.Images;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.XIntegrationTest.Base;
+using Streetcode.XIntegrationTest.ControllerTests.BaseController;
+using Streetcode.XIntegrationTest.ControllerTests.Utils;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Team;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Team;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.AdditionalContent;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.MediaExtracter.Image;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.Team;
+using Xunit;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Team
+{
+    [Collection("Authorization")]
+    public class TeamLinkControllerTests : BaseAuthorizationControllerTests<TeamLinkClient>,
+        IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly Image _testImage;
+        private readonly TeamMember _testTeamMember;
+        private readonly TeamMemberLink _testLink;
+
+        public TeamLinkControllerTests(CustomWebApplicationFactory<Program> factory, TokenStorage tokenStorage)
+            : base(factory, "api/TeamLink", tokenStorage)
+        {
+            int uniqueId = UniqueNumberGenerator.GenerateInt();
+            _testImage = ImageExtracter.Extract(uniqueId);
+            _testTeamMember = TeamMemberExtracter.Extract(uniqueId, _testImage.Id);
+            _testTeamMember.ImageId = _testImage.Id;
+            _testLink = TeamLinkExtracter.Extract(uniqueId, _testTeamMember.Id);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsSuccessStatusCode()
+        {
+            // Act
+            var response = await this.Client.GetAllAsync();
+            var returnedValue = CaseIsensitiveJsonDeserializer.Deserialize<IEnumerable<TeamMemberLinkDTO>>(response.Content);
+
+            // Assert
+            Assert.Multiple(
+                () => Assert.True(response.IsSuccessStatusCode),
+                () => Assert.NotNull(returnedValue));
+        }
+
+        [Fact]
+        [ExtractCreateTeamLink]
+        public async Task Create_ReturnSuccessStatusCode()
+        {
+            // Act
+            var teamLinkCreateDto = ExtractCreateTeamLinkAttribute.TeamLinkForTest;
+            teamLinkCreateDto.TeamMemberId = _testTeamMember.Id;
+
+            // Act
+            var response = await this.Client.CreateAsync(teamLinkCreateDto, this.TokenStorage.AdminAccessToken);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        [ExtractCreateTeamLink]
+        public async Task Create_TokenNotPassed_ReturnsUnauthorized()
+        {
+            // Act
+            var teamLinkCreateDto = ExtractCreateTeamLinkAttribute.TeamLinkForTest;
+
+            // Act
+            var response = await this.Client.CreateAsync(teamLinkCreateDto);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        [ExtractCreateTeamLink]
+        public async Task Create_NotAdminTokenPassed_ReturnsForbidden()
+        {
+            // Act
+            var teamLinkCreateDto = ExtractCreateTeamLinkAttribute.TeamLinkForTest;
+            teamLinkCreateDto.TeamMemberId = _testTeamMember.Id;
+
+            // Act
+            var response = await this.Client.CreateAsync(teamLinkCreateDto, this.TokenStorage.UserAccessToken);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Fact]
+        [ExtractCreateTeamLink]
+        public async Task Create_CreatesNewTeamMemberLink()
+        {
+            // Act
+            var teamLinkCreateDto = ExtractCreateTeamLinkAttribute.TeamLinkForTest;
+            teamLinkCreateDto.TeamMemberId = _testTeamMember.Id;
+
+            // Act
+            var response = await this.Client.CreateAsync(teamLinkCreateDto, this.TokenStorage.AdminAccessToken);
+            var fetchedLink = CaseIsensitiveJsonDeserializer.Deserialize<TeamMemberLinkDTO>(response.Content);
+
+            // Assert
+            Assert.Multiple(
+                () => Assert.Equal(teamLinkCreateDto.LogoType, fetchedLink?.LogoType),
+                () => Assert.Equal(teamLinkCreateDto.TargetUrl, fetchedLink?.TargetUrl));
+        }
+
+        [Fact]
+        [ExtractCreateTeamLink]
+        public async Task Create_WithInvalidData_ReturnsBadRequest()
+        {
+            // Arrange
+            var teamLinkCreateDto = ExtractCreateTeamLinkAttribute.TeamLinkForTest;
+            teamLinkCreateDto.TargetUrl = null!;
+
+            // Act
+            var response = await this.Client.CreateAsync(teamLinkCreateDto, this.TokenStorage.AdminAccessToken);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                TeamMemberExtracter.Remove(_testTeamMember);
+                ImageExtracter.Remove(_testImage);
+                TeamLinkExtracter.Remove(_testLink);
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/BeforeAndAfterTestAtribute/Team/ExtractCreateTeamLinkAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using Streetcode.BLL.DTO.Team;
+using Streetcode.DAL.Entities.Team;
+using Streetcode.XIntegrationTest.ControllerTests.BaseController;
+using Xunit.Sdk;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Team
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class ExtractCreateTeamLinkAttribute : BeforeAfterTestAttribute
+    {
+        public static TeamMemberLinkCreateDTO TeamLinkForTest { get; set; } = null!;
+
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            TeamLinkForTest = new TeamMemberLinkCreateDTO
+            {
+                LogoType = DAL.Enums.LogoType.Instagram,
+                TargetUrl = "https://instagram.com/testCreate",
+            };
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            var sqlDbHelper = BaseControllerTests.GetSqlDbHelper();
+            var context = sqlDbHelper.GetExistItem<TeamMemberLink>(t => t.TargetUrl == TeamLinkForTest.TargetUrl);
+            if (context != null)
+            {
+                sqlDbHelper.DeleteItem(context);
+                sqlDbHelper.SaveChanges();
+            }
+        }
+    }
+}

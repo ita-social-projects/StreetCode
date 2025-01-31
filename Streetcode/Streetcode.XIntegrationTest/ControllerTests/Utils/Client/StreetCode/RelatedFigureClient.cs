9e78790b3e8333c5f41406f312a86576a3626136
JuliaAndreyeva <blackonblackua@gmail.com>
2025-01-31 11:35:16 +0200

    feat: added RelatedFigure client for integration tests

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/StreetCode/RelatedFigureClient.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.StreetCode;
+
+public class RelatedFigureClient : StreetcodeRelatedBaseClient
+{
+    public RelatedFigureClient(
+        HttpClient client,
+        string secondPartUrl = "")
+        : base(client, secondPartUrl)
+    {
+    }
+
+    public async Task<RestResponse> GetByTagId(int id, string authToken = "")
+    {
+        return await SendQuery($"/GetByTagId/{id}", authToken);
+    }
+
+    public async Task<RestResponse> Create(int observerId, int targetId, string authToken = "")
+    {
+        var requestBody = new { observerId, targetId };
+        return await SendCommand($"/Create/{observerId}&{targetId}", Method.Post, requestBody, authToken);
+    }
+
+    public async Task<RestResponse> Delete(int observerId, int targetId, string authToken = "")
+    {
+        var requestBody = new { observerId, targetId };
+        return await SendCommand($"/Delete/{observerId}&{targetId}", Method.Delete, requestBody, authToken);
+    }
+}

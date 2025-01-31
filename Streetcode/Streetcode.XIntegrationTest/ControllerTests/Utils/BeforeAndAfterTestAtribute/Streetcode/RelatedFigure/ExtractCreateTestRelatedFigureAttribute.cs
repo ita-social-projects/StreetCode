b38c9444da35287dc446d0b2ddf5c94b2203da51
JuliaAndreyeva <blackonblackua@gmail.com>
2025-01-31 11:36:52 +0200

    feat: added extracter and attributes for testing related figure

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/BeforeAndAfterTestAtribute/Streetcode/RelatedFigure/ExtractCreateTestRelatedFigureAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using Streetcode.DAL.Entities.Streetcode;
+using Streetcode.XIntegrationTest.Base;
+using Streetcode.XIntegrationTest.ControllerTests.BaseController;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.StreetcodeExtracter;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Extracter.StreetcodeExtracter.RelatedFigure;
+using Xunit.Sdk;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.BeforeAndAfterTestAtribute.Streetcode.RelatedFigure;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public class ExtractCreateTestRelatedFigureAttribute : BeforeAfterTestAttribute
+{
+    public static StreetcodeContent StreetcodeContent1 { get; private set; } = null!;
+
+    public static StreetcodeContent StreetcodeContent2 { get; private set; } = null!;
+
+    public override void Before(MethodInfo methodUnderTest)
+    {
+        int observerId = UniqueNumberGenerator.GenerateInt();
+        int targetId = UniqueNumberGenerator.GenerateInt();
+        StreetcodeContent1 = StreetcodeContentExtracter
+            .Extract(
+                observerId,
+                observerId,
+                Guid.NewGuid().ToString());
+        StreetcodeContent2 = StreetcodeContentExtracter
+            .Extract(
+                targetId,
+                targetId,
+                Guid.NewGuid().ToString());
+    }
+
+    public override void After(MethodInfo methodUnderTest)
+    {
+        var sqlDbHelper = BaseControllerTests.GetSqlDbHelper();
+        var relatedFigures = sqlDbHelper.GetAll<DAL.Entities.Streetcode.RelatedFigure>(
+            rf => rf.ObserverId == StreetcodeContent1.Id || rf.TargetId == StreetcodeContent2.Id);
+        foreach (var relatedFigure in relatedFigures)
+        {
+            RelatedFigureExtracter.Remove(relatedFigure);
+        }
+
+        StreetcodeContentExtracter.Remove(StreetcodeContent1);
+        StreetcodeContentExtracter.Remove(StreetcodeContent2);
+    }
+}

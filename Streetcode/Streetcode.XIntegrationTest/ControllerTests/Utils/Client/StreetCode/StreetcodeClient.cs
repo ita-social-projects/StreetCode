d8f8f9ca0bfe44c30448c98a283e66b5832cdaf8
JuliaAndreyeva <blackonblackua@gmail.com>
2025-01-31 11:35:37 +0200

    feat: added new commands/queries for StreetcodeClient.cs

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/StreetCode/StreetcodeClient.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/StreetCode/StreetcodeClient.cs
@@ -1,6 +1,11 @@
 ﻿using RestSharp;
+using Streetcode.BLL.DTO.AdditionalContent.Filter;
+using Streetcode.BLL.DTO.Streetcode;
 using Streetcode.BLL.DTO.Streetcode.Create;
 using Streetcode.BLL.DTO.Streetcode.Update;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.Delete;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.DeleteSoft;
+using Streetcode.BLL.MediatR.Streetcode.Streetcode.UpdateStatus;
 using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
 
 namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.StreetCode
@@ -12,19 +17,90 @@ namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.StreetCode
         {
         }
 
-        public async Task<RestResponse> GetArtsByStreetcodeId(int id, string authToken = "")
+        public async Task<RestResponse> UpdateAsync(StreetcodeUpdateDTO updateStreetcodeDto, string authToken = "")
         {
-            return await this.SendQuery($"/getArtsByStreetcodeId/{id}", authToken);
+            return await SendCommand("/Update", Method.Put, updateStreetcodeDto, authToken);
         }
 
-        public async Task<RestResponse> UpdateAsync(StreetcodeUpdateDTO updateStreetcodeDTO, string authToken = "")
+        public async Task<RestResponse> CreateAsync(StreetcodeCreateDTO createStreetcodeDto, string authToken = "")
         {
-            return await this.SendCommand("/Update", Method.Put, updateStreetcodeDTO, authToken);
+            return await SendCommand("/Create", Method.Post, createStreetcodeDto, authToken);
         }
 
-        public async Task<RestResponse> CreateAsync(StreetcodeCreateDTO createStreetcodeDTO, string authToken = "")
+        public async Task<RestResponse> GetAllAsync(GetAllStreetcodesRequestDTO request)
         {
-            return await this.SendCommand("/Create", Method.Post, createStreetcodeDTO, authToken);
+            string queryString = QueryStringHelper<GetAllStreetcodesRequestDTO>.ToQueryString(request);
+            return await SendQuery($"/GetAll{queryString}");
+        }
+
+        public async Task<RestResponse> GetAllPublishedAsync() => await SendQuery("/GetAllPublished");
+
+        public async Task<RestResponse> GetAllShortAsync() => await SendQuery("/GetAllShort");
+
+        public async Task<RestResponse> GetAllMainPageAsync() => await SendQuery("/GetAllMainPage");
+
+        public async Task<RestResponse> GetPageMainPageAsync(ushort page, ushort pageSize)
+        {
+            return await SendQuery($"/GetPageMainPage?page={page}&pageSize={pageSize}");
+        }
+
+        public async Task<RestResponse> GetShortByIdAsync(int id) => await SendQuery($"/GetShortById/{id}");
+
+        public async Task<RestResponse> GetByFilterAsync(StreetcodeFilterRequestDTO request)
+        {
+            string queryString = QueryStringHelper<StreetcodeFilterRequestDTO>.ToQueryString(request);
+            return await SendQuery($"/GetByFilter{queryString}");
+        }
+
+        public async Task<RestResponse> ExistWithIndexAsync(int index)
+        {
+            return await SendQuery($"/ExistWithIndex/{index}");
+        }
+
+        public async Task<RestResponse> ExistWithUrlAsync(string url)
+        {
+            return await SendQuery($"/ExistWithUrl/{url}");
+        }
+
+        public async Task<RestResponse> GetAllCatalogAsync(int page, int count)
+        {
+            return await this.SendQuery($"/GetAllCatalog?page={page}&count={count}");
+        }
+
+        public async Task<RestResponse> GetCountAsync(bool? onlyPublished)
+        {
+            var query = onlyPublished.HasValue ? $"?onlyPublished={onlyPublished.Value}" : string.Empty;
+            return await SendQuery($"/GetCount{query}");
+        }
+
+        public async Task<RestResponse> GetByTransliterationUrl(string url)
+        {
+            return await this.SendQuery($"/GetByTransliterationUrl/{url}");
+        }
+
+        public async Task<RestResponse> GetByQrIdAsync(int qrid)
+        {
+            return await SendQuery($"/GetByQrId/{qrid}");
+        }
+
+        public async Task<RestResponse> GetByIndexAsync(int index)
+        {
+            return await this.SendQuery($"/GetByIndex/{index}");
+        }
+
+        public async Task<RestResponse> PatchStageAsync(UpdateStatusStreetcodeByIdCommand request, string authToken = "")
+        {
+            return await SendCommand($"/PatchStage/{request.Id}/{request.Status}", Method.Put, request, authToken);
+        }
+
+        public async Task<RestResponse> SoftDeleteAsync(DeleteSoftStreetcodeCommand request, string authToken = "")
+        {
+            return await SendCommand<object>($"/SoftDelete/{request.Id}", Method.Delete, request, authToken);
+        }
+
+        public async Task<RestResponse> DeleteAsync(DeleteStreetcodeCommand request, string authToken = "")
+        {
+            return await SendCommand<object>($"/Delete/{request.Id}", Method.Delete, request, authToken);
         }
     }
 }

6453f7832aa397506aaa84f816affc3fcab88670
JuliaAndreyeva <blackonblackua@gmail.com>
2025-01-31 11:38:41 +0200

    feat: added helper query string builder

---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/QueryStringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils;
+
+public static class QueryStringHelper<T>
+{
+    public static string ToQueryString(T obj)
+    {
+        if (obj is null)
+        {
+            return string.Empty;
+        }
+
+        var properties = obj.GetType().GetProperties()
+            .Where(p => p.GetValue(obj) != null)
+            .Select(p => $"{HttpUtility.UrlEncode(p.Name)}={HttpUtility.UrlEncode(p.GetValue(obj)?.ToString())}");
+
+        var enumerable = properties as string[] ?? properties.ToArray();
+        return enumerable.Any() ? "?" + string.Join("&", enumerable) : string.Empty;
+    }
+}

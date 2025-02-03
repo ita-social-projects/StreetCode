b5b89c1c29c9e66431a8511629c28f616e443c73
lisaaksionova bc2024-6 <lisaaksionova@gmail.com>
2025-02-03 21:42:48 +0200

    changed favourites repository file location

--- Streetcode/Streetcode.DAL/Repositories/Interfaces/Streetcode/Favourites/IFavouritesRepository.cs
+++ Streetcode/Streetcode.DAL/Repositories/Interfaces/Streetcode/Favourites/IFavouritesRepository.cs
@@ -1,7 +1,7 @@
 ﻿using Streetcode.DAL.Repositories.Interfaces.Base;
 using FavouriteStreetcodes = Streetcode.DAL.Entities.Users.Favourites.Favourites;
 
-namespace Streetcode.DAL.Repositories.Interfaces.Users.Favourites
+namespace Streetcode.DAL.Repositories.Interfaces.Streetcode.Favourites
 {
     public interface IFavouritesRepository : IRepositoryBase<FavouriteStreetcodes>
     {
--- Streetcode/Streetcode.DAL/Repositories/Realizations/Streetcode/FavouritesRepository.cs
+++ Streetcode/Streetcode.DAL/Repositories/Realizations/Streetcode/FavouritesRepository.cs
@@ -1,9 +1,9 @@
 ﻿using Streetcode.DAL.Persistence;
-using Streetcode.DAL.Repositories.Interfaces.Users.Favourites;
+using Streetcode.DAL.Repositories.Interfaces.Streetcode.Favourites;
 using Streetcode.DAL.Repositories.Realizations.Base;
 using FavouriteStreetcodes = Streetcode.DAL.Entities.Users.Favourites.Favourites;
 
-namespace Streetcode.DAL.Repositories.Realizations.Users
+namespace Streetcode.DAL.Repositories.Realizations.Streetcode
 {
     public class FavouritesRepository : RepositoryBase<FavouriteStreetcodes>, IFavouritesRepository
     {

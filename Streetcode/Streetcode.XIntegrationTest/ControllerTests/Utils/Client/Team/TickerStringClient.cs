123ef49f25fd723570017bbd38e4a4f94d8881df
Yelyzaveta Aksionova <135411053+lisaaksionova@users.noreply.github.com>
2025-02-03 11:28:02 +0200

    removed primary constructor TickerStringClient.cs

--- Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Team/TickerStringClient.cs
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Team/TickerStringClient.cs
@@ -3,9 +3,12 @@ using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
 
 namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Team
 {
-    public class TickerStringClient(HttpClient client, string secondPartUrl = "")
-        : BaseClient(client, secondPartUrl)
+    public class TickerStringClient : BaseClient
     {
+        public TickerStringClient(HttpClient client, string secondPartUrl = "")
+            : base(client, secondPartUrl)
+        {
+        }
         public async Task<RestResponse> GetNameTickerString(string authToken = "")
         {
             return await this.SendQuery("/GetNameTickerString/", authToken);

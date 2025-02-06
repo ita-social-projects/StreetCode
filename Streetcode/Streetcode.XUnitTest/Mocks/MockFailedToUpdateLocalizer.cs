be900e34b1e4045e335e69fb701683e1d5d3213c
JuliaAndreyeva <blackonblackua@gmail.com>
2025-02-06 17:00:44 +0200

    feat: added unit tests for UpdateStatusStreetcodeByIdHandler

---
+++ Streetcode/Streetcode.XUnitTest/Mocks/MockFailedToUpdateLocalizer.cs
@@ -0,0 +1,19 @@
+﻿using Streetcode.BLL.SharedResource;
+
+namespace Streetcode.XUnitTest.Mocks;
+
+public class MockFailedToUpdateLocalizer : BaseMockStringLocalizer<FailedToUpdateSharedResource>
+{
+    protected override Dictionary<int, List<string>> DefineGroupedErrors()
+    {
+        return new Dictionary<int, List<string>>
+        {
+            {
+                0, new List<string>
+                {
+                    "FailedToUpdateStatusOfStreetcode",
+                }
+            },
+        };
+    }
+}

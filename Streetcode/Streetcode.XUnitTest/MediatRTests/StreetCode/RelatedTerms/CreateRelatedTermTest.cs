2620e1b00e79099654f1149fe51701214da53806
Michael-Kolpakov <mihailkolp@gmail.com>
2025-02-12 02:24:37 +0200

    Implemented unit tests for Related Term handlers

---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedTerms/CreateRelatedTermTest.cs
@@ -0,0 +1,232 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.Streetcode.TextContent;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.RelatedTerm.Create;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.Mocks;
+using Xunit;
+using RelatedTermEntity = Streetcode.DAL.Entities.Streetcode.TextContent.RelatedTerm;
+
+namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerms;
+
+public class CreateRelatedTermTest
+{
+    private readonly Mock<IRepositoryWrapper> _mockRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly Mock<ILoggerService> _mockLogger;
+    private readonly MockCannotSaveLocalizer _mockCannotSaveLocalizer;
+    private readonly MockCannotMapLocalizer _mockCannotMapLocalizer;
+    private readonly MockCreateRelatedTermLocalizer _mockCreateRelatedTermLocalizer;
+    private readonly MockCannotCreateLocalizer _mockCannotCreateLocalizer;
+    private readonly CreateRelatedTermHandler _handler;
+
+    public CreateRelatedTermTest()
+    {
+        _mockRepository = new Mock<IRepositoryWrapper>();
+        _mockMapper = new Mock<IMapper>();
+        _mockLogger = new Mock<ILoggerService>();
+        _mockCannotSaveLocalizer = new MockCannotSaveLocalizer();
+        _mockCannotMapLocalizer = new MockCannotMapLocalizer();
+        _mockCreateRelatedTermLocalizer = new MockCreateRelatedTermLocalizer();
+        _mockCannotCreateLocalizer = new MockCannotCreateLocalizer();
+        _handler = new CreateRelatedTermHandler(
+            _mockRepository.Object,
+            _mockMapper.Object,
+            _mockLogger.Object,
+            _mockCannotSaveLocalizer,
+            _mockCannotMapLocalizer,
+            _mockCreateRelatedTermLocalizer,
+            _mockCannotCreateLocalizer);
+    }
+
+    [Fact]
+    public async Task ShouldCreateSuccessfully_WhenRelatedTermAdded()
+    {
+        // Arrange
+        var (relatedTermCreateDto, relatedTerm, relatedTermDto) = GetRelatedTermObjects();
+        var existingTerms = GetEmptyRelatedTermList();
+        var request = GetRequest(relatedTermCreateDto);
+
+        SetupMockGetAllAsync(request, existingTerms);
+        SetupMockCreateAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTerm, request.RelatedTerm);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTermDto, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Word.Should().Be(relatedTerm.Word);
+        _mockRepository.Verify(r => r.RelatedTermRepository.CreateAsync(relatedTerm), Times.Once);
+        _mockRepository.Verify(r => r.SaveChangesAsync(), Times.Once);
+        _mockMapper.Verify(x => x.Map<RelatedTermDTO>(relatedTerm), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldCreateSuccessfully_WithCorrectDataType()
+    {
+        // Arrange
+        var (relatedTermCreateDto, relatedTerm, relatedTermDto) = GetRelatedTermObjects();
+        var existingTerms = GetEmptyRelatedTermList();
+        var request = GetRequest(relatedTermCreateDto);
+
+        SetupMockGetAllAsync(request, existingTerms);
+        SetupMockCreateAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTerm, request.RelatedTerm);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTermDto, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.ValueOrDefault.Should().BeOfType<RelatedTermDTO>();
+    }
+
+    [Fact]
+    public async Task ShouldCreateFailingly_WhenFirstMappingFailed()
+    {
+        // Arrange
+        var (relatedTermCreateDto, _, _) = GetRelatedTermObjects();
+        var request = GetRequest(relatedTermCreateDto);
+        var expectedErrorMessage = _mockCannotCreateLocalizer["CannotCreateNewRelatedWordForTerm"].Value;
+
+        MockHelpers.SetupMockMapper<RelatedTermEntity?, RelatedTermCreateDTO>(_mockMapper, null, request.RelatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldCreateFailingly_WhenRelatedTermAlreadyExists()
+    {
+        // Arrange
+        var (relatedTermCreateDto, relatedTerm, _) = GetRelatedTermObjects();
+        var existingTerms = GetRelatedTermList();
+        var request = GetRequest(relatedTermCreateDto);
+        var expectedErrorMessage = _mockCreateRelatedTermLocalizer["WordWithThisDefinitionAlreadyExists"].Value;
+
+        SetupMockGetAllAsync(request, existingTerms);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTerm, request.RelatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldCreateFailingly_WhenSaveChangesAsyncFailed()
+    {
+        // Arrange
+        var (relatedTermCreateDto, relatedTerm, _) = GetRelatedTermObjects();
+        var existingTerms = GetEmptyRelatedTermList();
+        var request = GetRequest(relatedTermCreateDto);
+        var expectedErrorMessage = _mockCannotSaveLocalizer["CannotSaveChangesInTheDatabaseAfterRelatedWordCreation"].Value;
+
+        SetupMockGetAllAsync(request, existingTerms);
+        SetupMockCreateAndSaveChangesAsync(relatedTerm, -1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTerm, request.RelatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    [Fact]
+    public async Task ShouldCreateFailingly_WhenSecondMappingFailed()
+    {
+        // Arrange
+        var (relatedTermCreateDto, relatedTerm, _) = GetRelatedTermObjects();
+        var existingTerms = GetEmptyRelatedTermList();
+        var request = GetRequest(relatedTermCreateDto);
+        var expectedErrorMessage = _mockCannotMapLocalizer["CannotMapEntity"].Value;
+
+        SetupMockGetAllAsync(request, existingTerms);
+        SetupMockCreateAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTerm, request.RelatedTerm);
+        MockHelpers.SetupMockMapper<RelatedTermDTO?, RelatedTermEntity>(_mockMapper, null, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    private static (RelatedTermCreateDTO, RelatedTermEntity, RelatedTermDTO) GetRelatedTermObjects()
+    {
+        const string word = "qwerty";
+
+        var relatedTermCreateDto = new RelatedTermCreateDTO()
+        {
+            Word = word,
+        };
+        var relatedTerm = new RelatedTermEntity()
+        {
+            Word = word,
+        };
+        var relatedTermDto = new RelatedTermDTO()
+        {
+            Word = word,
+        };
+
+        return (relatedTermCreateDto, relatedTerm, relatedTermDto);
+    }
+
+    private static List<RelatedTermEntity> GetRelatedTermList()
+    {
+        return new List<RelatedTermEntity>()
+        {
+            new RelatedTermEntity(),
+            new RelatedTermEntity(),
+        };
+    }
+
+    private static List<RelatedTermEntity> GetEmptyRelatedTermList()
+    {
+        return new List<RelatedTermEntity>();
+    }
+
+    private static CreateRelatedTermCommand GetRequest(RelatedTermCreateDTO relatedTermDto)
+    {
+        return new CreateRelatedTermCommand(relatedTermDto);
+    }
+
+    private void SetupMockGetAllAsync(CreateRelatedTermCommand request, List<RelatedTermEntity> getAllAsyncResult)
+    {
+        _mockRepository
+            .Setup(x => x.RelatedTermRepository.GetAllAsync(
+                x => x.TermId == request.RelatedTerm.TermId && x.Word == request.RelatedTerm.Word,
+                It.IsAny<Func<IQueryable<RelatedTermEntity>, IIncludableQueryable<RelatedTermEntity, object>>>()))
+            .ReturnsAsync(getAllAsyncResult);
+    }
+
+    private void SetupMockCreateAndSaveChangesAsync(RelatedTermEntity relatedTerm, int saveChangesResult)
+    {
+        _mockRepository
+            .Setup(x => x.RelatedTermRepository.CreateAsync(relatedTerm))
+            .ReturnsAsync(relatedTerm);
+        _mockRepository
+            .Setup(x => x.SaveChangesAsync())
+            .ReturnsAsync(saveChangesResult);
+    }
+}
---
+++ Streetcode/Streetcode.XUnitTest/MediatRTests/StreetCode/RelatedTerms/DeleteRelatedTermTest.cs
@@ -0,0 +1,179 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query;
+using Moq;
+using Streetcode.BLL.DTO.Streetcode.TextContent;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Streetcode.RelatedTerm.Delete;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+using Streetcode.XUnitTest.Mocks;
+using Xunit;
+using RelatedTermEntity = Streetcode.DAL.Entities.Streetcode.TextContent.RelatedTerm;
+
+namespace Streetcode.XUnitTest.MediatRTests.StreetCode.RelatedTerms;
+
+public class DeleteRelatedTermTest
+{
+    private readonly Mock<IRepositoryWrapper> _mockRepository;
+    private readonly Mock<IMapper> _mockMapper;
+    private readonly Mock<ILoggerService> _mockLogger;
+    private readonly MockFailedToDeleteLocalizer _mockFailedToDeleteLocalizer;
+    private readonly MockCannotFindLocalizer _mockCannotFindLocalizer;
+    private readonly DeleteRelatedTermHandler _handler;
+
+    public DeleteRelatedTermTest()
+    {
+        _mockRepository = new Mock<IRepositoryWrapper>();
+        _mockMapper = new Mock<IMapper>();
+        _mockLogger = new Mock<ILoggerService>();
+        _mockFailedToDeleteLocalizer = new MockFailedToDeleteLocalizer();
+        _mockCannotFindLocalizer = new MockCannotFindLocalizer();
+        _handler = new DeleteRelatedTermHandler(
+            _mockRepository.Object,
+            _mockMapper.Object,
+            _mockLogger.Object,
+            _mockFailedToDeleteLocalizer,
+            _mockCannotFindLocalizer);
+    }
+
+    [Theory]
+    [InlineData("qwerty")]
+    public async Task ShouldDeleteSuccessfully_WhenRelatedTermExists(string word)
+    {
+        // Arrange
+        var (relatedTerm, relatedTermDto) = GetRelatedTermObjects(word);
+        var request = GetRequest(word);
+
+        SetupMockGetFirstOrDefaultAsync(request, relatedTerm);
+        SetupMockDeleteAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTermDto, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Word.Should().Be(word);
+        _mockRepository.Verify(x => x.RelatedTermRepository.Delete(relatedTerm), Times.Once);
+        _mockRepository.Verify(x => x.SaveChangesAsync(), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("qwerty")]
+    public async Task ShouldDeleteSuccessfully_WithCorrectDataType(string word)
+    {
+        // Arrange
+        var (relatedTerm, relatedTermDto) = GetRelatedTermObjects(word);
+        var request = GetRequest(word);
+
+        SetupMockGetFirstOrDefaultAsync(request, relatedTerm);
+        SetupMockDeleteAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper(_mockMapper, relatedTermDto, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.ValueOrDefault.Should().BeOfType<RelatedTermDTO>();
+    }
+
+    [Theory]
+    [InlineData("qwerty")]
+    public async Task ShouldDeleteFailingly_WhenRelatedTermNotExist(string word)
+    {
+        // Arrange
+        var request = GetRequest(word);
+        var expectedErrorMessage = _mockCannotFindLocalizer["CannotFindRelatedTermWithCorrespondingId", request.word].Value;
+
+        SetupMockGetFirstOrDefaultAsync(request, null);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("qwerty")]
+    public async Task ShouldDeleteFailingly_WhenSaveChangesAsyncFailed(string word)
+    {
+        // Arrange
+        var (relatedTerm, _) = GetRelatedTermObjects(word);
+        var request = GetRequest(word);
+        var expectedErrorMessage = _mockFailedToDeleteLocalizer["FailedToDeleteRelatedTerm"].Value;
+
+        SetupMockGetFirstOrDefaultAsync(request, relatedTerm);
+        SetupMockDeleteAndSaveChangesAsync(relatedTerm, -1);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("qwerty")]
+    public async Task ShouldDeleteFailingly_WhenMappingFailed(string word)
+    {
+        // Arrange
+        var (relatedTerm, _) = GetRelatedTermObjects(word);
+        var request = GetRequest(word);
+        var expectedErrorMessage = _mockFailedToDeleteLocalizer["FailedToDeleteRelatedTerm"].Value;
+
+        SetupMockGetFirstOrDefaultAsync(request, relatedTerm);
+        SetupMockDeleteAndSaveChangesAsync(relatedTerm, 1);
+        MockHelpers.SetupMockMapper<RelatedTermDTO?, RelatedTermEntity>(_mockMapper, null, relatedTerm);
+
+        // Act
+        var result = await _handler.Handle(request, CancellationToken.None);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors[0].Message.Should().Be(expectedErrorMessage);
+        _mockLogger.Verify(x => x.LogError(request, expectedErrorMessage), Times.Once);
+    }
+
+    private static (RelatedTermEntity, RelatedTermDTO) GetRelatedTermObjects(string word)
+    {
+        var relatedTerm = new RelatedTermEntity()
+        {
+            Word = word,
+        };
+        var relatedTermDto = new RelatedTermDTO()
+        {
+            Word = word,
+        };
+
+        return (relatedTerm, relatedTermDto);
+    }
+
+    private static DeleteRelatedTermCommand GetRequest(string word)
+    {
+        return new DeleteRelatedTermCommand(word);
+    }
+
+    private void SetupMockGetFirstOrDefaultAsync(DeleteRelatedTermCommand request, RelatedTermEntity? getFirstOrDefaultAsyncResult)
+    {
+        _mockRepository
+            .Setup(x => x.RelatedTermRepository.GetFirstOrDefaultAsync(
+                x => x.Word!.ToLower().Equals(request.word.ToLower()),
+                It.IsAny<Func<IQueryable<RelatedTermEntity>, IIncludableQueryable<RelatedTermEntity, object>>>()))
+            .ReturnsAsync(getFirstOrDefaultAsyncResult);
+    }
+
+    private void SetupMockDeleteAndSaveChangesAsync(RelatedTermEntity relatedTerm, int saveChangesResult)
+    {
+        _mockRepository
+            .Setup(x => x.RelatedTermRepository.Delete(relatedTerm));
+        _mockRepository
+            .Setup(x => x.SaveChangesAsync())
+            .ReturnsAsync(saveChangesResult);
+    }
+}

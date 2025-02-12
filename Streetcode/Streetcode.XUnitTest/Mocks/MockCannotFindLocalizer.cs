6c371ed66920401916d3e62f82f2a2c5218b611c
Michael-Kolpakov <mihailkolp@gmail.com>
2025-02-12 02:21:03 +0200

    Setuped localizer and mapper mocks

---
+++ Streetcode/Streetcode.XUnitTest/Mocks/MockCannotFindLocalizer.cs
@@ -0,0 +1,34 @@
+﻿using Streetcode.BLL.SharedResource;
+
+namespace Streetcode.XUnitTest.Mocks;
+
+public class MockCannotFindLocalizer : BaseMockStringLocalizer<CannotFindSharedResource>
+{
+    protected override Dictionary<int, List<string>> DefineGroupedErrors()
+    {
+        var groupedErrors = new Dictionary<int, List<string>>()
+        {
+            {
+                0, new List<string>()
+                {
+                    "CannotFindAnyFact",
+                    "CannotFindAnyTerm",
+                    "CannotFindAnyText",
+                }
+            },
+            {
+                1, new List<string>()
+                {
+                    "CannotFindFactWithCorrespondingCategoryId",
+                    "CannotFindRelatedTermWithCorrespondingId",
+                    "CannotFindAnyTermWithCorrespondingId",
+                    "CannotFindTextWithCorrespondingCategoryId",
+                    "CannotFindAnyTextWithCorrespondingId",
+                    "CannotFindTransactionLinkByStreetcodeIdBecause",
+                }
+            },
+        };
+
+        return groupedErrors;
+    }
+}

1214f91ea92f4ea548575c2f8bc4911058de85d1
vlad-zhadan <zhadan.vld@gmail.com>
2025-03-01 19:39:35 +0200

    add access manager for video controller

--- Streetcode/Streetcode.BLL/MediatR/Media/Video/GetByStreetcodeId/GetVideoByStreetcodeIdHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Media/Video/GetByStreetcodeId/GetVideoByStreetcodeIdHandler.cs
@@ -1,10 +1,12 @@
-﻿using AutoMapper;
+﻿using System.Linq.Expressions;
+using AutoMapper;
 using FluentResults;
 using MediatR;
 using Microsoft.Extensions.Localization;
 using Streetcode.BLL.DTO.Media.Video;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.MediatR.ResultVariations;
+using Streetcode.BLL.Services.EntityAccessManager;
 using Streetcode.BLL.SharedResource;
 using Streetcode.DAL.Entities.Streetcode;
 using Streetcode.DAL.Repositories.Interfaces.Base;
@@ -28,8 +30,11 @@ public class GetVideoByStreetcodeIdHandler : IRequestHandler<GetVideoByStreetcod
 
     public async Task<Result<VideoDTO>> Handle(GetVideoByStreetcodeIdQuery request, CancellationToken cancellationToken)
     {
-        var video = await _repositoryWrapper.VideoRepository
-            .GetFirstOrDefaultAsync(video => video.StreetcodeId == request.StreetcodeId);
+        Expression<Func<DAL.Entities.Media.Video, bool>>? basePredicate = v => v.StreetcodeId == request.StreetcodeId;
+        var predicate = basePredicate.ExtendWithAccessPredicate(new StreetcodeAccessManager(), request.UserRole, v => v.Streetcode);
+
+        var video = await _repositoryWrapper.VideoRepository.GetFirstOrDefaultAsync(predicate: predicate);
+
         if(video == null)
         {
             StreetcodeContent? streetcode = await _repositoryWrapper.StreetcodeRepository.GetFirstOrDefaultAsync(x => x.Id == request.StreetcodeId);

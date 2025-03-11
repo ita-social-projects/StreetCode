e0bd0e09cb2e9e4071db7e00805df3e09a01cffd
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2025-03-11 21:43:20 +0200

    Implemented integration tests for Events controller

--- Streetcode/Streetcode.WebApi/Controllers/Event/EventsController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Event/EventsController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Streetcode.BLL.DTO.Event;
 using Streetcode.BLL.DTO.Event.CreateUpdate;
---
+++ Streetcode/Streetcode.XIntegrationTest/ControllerTests/Utils/Client/Event/EventClient.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using Streetcode.BLL.DTO.Event.CreateUpdate;
+using Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Base;
+
+namespace Streetcode.XIntegrationTest.ControllerTests.Utils.Client.Event
+{
+    public class EventClient : BaseClient
+    {
+        public EventClient(HttpClient client, string secondPartUrl = "/api/Events")
+            : base(client, secondPartUrl)
+        {
+        }
+
+        public async Task<RestResponse> GetAllAsync() => await this.SendQuery("/GetAll");
+
+        public async Task<RestResponse> GetByIdAsync(int id) => await this.SendQuery($"/GetById/{id}");
+
+        public async Task<RestResponse> CreateAsync(CreateUpdateEventDTO eventDto, string authToken = "")
+            => await this.SendCommand($"/Create", Method.Post, eventDto, authToken);
+
+        public async Task<RestResponse> DeleteAsync(int id, string authToken = "")
+        {
+            var dto = new { Id = id };
+            return await this.SendCommand($"/Delete/{id}", Method.Delete, dto, authToken);
+        }
+    }
+}

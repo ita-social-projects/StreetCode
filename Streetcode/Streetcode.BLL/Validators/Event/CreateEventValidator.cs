42f5bfd1896ddbd0b6f5c97fe206b9302ad46e45
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2025-03-13 09:53:00 +0200

    Added validation for creating/updating Event

---
+++ Streetcode/Streetcode.BLL/Validators/Event/CreateEventValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Event.Create;
+
+namespace Streetcode.BLL.Validators.Event;
+
+public class CreateEventValidator : AbstractValidator<CreateEventCommand>
+{
+    public CreateEventValidator(BaseEventValidator baseEventValidator)
+    {
+        RuleFor(e => e.Event).SetValidator(baseEventValidator);
+    }
+}
---
+++ Streetcode/Streetcode.BLL/Validators/Event/UpdateEventValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using Streetcode.BLL.MediatR.Event.Update;
+
+namespace Streetcode.BLL.Validators.Event;
+public class UpdateEventValidator : AbstractValidator<UpdateEventCommand>
+{
+    public UpdateEventValidator(BaseEventValidator baseEventValidator)
+    {
+        RuleFor(e => e.Event).SetValidator(baseEventValidator);
+    }
+}

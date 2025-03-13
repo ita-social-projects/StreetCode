20e0fbc67ac9014e74aa5f359b23728468302ffb
IlliaBonkovskyi <bonkovskyiillia@gmail.com>
2025-03-13 11:46:36 +0200

    Refactored Create and Update Event handlers

--- Streetcode/Streetcode.BLL/MediatR/Event/Create/CreateEventHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Event/Create/CreateEventHandler.cs
@@ -4,9 +4,11 @@ using MediatR;
 using Microsoft.Extensions.Localization;
 using Streetcode.BLL.Factories.Event;
 using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.BLL.MediatR.Event.Update;
 using Streetcode.BLL.SharedResource;
 using Streetcode.DAL.Entities.Event;
 using Streetcode.DAL.Repositories.Interfaces.Base;
+using EventEntity = Streetcode.DAL.Entities.Event.Event;
 
 namespace Streetcode.BLL.MediatR.Event.Create
 {
@@ -40,30 +42,14 @@ namespace Streetcode.BLL.MediatR.Event.Create
                     var eventEntity = EventFactory.CreateEvent(request.Event.EventType);
                     _mapper.Map(request.Event, eventEntity);
 
-                    if (eventEntity is HistoricalEvent historicalEvent)
-                    {
-                        historicalEvent.TimelineItemId = request.Event.TimelineItemId;
-                    }
-                    else if(eventEntity is CustomEvent customEvent)
-                    {
-                        customEvent.Location = request.Event.Location;
-                        customEvent.Organizer = request.Event.Organizer;
-                    }
+                    SetSpecialEventFields(eventEntity, request);
 
                     _repositoryWrapper.EventRepository.Create(eventEntity);
                     var isResultSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
 
                     if (isResultSuccess)
                     {
-                        if (request.Event.StreetcodeIds != null && request.Event.StreetcodeIds.Any())
-                        {
-                            var eventStreetcodes = request.Event.StreetcodeIds
-                                .Select(id => new EventStreetcodes { EventId = eventEntity.Id, StreetcodeId = id })
-                                .ToList();
-
-                            await _repositoryWrapper.EventStreetcodesRepository.CreateRangeAsync(eventStreetcodes);
-                            await _repositoryWrapper.SaveChangesAsync();
-                        }
+                        await SetEventStreetcodes(eventEntity, request);
 
                         transactionScope.Complete();
                         return Result.Ok(eventEntity.Id);
@@ -82,6 +68,36 @@ namespace Streetcode.BLL.MediatR.Event.Create
                     return Result.Fail(new Error(errorMsg));
                 }
             }
+        }
+
+        private void SetSpecialEventFields(EventEntity eventToCreate, CreateEventCommand request)
+        {
+            _mapper.Map(request.Event, eventToCreate);
+
+            if (eventToCreate is HistoricalEvent historicalEvent)
+            {
+                historicalEvent.TimelineItemId = request.Event.TimelineItemId;
+            }
+            else if (eventToCreate is CustomEvent customEvent)
+            {
+                customEvent.Location = request.Event.Location;
+                customEvent.Organizer = request.Event.Organizer;
+            }
+        }
+
+        private async Task SetEventStreetcodes(EventEntity eventEntity, CreateEventCommand request)
+        {
+            if (request.Event.StreetcodeIds == null)
+            {
+                return;
+            }
+
+            var eventStreetcodes = request.Event.StreetcodeIds
+                                .Select(id => new EventStreetcodes { EventId = eventEntity.Id, StreetcodeId = id })
+                                .ToList();
+
+            await _repositoryWrapper.EventStreetcodesRepository.CreateRangeAsync(eventStreetcodes);
+            await _repositoryWrapper.SaveChangesAsync();
         }
     }
 }
--- Streetcode/Streetcode.BLL/MediatR/Event/Update/UpdateEventHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Event/Update/UpdateEventHandler.cs
@@ -7,7 +7,9 @@ using Streetcode.BLL.Factories.Event;
 using Streetcode.BLL.Interfaces.Logging;
 using Streetcode.BLL.SharedResource;
 using Streetcode.DAL.Entities.Event;
+using Streetcode.DAL.Enums;
 using Streetcode.DAL.Repositories.Interfaces.Base;
+using EventEntity = Streetcode.DAL.Entities.Event.Event;
 
 namespace Streetcode.BLL.MediatR.Event.Update
 {
@@ -54,66 +56,16 @@ namespace Streetcode.BLL.MediatR.Event.Update
                         return Result.Fail(exMessage);
                     }
 
-                    bool eventTypeChanged = eventToUpdate.EventType != request.Event.EventType.ToString();
-
-                    if(eventTypeChanged)
+                    if (HasEventTypeChanged(eventToUpdate, request.Event.EventType))
                     {
-                        if (eventToUpdate is HistoricalEvent historicalEvent)
-                        {
-                            historicalEvent.TimelineItemId = null;
-                        }
-                        else if (eventToUpdate is CustomEvent customEvent)
-                        {
-                            customEvent.Location = null;
-                            customEvent.Organizer = null;
-                        }
-
-                        eventToUpdate = EventFactory.CreateEvent(request.Event.EventType);
-                        eventToUpdate.Id = request.Event.Id;
+                        eventToUpdate = HandleEventTypeChange(eventToUpdate, request.Event.EventType);
                     }
 
                     _mapper.Map(request.Event, eventToUpdate);
 
-                    if (eventToUpdate is HistoricalEvent updatedHistoricalEvent)
-                    {
-                        updatedHistoricalEvent.TimelineItemId = request.Event.TimelineItemId;
-                    }
-                    else if (eventToUpdate is CustomEvent updatedCustomEvent)
-                    {
-                        updatedCustomEvent.Location = request.Event.Location;
-                        updatedCustomEvent.Organizer = request.Event.Organizer;
-                    }
+                    UpdateSpecialEventFields(eventToUpdate, request);
 
-                    eventToUpdate.EventStreetcodes?.Clear();
-
-                    if (request.Event.StreetcodeIds != null)
-                    {
-                        var existingStreetcodeLinks = await _repositoryWrapper.EventStreetcodesRepository
-                            .GetAllAsync(es => es.EventId == eventToUpdate.Id);
-
-                        var existingStreetcodeIds = existingStreetcodeLinks.Select(es => es.StreetcodeId).ToList();
-
-                        var streetcodesToAdd = request.Event.StreetcodeIds
-                            .Where(id => !existingStreetcodeIds.Contains(id))
-                            .Select(id => new EventStreetcodes { EventId = eventToUpdate.Id, StreetcodeId = id })
-                            .ToList();
-
-                        var streetcodesToRemove = existingStreetcodeLinks
-                            .Where(es => !request.Event.StreetcodeIds.Contains(es.StreetcodeId))
-                            .ToList();
-
-                        if (streetcodesToAdd.Any())
-                        {
-                            await _repositoryWrapper.EventStreetcodesRepository.CreateRangeAsync(streetcodesToAdd);
-                        }
-
-                        if (streetcodesToRemove.Any())
-                        {
-                            _repositoryWrapper.EventStreetcodesRepository.DeleteRange(streetcodesToRemove);
-                        }
-
-                        await _repositoryWrapper.SaveChangesAsync();
-                    }
+                    await UpdateEventStreetcodes(eventToUpdate, request);
 
                     _repositoryWrapper.EventRepository.Update(eventToUpdate);
                     var isResultSuccess = await _repositoryWrapper.SaveChangesAsync() > 0;
@@ -138,6 +90,75 @@ namespace Streetcode.BLL.MediatR.Event.Update
                     return Result.Fail(new Error(errorMsg));
                 }
             }
+        }
+
+        private bool HasEventTypeChanged(EventEntity eventToUpdate, string newEventType)
+        {
+            return eventToUpdate.EventType != newEventType;
+        }
+
+        private EventEntity HandleEventTypeChange(EventEntity eventToUpdate, string newEventType)
+        {
+            if (eventToUpdate is HistoricalEvent historicalEvent)
+            {
+                historicalEvent.TimelineItemId = null;
+            }
+            else if (eventToUpdate is CustomEvent customEvent)
+            {
+                customEvent.Location = null;
+                customEvent.Organizer = null;
+            }
+
+            var newEvent = EventFactory.CreateEvent(newEventType);
+            newEvent.Id = eventToUpdate.Id;
+            return newEvent;
+        }
+
+        private void UpdateSpecialEventFields(EventEntity eventToUpdate, UpdateEventCommand request)
+        {
+            _mapper.Map(request.Event, eventToUpdate);
+
+            if (eventToUpdate is HistoricalEvent historicalEvent)
+            {
+                historicalEvent.TimelineItemId = request.Event.TimelineItemId;
+            }
+            else if (eventToUpdate is CustomEvent customEvent)
+            {
+                customEvent.Location = request.Event.Location;
+                customEvent.Organizer = request.Event.Organizer;
+            }
+        }
+
+        private async Task UpdateEventStreetcodes(EventEntity eventToUpdate, UpdateEventCommand request)
+        {
+            eventToUpdate.EventStreetcodes?.Clear();
+
+            if (request.Event.StreetcodeIds == null)
+            {
+                return;
+            }
+
+            var existingLinks = await _repositoryWrapper.EventStreetcodesRepository
+                .GetAllAsync(es => es.EventId == eventToUpdate.Id);
+
+            var existingIds = existingLinks.Select(es => es.StreetcodeId).ToList();
+            var toAdd = request.Event.StreetcodeIds.Except(existingIds)
+                .Select(id => new EventStreetcodes { EventId = eventToUpdate.Id, StreetcodeId = id })
+                .ToList();
+
+            var toRemove = existingLinks.Where(es => !request.Event.StreetcodeIds.Contains(es.StreetcodeId)).ToList();
+
+            if (toAdd.Any())
+            {
+                await _repositoryWrapper.EventStreetcodesRepository.CreateRangeAsync(toAdd);
+            }
+
+            if (toRemove.Any())
+            {
+                _repositoryWrapper.EventStreetcodesRepository.DeleteRange(toRemove);
+            }
+
+            await _repositoryWrapper.SaveChangesAsync();
         }
     }
 }

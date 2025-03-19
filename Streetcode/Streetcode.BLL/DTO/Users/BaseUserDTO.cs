d7af9005f26eb44f19c9ca687ce10e0a2aef8117
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2025-03-19 03:24:43 +0200

    changed dto

--- Streetcode/Streetcode.BLL/DTO/Users/BaseUserDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Users/BaseUserDTO.cs
@@ -5,11 +5,17 @@ namespace Streetcode.BLL.DTO.Users;
 public abstract class BaseUserDTO
 {
     public string Id { get; set; } = null!;
+
     public string Name { get; set; } = null!;
+
     public string Surname { get; set; } = null!;
+
     public string UserName { get; set; } = null!;
+
     public string? AboutYourself { get; set; }
+
     public int? AvatarId { get; set; } = null!;
+
     public List<ExpertiseDTO> Expertises { get; set; } = new();
 
     public string Role { get; set; } = null!;
--- Streetcode/Streetcode.BLL/DTO/Users/UpdateUserDTO.cs
+++ Streetcode/Streetcode.BLL/DTO/Users/UpdateUserDTO.cs
@@ -6,11 +6,18 @@ namespace Streetcode.BLL.DTO.Users;
 public class UpdateUserDTO
 {
     public string Id { get; set; } = null!;
+
     public string Name { get; set; } = null!;
+
     public string Surname { get; set; } = null!;
+
     public string UserName { get; set; } = null!;
+
     public string? AboutYourself { get; set; }
+
     public int? AvatarId { get; set; } = null!;
+
     public List<ExpertiseDTO> Expertises { get; set; } = new();
+
     public string PhoneNumber { get; set; } = null!;
 }

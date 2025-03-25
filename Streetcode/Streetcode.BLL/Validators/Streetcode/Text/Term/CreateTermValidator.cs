d9d67c541475a6fdfe1119b30d6a12601c94e18b
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2025-03-25 23:11:12 +0200

    Add validator on term updating

--- Streetcode/Streetcode.BLL/Validators/Streetcode/Text/Term/CreateTermValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/Streetcode/Text/Term/CreateTermValidator.cs
@@ -2,20 +2,16 @@
 using Microsoft.Extensions.Localization;
 using Streetcode.BLL.MediatR.Streetcode.Term.Create;
 using Streetcode.BLL.SharedResource;
-using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.Validators.Streetcode.Text.Term
 {
     public class CreateTermValidator : AbstractValidator<CreateTermCommand>
     {
-        private readonly IRepositoryWrapper _repositoryWrapper;
         public CreateTermValidator(
-            IRepositoryWrapper repositoryWrapper,
             BaseTermValidator baseTermValidator,
             IStringLocalizer<FailedToValidateSharedResource> localizer,
             IStringLocalizer<FieldNamesSharedResource> fieldLocalizer)
         {
-            _repositoryWrapper = repositoryWrapper;
             RuleFor(c => c.Term).SetValidator(baseTermValidator);
         }
     }

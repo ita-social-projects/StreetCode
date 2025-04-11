37823a791ce9f9bb91b5fcdaa9ccc05ded00da2e
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2025-04-11 23:51:33 +0300

    Create HealthCheck.sql

---
+++ Streetcode/Streetcode.DAL/Persistence/ScriptsMigration/HealthCheck.sql
@@ -0,0 +1,11 @@
+﻿BEGIN
+    IF EXISTS (SELECT 1 FROM [AspNetUsers] WHERE Email = 'admin@admin.com')
+    BEGIN
+        SELECT * FROM [AspNetUsers] WHERE Email = 'admin@admin.com';
+    END
+    ELSE
+    BEGIN
+        SELECT 1 FROM [AspNetUsers];
+    END
+END;
+GO

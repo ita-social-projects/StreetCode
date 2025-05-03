67b5d84daa5c5040dd846b6650328d04cf295e62
JuliaAndreyeva <blackonblackua@gmail.com>
2025-05-03 11:45:46 +0300

    issue-2675: made dateString field optional in validator

--- Streetcode/Streetcode.BLL/Validators/Streetcode/BaseStreetcodeValidator.cs
+++ Streetcode/Streetcode.BLL/Validators/Streetcode/BaseStreetcodeValidator.cs
@@ -63,10 +63,10 @@ public class BaseStreetcodeValidator : AbstractValidator<StreetcodeCreateUpdateD
             .InclusiveBetween(IndexMinValue, IndexMaxValue).WithMessage(localizer["MustBeBetween", fieldLocalizer["Index"], IndexMinValue, IndexMaxValue]);
 
         RuleFor(dto => dto.DateString)
-            .NotEmpty().WithMessage(localizer["CannotBeEmpty", fieldLocalizer["DateString"]])
             .MaximumLength(DateStringMaxLength).WithMessage(localizer["MaxLength", fieldLocalizer["DateString"], DateStringMaxLength])
             .Matches(@"^[А-Яа-яЁёЇїІіЄєҐґ0-9\s\(\)\-\–]+$") // Cyrillic letters, digits, parentheses, hyphen
-            .WithMessage(localizer["DateStringFormat"]);
+            .WithMessage(localizer["DateStringFormat"])
+            .When(dto => !string.IsNullOrEmpty(dto.DateString), ApplyConditionTo.CurrentValidator);
 
         RuleFor(dto => dto.StreetcodeType)
             .NotNull().WithMessage(localizer["IsRequired", fieldLocalizer["StreetcodeType"]])
